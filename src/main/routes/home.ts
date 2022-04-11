a2f89bbb586111cb2dad530734b83f0bd1e0e134
PraveenGuntoju <praveenkumar.guntoju@hmcts.net>
2022-04-11 21:05:22 +0100

    updated home page

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,7 +1,8 @@
 import { Application } from 'express';
+import { commonJson } from '../steps/content'
 
 export default function (app: Application): void {
   app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', commonJson);
   });
 }
