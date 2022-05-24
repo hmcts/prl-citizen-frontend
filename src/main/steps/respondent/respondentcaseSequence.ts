bb88a32a0831ae40e96514aa0233a1d9edd71207
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:31:16 +0100

    added navigation for screens for international factors

--- src/main/steps/respondent/respondentcaseSequence.ts
+++ src/main/steps/respondent/respondentcaseSequence.ts
@@ -1,6 +1,6 @@
 import { Sections, Step } from '../constants';
-import { DETAILS_KNOWN, RESPONDENT_TASK_LIST_URL, START_ALTERNATIVE, MIAM_START, MIAM_ATTEND_WILLINGNESS } from '../urls';
 import { YesOrNo } from 'app/case/definition';
+import { DETAILS_KNOWN, RESPONDENT_TASK_LIST_URL, START_ALTERNATIVE, MIAM_START, MIAM_ATTEND_WILLINGNESS, INTERNATIONAL_FACTORS_START, INTERNATIONAL_FACTORS_PARENTS, INTERNATIONAL_FACTORS_JURISDICTION, INTERNATIONAL_FACTORS_REQUEST } from '../urls';
 
 export const repondentCaseSequence: Step[] = [
   {
@@ -28,6 +28,26 @@ export const repondentCaseSequence: Step[] = [
   },
   {
     url: MIAM_ATTEND_WILLINGNESS,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => RESPONDENT_TASK_LIST_URL,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_START,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_PARENTS,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_PARENTS,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_JURISDICTION,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_JURISDICTION,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_REQUEST,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_REQUEST,
     showInSection: Sections.AboutRespondentCase,
     getNextStep: () => RESPONDENT_TASK_LIST_URL,
   },
