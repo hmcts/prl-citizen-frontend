30bc86fd01a96eb9ac5a65a6a19ebf899bd9fdc0
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:32:57 +0100

    added international factors to task list

--- src/main/steps/respondent/task-list/tasklist-items.ts
+++ src/main/steps/respondent/task-list/tasklist-items.ts
@@ -1,4 +1,5 @@
 export const respondent_tasklist_items_en = {
     keep_your_details_private: 'Keep your details private',
-    mediation_miam: 'Mediation(MIAM)'
+    mediation_miam: 'Mediation(MIAM)',
+    international_factors: 'International factors'
   }
