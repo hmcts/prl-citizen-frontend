8e4458e735dd12e796b12c3333750a83d1d4494a
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:32:12 +0100

    added section title for international factors in task list

--- src/main/steps/respondent/task-list/section-titles.ts
+++ src/main/steps/respondent/task-list/section-titles.ts
@@ -1,4 +1,5 @@
 export const respondent_en = {
     respondentYourDetails: 'Your details',
-    applicationDetails: 'Application detail'
+    applicationDetails: 'Application detail',
+    respondentAdditionalInformation: 'Additional information'
   }
