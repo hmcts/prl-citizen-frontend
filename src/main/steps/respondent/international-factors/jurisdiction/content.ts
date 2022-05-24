105e1ff11e53aa523d12f8a18872b5c954a5e17b
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:36:59 +0100

    template and content files for international factors added

---
+++ src/main/steps/respondent/international-factors/jurisdiction/content.ts
@@ -0,0 +1,80 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn, isTextAreaValid } from '../../../../app/form/validation';
+
+const en = {
+  section: ' ',
+  title: 'Could another person in the application apply for a similar order in a country outside England or Wales?',
+  one: 'Yes',
+  two: 'No',
+  twoHint: 'For example, because a court in another country has the power to make decisions or judgements (jurisdiction).',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    jurisdiction: {
+      required: 'Please select one of the options before proceeding further',
+    }
+  },
+};
+
+const cy: typeof en = {
+  section: ' ',
+  title: 'Could another person in the application apply for a similar order in a country outside England or Wales?',
+  one: 'Yes',
+  two: 'No',
+  twoHint: 'For example, because a court in another country has the power to make decisions or judgements (jurisdiction).',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    jurisdiction: {
+      required: 'Please select one of the options before proceeding further',
+    }
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    jurisdiction: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.label,
+      section: l => l.section,
+      hint: l => l.twoHint,
+      values: [
+        {
+          label: l => l.one,
+          value: 'Yes',
+          subFields: {
+            iFactorsJurisdictionProvideDetails: {
+              type: 'textarea',
+              label: 'Provide details',
+              labelSize: null,
+              validator: value => isFieldFilledIn(value) || isTextAreaValid(value),
+            },
+          },
+        },
+        {
+          label: l => l.two,
+          value: 'No',
+        }
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
