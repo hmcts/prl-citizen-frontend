34dc7ac64ead65f61cdb29d0d48896043de45cbb
Aloknath Datta <alokdatta.uit@gmail.com>
2022-05-24 14:19:21 +0100

    config clean up

--- Dockerfile
+++ Dockerfile
@@ -18,5 +18,5 @@ RUN PUPPETEER_SKIP_DOWNLOAD=true yarn install && yarn build:prod
 FROM base as runtime
 RUN rm -rf webpack/ webpack.config.js
 COPY --from=build $WORKDIR/src/main ./src/main
-# TODO: expose the right port for your application
-EXPOSE 3000
+
+EXPOSE 3001
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 const sourcePath = path.resolve(__dirname, 'src/main/assets/js');
 const govukFrontend = require(path.resolve(__dirname, 'webpack/govukFrontend'));
+const hmctsFrontend = require(path.resolve(__dirname, 'webpack/hmctsFrontend'));
 const scss = require(path.resolve(__dirname, 'webpack/scss'));
+const app = require(path.resolve(__dirname, 'webpack/app'));
 const HtmlWebpack = require(path.resolve(__dirname, 'webpack/htmlWebpack'));
 
 const devMode = process.env.NODE_ENV !== 'production';
@@ -10,7 +12,13 @@ const fileNameSuffix = devMode ? '-dev' : '.[contenthash]';
 const filename = `[name]${fileNameSuffix}.js`;
 
 module.exports = {
-  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins],
+  plugins: [
+    ...govukFrontend.plugins,
+    ...scss.plugins,
+    ...HtmlWebpack.plugins,
+    ...hmctsFrontend.plugins,
+    ...app.plugins,
+  ],
   entry: path.resolve(sourcePath, 'index.ts'),
   mode: devMode ? 'development' : 'production',
   module: {
