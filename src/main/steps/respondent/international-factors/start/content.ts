1ac48764d492b922ae7f13e1c02e45eafc020b63
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:36:59 +0100

    template and content files for international factors added

---
+++ src/main/steps/respondent/international-factors/start/content.ts
@@ -0,0 +1,82 @@
+import { YesOrNo } from 'app/case/definition';
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn, isTextAreaValid } from '../../../../app/form/validation';
+
+const en = {
+  section: ' ',
+  title: 'Do the children live outside of England or Wales?',
+  one: 'Yes',
+  two: 'No',
+  twoHint: 'For example, does their main family life take place outside of England and Wales?',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    start: {
+      required: 'Please select one of the options before proceeding further',
+    }
+  },
+};
+
+const cy: typeof en = {
+  section: ' ',
+  title: 'Do the children live outside of England or Wales?',
+  one: 'Yes',
+  two: 'No',
+  twoHint: 'For example, does their main family life take place outside of England and Wales?',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    start: {
+      required: 'Please select one of the options before proceeding further',
+    }
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    start: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.label,
+      section: l => l.section,
+      hint: l => l.hint,
+      values: [
+        {
+          label: l => l.one,
+          value: YesOrNo.YES,
+          subFields: {
+            iFactorsStartProvideDetails: {
+              type: 'textarea',
+              label: 'Provide details',
+              labelSize: null,
+              validator: value => isFieldFilledIn(value) || isTextAreaValid(value),
+            },
+          },
+        },
+        {
+          label: l => l.two,
+          value: YesOrNo.NO,
+        },
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
