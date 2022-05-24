94dd8c635a3f5997c5f53c8f6c52a16c8d4519c2
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:31:16 +0100

    added navigation for screens for international factors

--- src/main/steps/respondent/respondentcaseSequence.ts
+++ src/main/steps/respondent/respondentcaseSequence.ts
@@ -1,5 +1,5 @@
 import { Sections, Step } from '../constants';
-import { DETAILS_KNOWN, RESPONDENT_TASK_LIST_URL, START_ALTERNATIVE } from '../urls';
+import { DETAILS_KNOWN, RESPONDENT_TASK_LIST_URL, START_ALTERNATIVE, INTERNATIONAL_FACTORS_START, INTERNATIONAL_FACTORS_PARENTS, INTERNATIONAL_FACTORS_JURISDICTION, INTERNATIONAL_FACTORS_REQUEST } from '../urls';
 
 export const repondentCaseSequence: Step[] = [
   {
@@ -14,6 +14,26 @@ export const repondentCaseSequence: Step[] = [
   },
   {
     url: START_ALTERNATIVE,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => RESPONDENT_TASK_LIST_URL,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_START,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_PARENTS,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_PARENTS,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_JURISDICTION,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_JURISDICTION,
+    showInSection: Sections.AboutRespondentCase,
+    getNextStep: () => INTERNATIONAL_FACTORS_REQUEST,
+  },
+  {
+    url: INTERNATIONAL_FACTORS_REQUEST,
     showInSection: Sections.AboutRespondentCase,
     getNextStep: () => RESPONDENT_TASK_LIST_URL,
   },
