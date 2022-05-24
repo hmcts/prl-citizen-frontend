24e382d18557b894270a5dea79d9772c1a3c718f
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-05-24 17:34:57 +0100

    added method to update status on completion of event for international factors

--- src/main/steps/respondent/task-list/utils.ts
+++ src/main/steps/respondent/task-list/utils.ts
@@ -14,14 +14,21 @@ export const getKeepYourDetailsPrivateStatus = (userCase: CaseWithId): SectionSt
   return SectionStatus.NOT_STARTED;
 };
 
-
-
-
 export const getMiamStatus = (userCase: CaseWithId): SectionStatus => {
   if (userCase?.miamStart && userCase?.miamWillingness) {
     return SectionStatus.COMPLETED;
   }
   if (userCase?.miamStart || userCase?.miamWillingness) {
+    return SectionStatus.IN_PROGRESS;
+  }
+  return SectionStatus.NOT_STARTED;
+};
+
+export const getInternationalFactorsStatus = (userCase: CaseWithId): SectionStatus => {
+  if (userCase?.start && userCase?.parents && userCase?.jurisdiction && userCase?.request ) {
+    return SectionStatus.COMPLETED;
+  }
+  if (userCase?.start || userCase?.parents || userCase?.request || userCase?.jurisdiction) {
     return SectionStatus.IN_PROGRESS;
   }
   return SectionStatus.NOT_STARTED;
