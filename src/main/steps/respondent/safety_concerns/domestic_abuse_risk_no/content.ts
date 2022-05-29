c99f9e57f816b6037b06a7815f313caf345ddbc6
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-05-29 23:27:23 +0100

    safety abuse No screens are added

---
+++ src/main/steps/respondent/safety_concerns/domestic_abuse_risk_no/content.ts
@@ -0,0 +1,70 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+
+const en = {
+  section: 'Safety concerns',
+  title: 'Your safety',
+  line1: 'What you told us',
+  line2: 'You have not suffered or are not at risk of suffering: ',
+  line3: 'psychological abuse',
+  line4: 'financial abuse',
+  line5: 'emotional abuse',
+  line6: 'physical abuse',
+  line7: 'sexual abuse',
+  line8: 'Based on your answer, we will not ask you any more questions about domestic violence or abuse. You can continue your response.',
+  line9: 'Are you unsure about the answer you gave?',
+  line10: 'Have a look at the following information if you are unsure about your answer.',
+  line11: '*These links are a placeholder and will changes',
+  line12: 'Find out about the signs of domestic violence or abuse',
+  line13: 'Lorem ipsum about Womens aid',
+  line14: 'If you are a man and have concerns for your safety',
+  line15: 'National Domestic Violence Helpline',
+  line16: 'If you want to change your answer, go back to the previous screen.',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+
+};
+
+const cy: typeof en = {
+  section: 'Safety concerns',
+  title: 'Your safety',
+  line1: 'What you told us',
+  line2: 'You have not suffered or are not at risk of suffering: ',
+  line3: 'psychological abuse',
+  line4: 'financial abuse',
+  line5: 'emotional abuse',
+  line6: 'physical abuse',
+  line7: 'sexual abuse',
+  line8: 'Based on your answer, we will not ask you any more questions about domestic violence or abuse. You can continue your response.',
+  line9: 'Are you unsure about the answer you gave?',
+  line10: 'Have a look at the following information if you are unsure about your answer.',
+  line11: 'These links are a placeholder and will changes',
+  line12: 'Find out about the signs of domestic violence or abuse',
+  line13: 'Lorem ipsum about Womens aid',
+  line14: 'If you are a man and have concerns for your safety',
+  line15: 'National Domestic Violence Helpline',
+  line16: 'If you want to change your answer, go back to the previous screen.',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+  },
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
---
+++ src/main/steps/respondent/safety_concerns/sexual_abuse/content.ts
@@ -0,0 +1,104 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn } from '../../../../app/form/validation';
+
+
+const en = {
+  section: 'Safety concerns',
+  title: 'Have you suffered or are you at risk of suffering domestic violence or abuse?',
+  line1: 'Only include abuse by the people in this application or someone connected to them.',
+  line2: 'Domestic violence or abuse includes incidents or a pattern of incidents of abusive or violent behaviour.',
+  line3: 'This may include:',
+  line4: 'controlling, threatening or coercive behaviour',
+  line5: 'violence',
+  line6: 'abuse',
+  line7: 'The incidents must have taken place between people who are aged 16 or over, who are current (or former) intimate partners or family members.',
+  line8: 'Domestic violence or abuse can occur no matter what their gender or sexuality of the people involved.',
+  line9: 'Examples include:',
+  line10: 'psychological abuse',
+  line11: 'financial abuse',
+  line12: 'emotional abuse',
+  line13: 'physical abuse',
+  line14: 'sexual abuse',
+  line15: 'It also includes culturally specific forms of abuse, including forced marriage, honour-based violence and dowry-related abuse.',
+  one: 'Yes',
+  two: 'No',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    detailsKnown: {
+      required: 'Enter your details known',
+    }
+  },
+};
+
+const cy: typeof en = {
+  section: 'Safety concerns',
+  title: 'Have you suffered or are you at risk of suffering domestic violence or abuse?',
+  line1: 'Only include abuse by the people in this application or someone connected to them.',
+  line2: 'Domestic violence or abuse includes incidents or a pattern of incidents of abusive or violent behaviour.',
+  line3: 'This may include:',
+  line4: 'controlling, threatening or coercive behaviour',
+  line5: 'violence',
+  line6: 'abuse',
+  line7: 'The incidents must have taken place between people who are aged 16 or over, who are current (or former) intimate partners or family members.',
+  line8: 'Domestic violence or abuse can occur no matter what their gender or sexuality of the people involved.',
+  line9: 'Examples include:',
+  line10: 'psychological abuse',
+  line11: 'financial abuse',
+  line12: 'emotional abuse',
+  line13: 'physical abuse',
+  line14: 'sexual abuse',
+  line15: 'It also includes culturally specific forms of abuse, including forced marriage, honour-based violence and dowry-related abuse.',
+  one: 'Yes',
+  two: 'No',
+  summaryText: 'Contacts for help',
+  continue: 'Continue',
+  errors: {
+    detailsKnown: {
+      required: 'Enter your details known',
+    }
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    detailsKnown: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.label,
+      section: l => l.section,
+      values: [
+        {
+          label: l => l.one,
+          value: 'Yes',
+        },
+        {
+          label: l => l.two,
+          value: 'No',
+        },
+        {
+          label: l => l.three,
+          value: 'I',
+        },
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
