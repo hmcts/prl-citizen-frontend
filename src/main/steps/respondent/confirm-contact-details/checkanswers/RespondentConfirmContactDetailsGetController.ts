cb39759e7ed7f0f6ef2cfa7556492486ad20686f
praveenrach <praveen.rachapally@HMCTS.NET>
2022-06-08 15:10:30 +0100

    renamed files for controllers

--- src/main/steps/respondent/confirm-contact-details/checkanswers/RespondentConfirmContactDetailsGetController.ts
+++ src/main/steps/respondent/confirm-contact-details/checkanswers/RespondentConfirmContactDetailsGetController.ts
@@ -6,7 +6,7 @@ import { AppRequest } from '../../../../app/controller/AppRequest';
 import { GetController } from '../../../../app/controller/GetController';
 //import { LanguagePreference } from '../../../../app/case/definition';
 @autobind
-export default class ConfirmContactDetailsGetController extends GetController {
+export default class RespondentConfirmContactDetailsGetController extends GetController {
   public async get(req: AppRequest, res: Response): Promise<void> {
     const redirect = false;
 
@@ -38,9 +38,12 @@ export default class ConfirmContactDetailsGetController extends GetController {
     req.session.userCase.applicant1Address1 = 'Flat 100';
     req.session.userCase.applicant1Address2 = 'Plashet Grove';
     req.session.userCase.applicant1AddressTown = 'London';
-    req.session.userCase.applicant1PhoneNumber = '1234567890';
-    req.session.userCase.applicant1EmailAddress = 'test@gmail.com';
+    req.session.userCase.applicant1PhoneNumber = '';
+    req.session.userCase.applicant1PhoneNumber = req.session.userCase.applicant1PhoneNumber !== '' ? req.session.userCase.applicant1PhoneNumber : '<span class="govuk-error-message">Complete this section</span>';
+    req.session.userCase.applicant1EmailAddress = '';
+    req.session.userCase.applicant1EmailAddress = req.session.userCase.applicant1EmailAddress !== '' ? req.session.userCase.applicant1EmailAddress : '<span class="govuk-error-message">Complete this section</span>';
     req.session.userCase.applicant1DateOfBirth = { day: '20', month: '1', year: '2000' };
+
 
     const callback = redirect ? undefined : () => super.get(req, res);
     super.saveSessionAndRedirect(req, res, callback);
