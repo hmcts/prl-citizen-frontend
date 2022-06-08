f8a5568041ba1ff255615cc8ced216036ed69578
Khurshed Khan <khurshed.khan@hmcts.net>
2022-06-08 16:29:18 +0100

    PRL-1496:added change for access code validation

--- src/main/app/controller/PostController.test.ts
+++ src/main/app/controller/PostController.test.ts
@@ -40,7 +40,38 @@ describe('PostController', () => {
     const controller = new PostController(mockFormContent.fields);
 
     const caseData = {
-      accessCode: 'QWERTY78',
+      respondentCaseInvites: [
+        {
+          id: '1c02d57f-fc86-46e6-8680-7aa45ca373ca',
+          value: {
+            partyId: 'c96d2b74-dd9a-44f5-aa0a-8974f21d7101',
+            accessCode: 'QWERTY78',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: 'sdsad@dda.com',
+          },
+        },
+        {
+          id: 'aa7b5d17-66dc-45e7-90a0-c84f9933f86c',
+          value: {
+            partyId: '5d99419b-1cb4-4b7d-bccd-0c946ab34210',
+            accessCode: '9PJ2MXEJ',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: 'eewr@sfsdf.com',
+          },
+        },
+        {
+          id: 'cd69d298-feba-412f-aea9-6f7e39c2d0cb',
+          value: {
+            partyId: '04aabcb4-cc4a-42bc-8097-4c55819e55cb',
+            accessCode: 'HC65ZLTA',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: null,
+          },
+        },
+      ],
       caseReference: '1234123412341234',
       accessCodeCheck: true,
       applicationType: ApplicationType.JOINT_APPLICATION,
@@ -55,7 +86,38 @@ describe('PostController', () => {
       }),
       getCaseById: jest.fn(() => {
         return {
-          accessCode: 'QWERTY78',
+          respondentCaseInvites: [
+            {
+              id: '1c02d57f-fc86-46e6-8680-7aa45ca373ca',
+              value: {
+                partyId: 'c96d2b74-dd9a-44f5-aa0a-8974f21d7101',
+                accessCode: 'QWERTY78',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'sdsad@dda.com',
+              },
+            },
+            {
+              id: 'aa7b5d17-66dc-45e7-90a0-c84f9933f86c',
+              value: {
+                partyId: '5d99419b-1cb4-4b7d-bccd-0c946ab34210',
+                accessCode: '9PJ2MXEJ',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'eewr@sfsdf.com',
+              },
+            },
+            {
+              id: 'cd69d298-feba-412f-aea9-6f7e39c2d0cb',
+              value: {
+                partyId: '04aabcb4-cc4a-42bc-8097-4c55819e55cb',
+                accessCode: 'HC65ZLTA',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: null,
+              },
+            },
+          ],
           caseReference: '1234123412341234',
           applicationType: ApplicationType.JOINT_APPLICATION,
         };
@@ -73,7 +135,38 @@ describe('PostController', () => {
     const controller = new PostController(mockFormContent.fields);
 
     const caseData = {
-      accessCode: 'QWERTY78',
+      respondentCaseInvites: [
+        {
+          id: '1c02d57f-fc86-46e6-8680-7aa45ca373ca',
+          value: {
+            partyId: 'c96d2b74-dd9a-44f5-aa0a-8974f21d7101',
+            accessCode: 'QWERTY78',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: 'sdsad@dda.com',
+          },
+        },
+        {
+          id: 'aa7b5d17-66dc-45e7-90a0-c84f9933f86c',
+          value: {
+            partyId: '5d99419b-1cb4-4b7d-bccd-0c946ab34210',
+            accessCode: '9PJ2MXEJ',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: 'eewr@sfsdf.com',
+          },
+        },
+        {
+          id: 'cd69d298-feba-412f-aea9-6f7e39c2d0cb',
+          value: {
+            partyId: '04aabcb4-cc4a-42bc-8097-4c55819e55cb',
+            accessCode: 'HC65ZLTA',
+            expiryDate: '2022-06-22',
+            invitedUserId: null,
+            caseInviteEmail: null,
+          },
+        },
+      ],
       caseReference: '1234123412341234',
       applicationType: ApplicationType.SOLE_APPLICATION,
     };
@@ -87,7 +180,38 @@ describe('PostController', () => {
       }),
       getCaseById: jest.fn(() => {
         return {
-          accessCode: 'QWERTY78',
+          respondentCaseInvites: [
+            {
+              id: '1c02d57f-fc86-46e6-8680-7aa45ca373ca',
+              value: {
+                partyId: 'c96d2b74-dd9a-44f5-aa0a-8974f21d7101',
+                accessCode: 'QWERTY78',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'sdsad@dda.com',
+              },
+            },
+            {
+              id: 'aa7b5d17-66dc-45e7-90a0-c84f9933f86c',
+              value: {
+                partyId: '5d99419b-1cb4-4b7d-bccd-0c946ab34210',
+                accessCode: '9PJ2MXEJ',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'eewr@sfsdf.com',
+              },
+            },
+            {
+              id: 'cd69d298-feba-412f-aea9-6f7e39c2d0cb',
+              value: {
+                partyId: '04aabcb4-cc4a-42bc-8097-4c55819e55cb',
+                accessCode: 'HC65ZLTA',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: null,
+              },
+            },
+          ],
           caseReference: '1234123412341234',
           applicationType: ApplicationType.SOLE_APPLICATION,
         };
@@ -109,7 +233,38 @@ describe('PostController', () => {
       triggerEvent: jest.fn(),
       getCaseById: jest.fn(() => {
         return {
-          accessCode: 'QWERTY78',
+          respondentCaseInvites: [
+            {
+              id: '1c02d57f-fc86-46e6-8680-7aa45ca373ca',
+              value: {
+                partyId: 'c96d2b74-dd9a-44f5-aa0a-8974f21d7101',
+                accessCode: 'QWERTY78',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'sdsad@dda.com',
+              },
+            },
+            {
+              id: 'aa7b5d17-66dc-45e7-90a0-c84f9933f86c',
+              value: {
+                partyId: '5d99419b-1cb4-4b7d-bccd-0c946ab34210',
+                accessCode: '9PJ2MXEJ',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: 'eewr@sfsdf.com',
+              },
+            },
+            {
+              id: 'cd69d298-feba-412f-aea9-6f7e39c2d0cb',
+              value: {
+                partyId: '04aabcb4-cc4a-42bc-8097-4c55819e55cb',
+                accessCode: 'HC65ZLTA',
+                expiryDate: '2022-06-22',
+                invitedUserId: null,
+                caseInviteEmail: null,
+              },
+            },
+          ],
           caseReference: '1234123412341234',
           applicationType: ApplicationType.JOINT_APPLICATION,
         };
