167586c0a69b5caee0a8d4250f83c25001371226
praveenrach <praveen.rachapally@HMCTS.NET>
2022-06-13 12:06:37 +0100

    css issues fix

--- src/main/steps/respondent/confirm-contact-details/checkanswers/RespondentConfirmContactDetailsGetController.ts
+++ src/main/steps/respondent/confirm-contact-details/checkanswers/RespondentConfirmContactDetailsGetController.ts
@@ -4,7 +4,7 @@ import { Response } from 'express';
 import { getCaseApi } from '../../../../app/case/CaseApi';
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { GetController } from '../../../../app/controller/GetController';
-//import { LanguagePreference } from '../../../../app/case/definition';
+import { CONFIDENTIAL_DETAILS } from '../../../../app/case/definition';
 @autobind
 export default class RespondentConfirmContactDetailsGetController extends GetController {
   public async get(req: AppRequest, res: Response): Promise<void> {
@@ -46,10 +46,10 @@ export default class RespondentConfirmContactDetailsGetController extends GetCon
     } else {
       req.session.userCase.applicant1PlaceOfBirthText = req.session.userCase.applicant1PlaceOfBirth;
     }
-    req.session.userCase.applicant1DateOfBirthText = req.session.userCase.applicant1DateOfBirth;
-    if(req.session.userCase.applicant1DateOfBirthText.day === '' || req.session.userCase.applicant1DateOfBirthText.month === '' || req.session.userCase.applicant1DateOfBirthText.year === ''){
-      req.session.userCase.applicant1DateOfBirthText = '<span class="govuk-error-message">Complete this section</span><div id="more-detail-hint-1" class="govuk-hint">Do not include personal or financial information, like your National Insurance number or credit card details.</div>';
-    }
+    // req.session.userCase.applicant1DateOfBirthText = req.session.userCase.applicant1DateOfBirth;
+    // if(req.session.userCase.applicant1DateOfBirthText.day === '' || req.session.userCase.applicant1DateOfBirthText.month === '' || req.session.userCase.applicant1DateOfBirthText.year === ''){
+    //   req.session.userCase.applicant1DateOfBirthText = '<span class="govuk-error-message">Complete this section</span><div id="more-detail-hint-1" class="govuk-hint">Do not include personal or financial information, like your National Insurance number or credit card details.</div>';
+    // }
 
     // if(!req.session.userCase.applicant1DateOfBirth || req.session.userCase.applicant1DateOfBirth.day === '' || req.session.userCase.applicant1DateOfBirth.month === '' || req.session.userCase.applicant1DateOfBirth.year === ''){
     //   req.session.userCase.applicant1PlaceOfBirthText = '<span class="govuk-error-message">Complete this section</span>';
@@ -69,10 +69,54 @@ export default class RespondentConfirmContactDetailsGetController extends GetCon
     req.session.userCase.applicant1EmailAddress =
       req.session.userCase.applicant1EmailAddress !== ''
         ? req.session.userCase.applicant1EmailAddress
-        : '<span class="govuk-error-message">Complete this section</span><span class="govuk-hint">Do not include personal or financial information.</span>';
+        : '<span class="govuk-error-message">Complete this section</span>';
     //req.session.userCase.applicant1DateOfBirth = { day: '20', month: '1', year: '2000' };
+
+    //keep your details private // done
+    this.confidentialDetails(req);
 
     const callback = redirect ? undefined : () => super.get(req, res);
     super.saveSessionAndRedirect(req, res, callback);
   }
+
+  
+  private confidentialDetails(req: AppRequest) {
+    if (req.session.userCase?.detailsKnown && req.session.userCase?.startAlternative) {
+      if (req.session.userCase.contactDetailsPrivate?.length !== 0) {
+        // public // address, email, phone
+
+        if (req.session.userCase?.contactDetailsPrivate?.includes('address')) {
+          //TO DO
+        }
+
+        if (req.session.userCase?.contactDetailsPrivate?.includes('phone')) {
+          req.session.userCase.applicant1PhoneNumber = req.session.userCase.applicant1PhoneNumber?.concat(
+            '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PRIVATE + '</span>'
+          );
+        } else {
+          req.session.userCase.applicant1PhoneNumber = req.session.userCase.applicant1PhoneNumber?.concat(
+            '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PUBLIC + '</span>'
+          );
+        }
+
+        if (req.session.userCase?.contactDetailsPrivate?.includes('email')) {
+          req.session.userCase.applicant1EmailAddress = req.session.userCase.applicant1EmailAddress?.concat(
+            '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PRIVATE + '</span>'
+          );
+        } else {
+          req.session.userCase.applicant1EmailAddress = req.session.userCase.applicant1EmailAddress?.concat(
+            '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PUBLIC + '</span>'
+          );
+        }
+      } else {
+        req.session.userCase.applicant1PhoneNumber = req.session.userCase.applicant1PhoneNumber?.concat(
+          '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PUBLIC + '</span>'
+        );
+        req.session.userCase.applicant1EmailAddress = req.session.userCase.applicant1EmailAddress?.concat(
+          '<span class="govuk-hint">' + CONFIDENTIAL_DETAILS.PUBLIC + '</span>'
+        );
+      }
+    }
+  }
 }
+
