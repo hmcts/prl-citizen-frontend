eb4234230d94859ff58abbe5cbab976679a69916
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-06-15 12:54:49 +0100

    test coverage is increased.

--- src/main/steps/applicant/all-documents/start/section-titles.ts
+++
@@ -1,6 +0,0 @@
-export const document_en = {
-  caseDocuments: 'Case documents',
-  attendTheHearing: 'Attending the hearing',
-  ordersFromTheCourt: 'Orders from the court',
-  finalDecision: 'Final decision from the court',
-};
