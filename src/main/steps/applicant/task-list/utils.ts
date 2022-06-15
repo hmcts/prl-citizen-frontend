54dfffd3caa0cf5529cea1629491214cd6993b0f
pete-lowe <petemlowe@gmail.com>
2022-06-15 16:10:49 +0100

    changed import

--- src/main/steps/applicant/task-list/utils.ts
+++ src/main/steps/applicant/task-list/utils.ts
@@ -1,15 +1,15 @@
 import { CaseWithId } from '../../../app/case/case';
 import { SectionStatus } from '../../../app/case/definition';
 
-// export const getKeepYourDetailsPrivateStatus = (userCase: CaseWithId): SectionStatus => {
-//   if (userCase?.detailsKnown && userCase?.startAlternative) {
-//     return SectionStatus.COMPLETED;
-//   }
-//   if (userCase?.detailsKnown || userCase?.startAlternative) {
-//     return SectionStatus.IN_PROGRESS;
-//   }
-//   return SectionStatus.NOT_STARTED;
-// };
+export const getKeepYourDetailsPrivateStatus = (userCase: CaseWithId): SectionStatus => {
+  if (userCase?.detailsKnown && userCase?.startAlternative) {
+    return SectionStatus.COMPLETED;
+  }
+  if (userCase?.detailsKnown || userCase?.startAlternative) {
+    return SectionStatus.IN_PROGRESS;
+  }
+  return SectionStatus.NOT_STARTED;
+};
 
 export const getConfirmOrEditYourContactDetails = (userCase: CaseWithId): SectionStatus => {
   if (userCase?.confirmcontactdetails) {
@@ -20,5 +20,3 @@ export const getConfirmOrEditYourContactDetails = (userCase: CaseWithId): Sectio
   }
   return SectionStatus.NOT_STARTED;
 };
-
-
