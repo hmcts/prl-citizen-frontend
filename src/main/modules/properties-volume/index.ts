fda120c95fea4b12ffd100fd741f82adacd95a2a
Khurshed Khan <khurshed.khan@hmcts.net>
2022-06-16 13:50:02 +0100

    changed user from system update to solicitor

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -13,7 +13,7 @@ export class PropertiesVolume {
       this.setSecret('secrets.prl.prl-citizen-frontend-idam-client-secret', 'services.idam.clientSecret');
       this.setSecret('secrets.prl.microservicekey-prl-citizen-frontend', 'services.authProvider.secret');
       this.setSecret('secrets.prl.idam-solicitor-username', 'services.idam.systemUsername');
-      this.setSecret('secrets.prl.dam-solicitor-password', 'services.idam.systemPassword');
+      this.setSecret('secrets.prl.idam-solicitor-password', 'services.idam.systemPassword');
     } else {
       this.setLocalSecret('prl-citizen-frontend-idam-client-secret', 'services.idam.clientSecret');
       this.setLocalSecret('microservicekey-prl-citizen-frontend', 'services.authProvider.secret');
