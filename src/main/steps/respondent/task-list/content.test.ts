fbbc4b5c24ed79b0dc46c805f2db34d74950f379
praveenrach <praveen.rachapally@HMCTS.NET>
2022-06-20 19:25:09 +0100

    unit test fixed

--- src/main/steps/respondent/task-list/content.test.ts
+++ src/main/steps/respondent/task-list/content.test.ts
@@ -63,7 +63,7 @@ describe('task-list > content', () => {
             {
               href: '/respondent/confirm-contact-details/checkanswers',
               id: 'confirm-or-edit-your-contact-details',
-              status: 'TO_DO',
+              status: 'IN_PROGRESS',
               text: 'Confirm or edit your contact details',
             },
           ],
