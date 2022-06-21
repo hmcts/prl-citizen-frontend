69ea9d4b2a0ba56a662343740cd9e4d633765200
Khurshed Khan <khurshed.khan@hmcts.net>
2022-06-21 11:48:55 +0100

    Changes to get data from ccd-data-store and  cos api connect

--- src/main/app/case/CosApiClient.integration.test.ts
+++ src/main/app/case/CosApiClient.integration.test.ts
@@ -1,5 +1,28 @@
-describe('getCosApi', () => {
-  test('should create a CosApi', () => {
-    expect(1).toBeLessThan(2);
+import axios, { AxiosInstance } from 'axios';
+
+import { mockRequest } from '../../../test/unit/utils/mockRequest';
+
+import { CosApiClient } from './CosApiClient';
+
+jest.mock('axios');
+jest.mock('config');
+jest.mock('../auth/service/get-service-auth-token');
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('CosApiClient', () => {
+
+  it('connect cos api', async () => {
+    const mockGet = jest.fn().mockResolvedValueOnce({ data: { mockPayment: 'data' } });
+    mockedAxios.create.mockReturnValueOnce({ get: mockGet } as unknown as AxiosInstance);
+    const req = mockRequest();
+
+    const client = new CosApiClient(req.session, 'http://return-url');
+
+    const actual = await client.get();
+
+    expect(mockGet).toHaveBeenCalledWith('/');
+
+    expect(actual).toEqual({ mockPayment: 'data' });
   });
 });
