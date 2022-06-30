1ec3b48d5067037be28420e13272dd3d43cda201
pete-lowe <petemlowe@gmail.com>
2022-06-30 10:41:56 +0100

    Added log for test

--- src/main/app/address/AddressLookupPostControllerBase.ts
+++ src/main/app/address/AddressLookupPostControllerBase.ts
@@ -31,6 +31,8 @@ export default class AddressLookupPostControllerBase extends PostController<AnyO
     }
     req.session.addresses = addresses;
 
+    console.log(addresses);
+
     this.redirect(req, res);
   }
 }
