4d05b6d086a6450993d0037a396cd24dbe29a680
Abdul Daim <abdul.daim@hmcts.net>
2022-07-19 16:34:52 +0100

    modified story to reflect the team

--- src/main/steps/c100-createcase/confidentiality/details-know/content.ts
+++ src/main/steps/c100-createcase/confidentiality/details-know/content.ts
@@ -5,23 +5,16 @@ import { isFieldFilledIn } from '../../../../app/form/validation';
 
 const en = () => ({
   caption: 'Keeping your contact details private',
-  headingTitle:
-    'Do you want to keep your contact details private from the other people named in the application (the respondents)?',
-  para1: 'The information you give us will be shared with the respondents. This includes your contact details.',
-  para2:
-    'For example, if you believe the other people in the case pose a risk to you or the children, you can ask the court to keep your contact details private',
+  headingTitle: 'Do the other people named in this application (the respondents) know any of your contact details?    ',
   one: 'Yes',
   two: 'No',
   three: "I don't know",
 });
 
 const cy = () => ({
-  caption: 'Keeping your contact details private - welsh',
+  caption: 'Keeping your contact details private  - welsh',
   headingTitle:
-    'Do you want to keep your contact details private from the other people named in the application (the respondents)? - welsh  ',
-  para1: 'The information you give us will be shared with the respondents. This includes your contact details. - welsh',
-  para2:
-    'For example, if you believe the other people in the case pose a risk to you or the children, you can ask the court to keep your contact details private. welsh',
+    'Do the other people named in this application (the respondents) know any of your contact details? - welsh  ',
   one: 'Yes - Welsh',
   two: 'No - Welsh',
   three: "I don't know - Welsh",
