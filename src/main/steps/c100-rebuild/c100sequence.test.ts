915d896dc507d20f3f0d8f734c00a56cf0e4ffa1
Abdul Daim <abdul.daim@hmcts.net>
2022-07-20 14:43:53 +0100

    completed feature PLC100-7

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -2,7 +2,7 @@ import { C100Sequence } from './c100sequence';
 
 describe('c100 application sequence', () => {
   test('should contain 1 entries in applicant 1 screen sequence', () => {
-    expect(C100Sequence).toHaveLength(2);
+    expect(C100Sequence).toHaveLength(3);
 
     expect(C100Sequence[0].url).toBe('/c100-rebuild/confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
@@ -10,6 +10,10 @@ describe('c100 application sequence', () => {
 
     expect(C100Sequence[1].url).toBe('/c100-rebuild/confidentiality/feedback');
     expect(C100Sequence[1].showInSection).toBe('c100');
-    expect(C100Sequence[1].getNextStep({})).toBe('/c100-rebuild/confidentiality/details-know');
+    expect(C100Sequence[1].getNextStep({})).toBe('/c100-rebuild/confidentiality/feedbackno');
+
+    expect(C100Sequence[2].url).toBe('/c100-rebuild/confidentiality/feedbackno');
+    expect(C100Sequence[2].showInSection).toBe('c100');
+    expect(C100Sequence[2].getNextStep({})).toBe('/c100-rebuild/confidentiality/details-know');
   });
 });
