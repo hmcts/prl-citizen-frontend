792c04bfad699ff2a76645c9989240798bc40413
Abdul Daim <abdul.daim@hmcts.net>
2022-07-20 13:58:17 +0100

    updated const to camelcase and update URL to details know

--- src/main/steps/c100-rebuild/c100sequence.ts
+++ src/main/steps/c100-rebuild/c100sequence.ts
@@ -1,10 +1,10 @@
 import { Sections, Step } from '../constants';
-import { C100_CONFIDENTIALITY_START_ALTERNATIVE } from '../urls';
+import { C100_CONFIDENTIALITY_DETAILS_KNOW } from '../urls';
 
 export const C100Sequence: Step[] = [
   {
-    url: C100_CONFIDENTIALITY_START_ALTERNATIVE,
+    url: C100_CONFIDENTIALITY_DETAILS_KNOW,
     showInSection: Sections.C100,
-    getNextStep: () => C100_CONFIDENTIALITY_START_ALTERNATIVE,
+    getNextStep: () => C100_CONFIDENTIALITY_DETAILS_KNOW,
   },
 ];
