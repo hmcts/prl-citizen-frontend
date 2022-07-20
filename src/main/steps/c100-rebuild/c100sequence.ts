3e10029b0483244f98e2ca9f3a5aebdfcf0f8bad
Abdul Daim <abdul.daim@hmcts.net>
2022-07-20 13:44:09 +0100

    finished feature PRLC100-5

--- src/main/steps/c100-rebuild/c100sequence.ts
+++ src/main/steps/c100-rebuild/c100sequence.ts
@@ -1,9 +1,14 @@
 import { Sections, Step } from '../constants';
-import { C100_CONFIDENTIALITY_DETAILS_KNOW } from '../urls';
+import { C100_CONFIDENTIALITY_DETAILS_KNOW, C100_CONFIDENTIALITY_FEEDBACK } from '../urls';
 
 export const C100Sequence: Step[] = [
   {
     url: C100_CONFIDENTIALITY_DETAILS_KNOW,
+    showInSection: Sections.C100,
+    getNextStep: () => C100_CONFIDENTIALITY_FEEDBACK,
+  },
+  {
+    url: C100_CONFIDENTIALITY_FEEDBACK,
     showInSection: Sections.C100,
     getNextStep: () => C100_CONFIDENTIALITY_DETAILS_KNOW,
   },
