0f119a2ee79276cf6c52972ebbe1d34e2ae0bd04
Poojitha Nagappa <poojithanag@gmail.com>
2022-07-21 15:07:50 +0100

    Automation Scripts-1538

--- src/test/end-to-end/steps_file.js
+++ src/test/end-to-end/steps_file.js
@@ -3,6 +3,9 @@ const Login = require('./pages/LoginPage');
 const HomePage = require('./pages/HomePage');
 const InternationalElement = require('./pages/InternationalElement');
 const generalHelper = require('./helpers/generalHelper');
+const CurrentOrPreviousProceedings = require('./pages/CurrentOrPreviousProceedings');
+const ConsentToApplication = require('./pages/ConsentToApplication');
+const SupportYouNeed = require('./pages/SupportYouNeedDuringYourCase');
 
 module.exports = () => {
   return actor({
@@ -13,11 +16,17 @@ module.exports = () => {
     loginAsCitizen() {
       return Login.loginAsCitizen();
     },
-    homePage() {
-      return HomePage.clickRespondentLink();
-    },
     internationalElement() {
-      return InternationalElement.clickInternationalElement();
+      return InternationalElement.clickInternationalElementHappyPath();
+    },
+    currentOrPreviousProceedings() {
+      return CurrentOrPreviousProceedings.currentOrPreviousProceedingsHappyPath();
+    },
+    consentToApplication() {
+      return ConsentToApplication.consentToApplicationHappyPath();
+    },
+    supportYouNeedDuringYourCase() {
+      return SupportYouNeed.supportYouNeedHappyPath();
     },
     amOnHistoryPageWithSuccessNotification() {
       return generalHelper.amOnHistoryPageWithSuccessNotification();
