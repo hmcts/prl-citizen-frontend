2a0e5cc41ed81b2a4bdf321636564b53c04c8ff9
kumar dhanasamy <kumar.dhanasamy@hmcts.net>
2022-07-21 10:10:28 +0100

    Update c100sequence.test.ts

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -3,12 +3,12 @@ import { C100Sequence } from './c100sequence';
 describe('C100Sequence', () => {
   test('should contain 1 entries in c100 screen sequence', () => {
     expect(C100Sequence).toHaveLength(2);
-    expect(C100Sequence[0].url).toBe('/confidentiality/details-know');
+    expect(C100Sequence[0].url).toBe('/c100-rebuild//confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
-    expect(C100Sequence[0].getNextStep({})).toBe('/confidentiality/details-know');
+    expect(C100Sequence[0].getNextStep({})).toBe('/c100-rebuild/confidentiality/details-know');
 
-    expect(C100Sequence[1].url).toBe('/confidentiality/start');
+    expect(C100Sequence[1].url).toBe('/c100-rebuild/confidentiality/start');
     expect(C100Sequence[1].showInSection).toBe('c100');
-    expect(C100Sequence[1].getNextStep({})).toBe('/confidentiality/start');
+    expect(C100Sequence[1].getNextStep({})).toBe('/c100-rebuild/confidentiality/start');
   });
 });
