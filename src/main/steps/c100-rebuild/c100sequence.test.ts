27d15b78fc9dbe54e40fefa98edb82f042a943ba
Abdul Daim <abdul.daim@hmcts.net>
2022-07-25 12:48:34 +0100

    added test cases for c100 sequences and GetController

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -1,3 +1,5 @@
+import { YesOrNo } from '../../app/case/definition';
+
 import { C100Sequence } from './c100sequence';
 
 describe('C100Sequence', () => {
@@ -5,7 +7,10 @@ describe('C100Sequence', () => {
     expect(C100Sequence).toHaveLength(5);
     expect(C100Sequence[0].url).toBe('/c100-rebuild/confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
-    expect(C100Sequence[0].getNextStep({})).toBe('/c100-rebuild/confidentiality/start');
+    expect(C100Sequence[0].getNextStep({ detailsKnown: YesOrNo.YES })).toBe(
+      '/c100-rebuild/confidentiality/start-alternative'
+    );
+    expect(C100Sequence[0].getNextStep({ detailsKnown: YesOrNo.NO })).toBe('/c100-rebuild/confidentiality/start');
 
     expect(C100Sequence[1].url).toBe('/c100-rebuild/confidentiality/feedback');
     expect(C100Sequence[1].showInSection).toBe('c100');
@@ -17,10 +22,16 @@ describe('C100Sequence', () => {
 
     expect(C100Sequence[3].url).toBe('/c100-rebuild/confidentiality/start');
     expect(C100Sequence[3].showInSection).toBe('c100');
-    expect(C100Sequence[3].getNextStep({})).toBe('/c100-rebuild/confidentiality/feedbackno');
+    expect(C100Sequence[3].getNextStep({ start: YesOrNo.YES })).toBe('/c100-rebuild/confidentiality/feedback');
+    expect(C100Sequence[3].getNextStep({ start: YesOrNo.NO })).toBe('/c100-rebuild/confidentiality/feedbackno');
 
     expect(C100Sequence[4].url).toBe('/c100-rebuild/confidentiality/start-alternative');
     expect(C100Sequence[4].showInSection).toBe('c100');
-    expect(C100Sequence[4].getNextStep({})).toBe('/c100-rebuild/confidentiality/feedbackno');
+    expect(C100Sequence[4].getNextStep({ startAlternative: YesOrNo.YES })).toBe(
+      '/c100-rebuild/confidentiality/feedback'
+    );
+    expect(C100Sequence[4].getNextStep({ startAlternative: YesOrNo.NO })).toBe(
+      '/c100-rebuild/confidentiality/feedbackno'
+    );
   });
 });
