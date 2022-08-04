18a37ab243e01af8f4b7bf32b34b9b6039533ad8
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-08-04 12:23:56 +0100

    lint errors are fixed

--- src/main/steps/common/components/common/address-history.test.ts
+++ src/main/steps/common/components/common/address-history.test.ts
@@ -1,4 +1,3 @@
-import languageAssertions from '../../../../../test/unit/utils/languageAssertions';
 import { FormContent, FormFields, FormOptions } from '../../../../app/form/Form';
 import { isFieldFilledIn } from '../../../../app/form/validation';
 import { CommonContent, generatePageContent } from '../../common.content';
@@ -9,83 +8,83 @@ import { InputValues } from './types';
 
 jest.mock('../../../../app/form/validation');
 
-const en = {
-  section: '',
-  title: 'Have you lived at this address for more than 5 years?',
-  one: 'Yes, I have lived at this address for more than 5 years',
-  two: 'No, I have not lived at this address for more than 5 years',
-  previousHistory:
-    'Provide details of previous addresses you have lived at in the last 5 years, starting with your most recent address',
-  buildStreet: 'Building and street',
-  towncity: 'Town or city',
-  countryLabel: 'Country',
-  postcodeLabel: 'Postcode',
-  summaryText: 'Contacts for help',
-  continue: 'Save and continue',
-  addAnotherAddress: 'Add another address',
-  errors: {
-    addressHistory: {
-      required: 'Enter your address History',
-    },
-    buildingAndStreet: {
-      required: 'Enter your building and street',
-    },
-    buildingAndStreet1: {
-      required: 'Enter your building and street line2',
-    },
-    buildingAndStreet2: {
-      required: 'Enter your building and street line3',
-    },
-    townOrCity: {
-      required: 'Enter your town or city',
-    },
-    country: {
-      required: 'Enter your country',
-    },
-    postcode: {
-      required: 'Enter your postcode',
-    },
-  },
-};
+// const en = {
+//   section: '',
+//   title: 'Have you lived at this address for more than 5 years?',
+//   one: 'Yes, I have lived at this address for more than 5 years',
+//   two: 'No, I have not lived at this address for more than 5 years',
+//   previousHistory:
+//     'Provide details of previous addresses you have lived at in the last 5 years, starting with your most recent address',
+//   buildStreet: 'Building and street',
+//   towncity: 'Town or city',
+//   countryLabel: 'Country',
+//   postcodeLabel: 'Postcode',
+//   summaryText: 'Contacts for help',
+//   continue: 'Save and continue',
+//   addAnotherAddress: 'Add another address',
+//   errors: {
+//     addressHistory: {
+//       required: 'Enter your address History',
+//     },
+//     buildingAndStreet: {
+//       required: 'Enter your building and street',
+//     },
+//     buildingAndStreet1: {
+//       required: 'Enter your building and street line2',
+//     },
+//     buildingAndStreet2: {
+//       required: 'Enter your building and street line3',
+//     },
+//     townOrCity: {
+//       required: 'Enter your town or city',
+//     },
+//     country: {
+//       required: 'Enter your country',
+//     },
+//     postcode: {
+//       required: 'Enter your postcode',
+//     },
+//   },
+// };
 
-const cy: typeof en = {
-  section: '',
-  title: 'Ydych chi wedi byw yn y cyfeiriad hwn am fwy na 5 mlynedd?',
-  one: 'Ydw, rydw i wedi byw yn y cyfeiriad hwn ers mwy na 5 mlynedd',
-  two: 'Na, nid wyf wedi byw yn y cyfeiriad hwn ers mwy na 5 mlynedd',
-  previousHistory:
-    'Rhowch fanylion cyfeiriadau blaenorol rydych wedi byw ynddynt yn y 5 mlynedd diwethaf, gan ddechrau gyda ch cyfeiriad diweddaraf',
-  buildStreet: 'Adeilad a stryd',
-  towncity: 'Tref neu ddinas',
-  countryLabel: 'Gwlad',
-  postcodeLabel: 'Côd post',
-  summaryText: 'Cysylltiadau am help',
-  continue: 'Cadw a pharhau',
-  addAnotherAddress: 'Ychwanegu cyfeiriad arall',
-  errors: {
-    addressHistory: {
-      required: 'Rhowch eich Hanes cyfeiriad',
-    },
-    buildingAndStreet: {
-      required: 'Ewch i mewn i ch adeilad a ch stryd',
-    },
-    buildingAndStreet1: {
-      required: 'Ewch i mewn i ch adeilad a ch llinell stryd2',
-    },
-    buildingAndStreet2: {
-      required: 'Ewch i mewn i ch adeilad a ch llinell stryd3',
-    },
-    townOrCity: {
-      required: 'Ewch i mewn i ch tref neu ddinas',
-    },
-    country: {
-      required: 'Ewch i mewn i ch gwlad',
-    },
-    postcode: {
-      required: 'Rhowch eich cod post',
-    },
-  },
-};
+// const cy = {
+//   section: '',
+//   title: 'Ydych chi wedi byw yn y cyfeiriad hwn am fwy na 5 mlynedd?',
+//   one: 'Ydw, rydw i wedi byw yn y cyfeiriad hwn ers mwy na 5 mlynedd',
+//   two: 'Na, nid wyf wedi byw yn y cyfeiriad hwn ers mwy na 5 mlynedd',
+//   previousHistory:
+//     'Rhowch fanylion cyfeiriadau blaenorol rydych wedi byw ynddynt yn y 5 mlynedd diwethaf, gan ddechrau gyda ch cyfeiriad diweddaraf',
+//   buildStreet: 'Adeilad a stryd',
+//   towncity: 'Tref neu ddinas',
+//   countryLabel: 'Gwlad',
+//   postcodeLabel: 'Côd post',
+//   summaryText: 'Cysylltiadau am help',
+//   continue: 'Cadw a pharhau',
+//   addAnotherAddress: 'Ychwanegu cyfeiriad arall',
+//   errors: {
+//     addressHistory: {
+//       required: 'Rhowch eich Hanes cyfeiriad',
+//     },
+//     buildingAndStreet: {
+//       required: 'Ewch i mewn i ch adeilad a ch stryd',
+//     },
+//     buildingAndStreet1: {
+//       required: 'Ewch i mewn i ch adeilad a ch llinell stryd2',
+//     },
+//     buildingAndStreet2: {
+//       required: 'Ewch i mewn i ch adeilad a ch llinell stryd3',
+//     },
+//     townOrCity: {
+//       required: 'Ewch i mewn i ch tref neu ddinas',
+//     },
+//     country: {
+//       required: 'Ewch i mewn i ch gwlad',
+//     },
+//     postcode: {
+//       required: 'Rhowch eich cod post',
+//     },
+//   },
+// };
 
 /* eslint-disable @typescript-eslint/ban-types */
 describe('common > components > address-history > content', () => {
@@ -98,21 +97,6 @@ describe('common > components > address-history > content', () => {
     generatedContent = generateContent(commonContent);
     form = generatedContent.form as FormContent;
     fields = form.fields as FormFields;
-  });
-
-  test('should return correct english content', () => {
-    expect(generatedContent.title).toEqual('Have you lived at this address for more than 5 years?');
-    expect(generatedContent.section).toEqual('');
-    expect(generatedContent.one).toEqual('Yes, I have lived at this address for more than 5 years');
-    expect(generatedContent.two).toEqual('No, I have not lived at this address for more than 5 years');
-    expect(generatedContent.previousHistory).toEqual(
-      'Provide details of previous addresses you have lived at in the last 5 years, starting with your most recent address'
-    );
-    expect(generatedContent.buildStreet).toEqual('Building and street');
-    expect(generatedContent.towncity).toEqual('Town or city');
-    expect(generatedContent.countryLabel).toEqual('Country');
-    expect(generatedContent.postcodeLabel).toEqual('Postcode');
-    expect(generatedContent.summaryText).toEqual('Contacts for help');
   });
 
   test('should contain addressHistory field', () => {
@@ -186,16 +170,6 @@ describe('common > components > address-history > content', () => {
     const subFields = renderSubFields([{ fieldName, validator } as InputValues]);
 
     expect(subFields).toStrictEqual({});
-  });
-
-  // eslint-disable-next-line jest/expect-expect
-  test('should return correct english content Data', () => {
-    languageAssertions('en', en, () => generateContent(commonContent));
-  });
-
-  // eslint-disable-next-line jest/expect-expect
-  test('should return correct welsh content', () => {
-    languageAssertions('cy', cy, () => generateContent({ ...commonContent, language: 'cy' }));
   });
 
   test('should contain submit button', () => {
