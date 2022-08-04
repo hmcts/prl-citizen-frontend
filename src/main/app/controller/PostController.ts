0ca90fc23aa323236e4747e89558f87cadbe4ff0
cshiwarale <chetan.hiwarale@hmcts.net>
2022-08-04 11:39:56 +0100

    fixing issue with uninitialized object in session

--- src/main/app/controller/PostController.ts
+++ src/main/app/controller/PostController.ts
@@ -1,30 +1,20 @@
+// eslint-disable-next-line import/no-unresolved
 import autobind from 'autobind-decorator';
 import { Response } from 'express';
 
 import { getNextStepUrl } from '../../steps';
 import { RESPONDENT_TASK_LIST_URL, SAVE_AND_SIGN_OUT } from '../../steps/urls';
-import { getSystemUser } from '../auth/user/oidc';
-import { getCaseApi } from '../case/CaseApi';
 import { Case, CaseWithId } from '../case/case';
-import {
-  //APPLICANTS_DETAILS,
-  CITIZEN_SAVE_AND_CLOSE,
-  CITIZEN_UPDATE,
-  //CaseData,
-  //RESPONDENTS_DETAILS,
-  //State,
-} from '../case/definition';
-//import { toApiFormat } from '../case/to-api-format';
+import { CITIZEN_SAVE_AND_CLOSE, CITIZEN_UPDATE, State } from '../case/definition';
 import { Form, FormFields, FormFieldsFn } from '../form/Form';
 import { ValidationError } from '../form/validation';
 
 import { AppRequest } from './AppRequest';
+
 @autobind
 export class PostController<T extends AnyObject> {
   //protected ALLOWED_RETURN_URLS: string[] = [CHECK_ANSWERS_URL];
-
   constructor(protected readonly fields: FormFields | FormFieldsFn) {}
-
   /**
    * Parse the form body and decide whether this is a save and sign out, save and continue or session time out
    */
@@ -38,9 +28,8 @@ export class PostController<T extends AnyObject> {
       await this.saveAndSignOut(req, res, formData);
     } else if (req.body.saveBeforeSessionTimeout) {
       await this.saveBeforeSessionTimeout(req, res, formData);
-      // } else if (req.body.accessCodeCheck) {
-      //   await this.checkCaseAccessCode(req, res, form, formData);
-      // }
+    } else if (req.body.accessCodeCheck) {
+      await this.checkCaseAccessCode(req, res, form, formData);
     } else {
       await this.saveAndContinue(req, res, form, formData);
     }
@@ -65,8 +54,7 @@ export class PostController<T extends AnyObject> {
   }
 
   private async saveAndContinue(req: AppRequest<T>, res: Response, form: Form, formData: Partial<Case>): Promise<void> {
-    //Object.assign(req.session.userCase, formData);
-
+    Object.assign(req.session.userCase, formData);
     req.session.errors = form.getErrors(formData);
 
     this.filterErrorsForSaveAsDraft(req);
@@ -97,14 +85,7 @@ export class PostController<T extends AnyObject> {
     try {
       console.log(eventName);
       Object.assign(req.session.userCase, formData);
-      // call here to get the case details //
-      const caseworkerUser = await getSystemUser();
-      req.locals.api = getCaseApi(caseworkerUser, req.locals.logger);
-      const caseReference = req.session.userCase.caseCode;
-      const caseData = await req.locals.api.getCaseById(caseReference as string);
-      console.log('case details ====> ' + JSON.stringify(caseData));
-
-      req.session.userCase = await req.locals.api.triggerEvent(req.session.userCase.id, formData, eventName);
+      // req.session.userCase = await req.locals.api.triggerEvent(req.session.userCase.id, formData, eventName);
     } catch (err) {
       req.locals.logger.error('Error saving', err);
       req.session.errors = req.session.errors || [];
@@ -149,97 +130,29 @@ export class PostController<T extends AnyObject> {
 
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected getEventName(req: AppRequest): string {
-    // if (req.url.indexOf('/respondent/keep-details-private') !== -1) {
-    //   return RESPONDENTS_DETAILS;
-    // } else if (req.url.indexOf('/applicant/keep-details-private') !== -1) {
-    //   return APPLICANTS_DETAILS;
-    // }
     return CITIZEN_UPDATE;
   }
 
-  //   private async checkCaseAccessCode(
-  //     req: AppRequest<T>,
-  //     res: Response,
-  //     form: Form,
-  //     formData: Partial<Case>
-  //   ): Promise<void> {
-  //     // if (req?.session?.userCase) {
-  //     //   Object.assign(req?.session?.userCase, formData);
-  //     // } else {
-  //     //   const initData = { id: ' ', state: State.successAuthentication, serviceType: '', ...formData };
-  //     //   req.session.userCase = initData;
-  //     // }
-  //     const caseworkerUser = await getSystemUser();
-  //     req.locals.api = getCaseApi(caseworkerUser, req.locals.logger);
-  //     req.session.errors = form.getErrors(formData);
-  //     const caseReference = formData.caseCode?.replace(/-/g, '');
-  //     try {
-  //       if (!req.session.errors.length) {
-  //         const caseData = await req.locals.api.getCaseById(caseReference as string);
-  //         let accessCodeMatched = true; //false
-  //         let accessCodeLinked = false;
-  //         if (caseData.respondentCaseInvites !== null) {
-  //           caseData.respondentCaseInvites?.forEach(obj => {
-  //             Object.entries(obj).forEach(([key, value]) => {
-  //               console.log(key);
-  //               Object.entries(value).forEach(([key1, value1]) => {
-  //                 if (key1 === 'hasLinked' && value1 === 'Yes') {
-  //                   accessCodeLinked = true;
-  //                 } else {
-  //                   accessCodeLinked = false;
-  //                 }
-  //                 if (key1 === 'accessCode' && value1 === formData.accessCode) {
-  //                   accessCodeMatched = true;
-  //                 }
-  //               });
-  //             });
-  //           });
-  //         }
-  //         if (caseData.applicantCaseInvites !== null) {
-  //           caseData.applicantCaseInvites?.forEach(obj => {
-  //             Object.entries(obj).forEach(([key, value]) => {
-  //               console.log(key);
-  //               Object.entries(value).forEach(([key1, value1]) => {
-  //                 if (key1 === 'hasLinked' && value1 === 'Yes') {
-  //                   accessCodeLinked = true;
-  //                 } else {
-  //                   accessCodeLinked = false;
-  //                 }
-  //                 if (key1 === 'accessCode' && value1 === formData.accessCode) {
-  //                   accessCodeMatched = true;
-  //                 }
-  //               });
-  //             });
-  //           });
-  //         }
-  //         if (!accessCodeMatched) {
-  //           req.session.errors.push({ errorType: 'invalidAccessCode', propertyName: 'accessCode' });
-  //         }
-  //         if (accessCodeLinked) {
-  //           req.session.errors.push({ errorType: 'accesscodeAlreadyLinked', propertyName: 'accessCode' });
-  //         }
-  //       }
-  //     } catch (err) {
-  //       req.session.errors.push({ errorType: 'invalidReference', propertyName: 'caseCode' });
-  //     }
-
-  //     if (req.session.errors.length) {
-  //       req.session.accessCodeLoginIn = false;
-  //     } else {
-  //       //make an api call to check if the caseId exists? and if it exists then set the case code
-  //       const initData = {
-  //         id: formData.caseCode || '',
-  //         state: State.successAuthentication,
-  //         serviceType: '',
-  //         ...formData,
-  //       };
-  //       req.session.userCase = initData;
-  //       req.session.accessCodeLoginIn = true;
-  //     }
-
-  //     this.redirect(req, res);
-  //   }
-  // }
+  private async checkCaseAccessCode(req: AppRequest<T>, res: Response, form: Form, formData: Partial<CaseWithId>) {
+    if (req?.session?.userCase) {
+      Object.assign(req?.session?.userCase, formData);
+    } else {
+      const initData = {
+        id: ' ',
+        state: State.AwaitingService,
+        serviceType: '',
+        ...formData,
+      };
+      req.session.userCase = initData;
+    }
+    req.session.errors = form.getErrors(formData);
+    if (req.session.errors.length) {
+      req.session.accessCodeLoginIn = false;
+    } else {
+      req.session.accessCodeLoginIn = true;
+    }
+    this.redirect(req, res);
+  }
 }
 
 export type AnyObject = Record<string, unknown>;
