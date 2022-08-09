28a36c49451756620b8d42364139899fd1ecc7f5
Khurshed Khan <khurshed.khan@hmcts.net>
2022-08-09 19:13:50 +0100

    PRL-1680: Added changes for upload document screen

---
+++ src/main/steps/respondent/upload-document/upload/content.ts
@@ -0,0 +1,52 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+
+
+
+const en = {
+  section: 'Provide the document',
+  title: 'Provide the documents',
+  continue: 'Save and continue',
+  add: 'Submit'
+};
+
+const cy: typeof en = {
+  section: 'Provide the document',
+  title: 'Provide the documents',
+  continue: 'Save and continue',
+  add: 'Submit'
+};
+
+
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    start: {
+      type: 'radios',
+      classes: 'radios',
+      label: l => l.label,
+      section: l => l.section,
+      hint: l => l.hint,
+      values: [
+
+      ],
+    },
+  },
+
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+  form,
+  };
+};
