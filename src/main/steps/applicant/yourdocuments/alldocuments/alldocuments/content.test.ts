1a78446d6ec7ae499c81b5eb7eba2d4bc9844907
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-08-14 12:58:09 +0100

    test cases

--- src/main/steps/applicant/yourdocuments/alldocuments/alldocuments/content.test.ts
+++ src/main/steps/applicant/yourdocuments/alldocuments/alldocuments/content.test.ts
@@ -143,6 +143,285 @@ describe('task-list > content', () => {
   });
   test.each([
     {
+      userCase: {
+        ...mockUserCase,
+        orderCollection: [],
+        allegationsOfHarmYesNo: 'NO',
+      },
+      expected: [
+        {
+          items: [
+            {
+              href: '#',
+              id: 'orders-from-the-court-all-docs',
+              text: 'View all orders from the court',
+            },
+          ],
+          title: 'Orders from the court',
+        },
+        {
+          items: [
+            {
+              id: 'applicant-response-to-request-for-child-arrangements',
+              text: "Applicant_FNAME_LNAME's request for child arrangements",
+              href: '/applicant/yourdocuments/alldocuments/carequest',
+            },
+            {
+              id: 'applicant-allegations-of-harm-and-violence',
+              text: "Applicant_FNAME_LNAME's allegations of harm and violence",
+              href: '/applicant/yourdocuments/alldocuments/aohviolence',
+            },
+            {
+              id: 'applicant_response_to_other_side_allegation_of_harm',
+              text: "Applicant_FNAME_LNAME's response to the other side's allegations of harm or violence",
+              href: '/applicant/yourdocuments/alldocuments/aohviolenceresponse',
+            },
+            {
+              id: 'applicant_position_statements',
+              text: "Applicant_FNAME_LNAME's position statements",
+              href: '/applicant/yourdocuments/alldocuments/positionstatement',
+            },
+            {
+              id: 'applicant_witness_statements',
+              text: "Applicant_FNAME_LNAME's witness statements",
+              href: '/applicant/yourdocuments/alldocuments/applicantwitnessstatements',
+            },
+            {
+              id: 'other_people_witness_statements',
+              text: "Other people's witness statements",
+              href: '/applicant/yourdocuments/alldocuments/otherpeoplewitnessstatement',
+            },
+            {
+              id: 'medical_reports',
+              text: 'Medical reports',
+              href: '/applicant/yourdocuments/alldocuments/medicalreports',
+            },
+            {
+              id: 'miam_certificate',
+              text: 'MIAM certificate',
+              href: '/applicant/yourdocuments/alldocuments/miamcertificate',
+            },
+            {
+              id: 'applications_made_in_these_proceedings',
+              text: 'Applications made in these proceedings',
+              href: '/applicant/yourdocuments/alldocuments/applicationmade',
+            },
+            {
+              id: 'previous_orders_submitted',
+              text: 'Previous orders submitted with application',
+              href: '/applicant/yourdocuments/alldocuments/previousorders',
+            },
+            {
+              id: 'letters_from_school',
+              text: 'Letters from school',
+              href: '/applicant/yourdocuments/alldocuments/letterfromschool',
+            },
+            {
+              id: 'digital_downloads',
+              text: 'Emails, screenshots, images and other media files',
+              href: '/applicant/yourdocuments/alldocuments/digitaldownloads',
+            },
+            {
+              id: 'photographic_evidence',
+              text: 'Photographic evidence',
+              href: '/applicant/yourdocuments/alldocuments/photographicevidence',
+            },
+            {
+              id: 'mobile_phone_screenshots',
+              text: 'Mobile phone screenshots',
+              href: '/applicant/yourdocuments/alldocuments/mobilescreenshots',
+            },
+            {
+              id: 'medical_records',
+              text: 'Medical records',
+              href: '/applicant/yourdocuments/alldocuments/medicalrecords',
+            },
+            {
+              id: 'paternity_test_reports',
+              text: 'Paternity test reports',
+              href: '/applicant/yourdocuments/alldocuments/paternity_test_reports',
+            },
+            {
+              id: 'drug_alcohol_tests',
+              text: 'Drug and alcohol tests (toxicology)',
+              href: '/applicant/yourdocuments/alldocuments/drug_alcohol_tests',
+            },
+            {
+              id: 'police_disclosures',
+              text: 'Police disclosures',
+              href: '/applicant/yourdocuments/alldocuments/police_disclosures',
+            },
+            {
+              id: 'witness_availability',
+              text: 'Witness availability',
+              href: '/applicant/yourdocuments/alldocuments/witness_availability',
+            },
+          ],
+          title: "Applicant's documents",
+        },
+        {
+          items: [
+            {
+              href: '/applicant/yourdocuments/alldocuments/caresponse',
+              id: 'respondent_response_to_request_for_child_arrangements',
+              text: "<namerespondentxxxxx>'s response to the request for child arrangements",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentaohresponse',
+              id: 'respondent_response_to_allegations_of_harm_and_violence',
+              text: "<namerespondentxxxxx>'s response to the allegations of harm and violence",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentaohviolence',
+              id: 'respondent_allegation_of_harm_and_violence',
+              text: "<namerespondentxxxxx>'s allegations of harm and violence",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentapplicationmade',
+              id: 'applications_made_in_these_proceedings_respondent',
+              text: 'Applications made in these proceedings',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentpreviousorders',
+              id: 'previous_orders_submitted_respondent',
+              text: 'Previous orders submitted with application',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentletterfromschool',
+              id: 'letters_from_school_respondent',
+              text: 'Letters from school',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentpositionstatement',
+              id: 'respondent_position_statements',
+              text: "<namerespondentxxxxx>'s position statements",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentwitnessstatement',
+              id: 'respondent_witness_statements',
+              text: "<namerespondentxxxxx>'s witness statements",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentOtherPeopleWitnessStatement',
+              id: 'other_people_witness_statements_respondent',
+              text: "Other people's witness statements",
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentdigitaldownloads',
+              id: 'digital_downloads_respondent',
+              text: 'Emails, screenshots, images and other media files',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentphotographicevidence',
+              id: 'photographic_evidence_respondent',
+              text: 'Photographic evidence',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentmobilescreenshots',
+              id: 'mobile_phone_screenshots_respondent',
+              text: 'Mobile phone screenshots',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentmedicalrecords',
+              id: 'medical_records_respondent',
+              text: 'Medical records',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondentmedicalreports',
+              id: 'medical_reports_respondent',
+              text: 'Medical reports',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondent_paternity_test_reports',
+              id: 'paternity_test_reports_respondent',
+              text: 'Paternity test reports',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondent_drug_alcohol_tests',
+              id: 'drug_alcohol_tests_respondent',
+              text: 'Drug and alcohol tests (toxicology)',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondent_police_disclosure',
+              id: 'police_disclosures_respondent',
+              text: 'Police disclosures',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/respondent_drug_alcohol_tests',
+              id: 'witness_availability_respondent',
+              text: 'Witness availability',
+            },
+          ],
+          title: "Respondent's documents",
+        },
+        {
+          items: [
+            {
+              href: '/applicant/yourdocuments/alldocuments/safeguarding_letter',
+              id: 'safeguarding_letter',
+              text: 'Safeguarding letter',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/section7_report',
+              id: 'section7_report',
+              text: 'Section 7 report',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/section37_report',
+              id: 'section37_report',
+              text: 'Section 37 report',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/risk_assessment_16a',
+              id: 'risk_assessment_16a',
+              text: '16a risk assessment',
+            },
+          ],
+          title: 'CAFCASS and local authority document',
+        },
+        {
+          items: [
+            {
+              href: '/applicant/yourdocuments/alldocuments/important_address_and_contact_details',
+              id: 'important_address_and_contact_details',
+              text: 'Important information about your address and contact details',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/dna_reports',
+              id: 'dna_reports',
+              text: 'DNA reports',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/privacy_notice',
+              id: 'privacy_notice',
+              text: 'Privacy notice',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/special_measures',
+              id: 'special_measures',
+              text: 'Special measures',
+            },
+          ],
+          title: 'Other documents',
+        },
+        {
+          items: [
+            {
+              href: '/applicant/yourdocuments/alldocuments/notice_of_hearing',
+              id: 'notice_of_hearing',
+              text: 'Notice of hearing',
+            },
+            {
+              href: '/applicant/yourdocuments/alldocuments/support_needed',
+              id: 'support_you_need_during_your_case',
+              text: 'Support you need during your case',
+            },
+          ],
+          title: 'Attending the hearing',
+        },
+      ],
+    },
+    {
       userCase: mockUserCase,
       expected: [
         {
--- src/main/steps/applicant/yourdocuments/alldocuments/alldocuments/tasklistalldocuments.ts
+++ src/main/steps/applicant/yourdocuments/alldocuments/alldocuments/tasklistalldocuments.ts
@@ -45,7 +45,7 @@ export const getApplicantDocuments = (sectionTitles, taskListItems, userCase) =>
       {
         id: 'applicant-response-to-request-for-child-arrangements',
         text: getText(taskListItems.applicant_response_to_request_for_child_arrangements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_CA_REQUEST : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_CA_REQUEST : '#',
       },
       {
         id: 'applicant-allegations-of-harm-and-violence',
@@ -58,12 +58,12 @@ export const getApplicantDocuments = (sectionTitles, taskListItems, userCase) =>
       {
         id: 'applicant_response_to_other_side_allegation_of_harm',
         text: getText(taskListItems.applicant_response_to_other_side_allegation_of_harm, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_RESPONSE_TO_AOH_VIOLENCE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_RESPONSE_TO_AOH_VIOLENCE : '#',
       },
       {
         id: 'applicant_position_statements',
         text: getText(taskListItems.applicant_position_statements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_POSITION_STATEMENT : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_POSITION_STATEMENT : '#',
       },
       {
         id: 'applicant_witness_statements',
@@ -73,72 +73,72 @@ export const getApplicantDocuments = (sectionTitles, taskListItems, userCase) =>
       {
         id: 'other_people_witness_statements',
         text: getText(taskListItems.other_people_witness_statements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.OTHER_PEOPLE_WITNESS_STATEMENTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.OTHER_PEOPLE_WITNESS_STATEMENTS : '#',
       },
       {
         id: 'medical_reports',
         text: getText(taskListItems.medical_reports, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_MEDICAL_REPORTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_MEDICAL_REPORTS : '#',
       },
       {
         id: 'miam_certificate',
         text: getText(taskListItems.miam_certificate, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_MIAM_CERTIFICATE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_MIAM_CERTIFICATE : '#',
       },
       {
         id: 'applications_made_in_these_proceedings',
         text: getText(taskListItems.applications_made_in_these_proceedings, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_APP_MADE_IN_PRCEEDINGS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_APP_MADE_IN_PRCEEDINGS : '#',
       },
       {
         id: 'previous_orders_submitted',
         text: getText(taskListItems.previous_orders_submitted, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_PREVIOUS_ORDERS_SUBMITTED : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_PREVIOUS_ORDERS_SUBMITTED : '#',
       },
       {
         id: 'letters_from_school',
         text: getText(taskListItems.letters_from_school, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_LETTER_FROM_SCHOOL : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_LETTER_FROM_SCHOOL : '#',
       },
       {
         id: 'digital_downloads',
         text: getText(taskListItems.digital_downloads, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_DIGITAL_DOWNLOADS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_DIGITAL_DOWNLOADS : '#',
       },
       {
         id: 'photographic_evidence',
         text: getText(taskListItems.photographic_evidence, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_PHOTOGRAPHIC_EVIDENCE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_PHOTOGRAPHIC_EVIDENCE : '#',
       },
       {
         id: 'mobile_phone_screenshots',
         text: getText(taskListItems.mobile_phone_screenshots, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_MOBILE_SCREENSHOTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_MOBILE_SCREENSHOTS : '#',
       },
       {
         id: 'medical_records',
         text: getText(taskListItems.medical_records, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_MEDICAL_RECORDS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_MEDICAL_RECORDS : '#',
       },
       {
         id: 'paternity_test_reports',
         text: getText(taskListItems.paternity_test_reports, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_PATERNITY_TEST_REPORTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_PATERNITY_TEST_REPORTS : '#',
       },
       {
         id: 'drug_alcohol_tests',
         text: getText(taskListItems.drug_alcohol_tests, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_DRUG_ALCOHOL_TESTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_DRUG_ALCOHOL_TESTS : '#',
       },
       {
         id: 'police_disclosures',
         text: getText(taskListItems.police_disclosures, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_POLICE_DISCLOSURE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_POLICE_DISCLOSURE : '#',
       },
       {
         id: 'witness_availability',
         text: getText(taskListItems.witness_availability, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.APPLICANT_WITNESS_AVAILABILITY : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.APPLICANT_WITNESS_AVAILABILITY : '#',
       },
     ],
   };
@@ -151,7 +151,7 @@ export const getRespondentDocuments = (sectionTitles, taskListItems, userCase) =
       {
         id: 'respondent_response_to_request_for_child_arrangements',
         text: getText(taskListItems.respondent_response_to_request_for_child_arrangements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_CA_RESPONSE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_CA_RESPONSE : '#',
       },
       {
         id: 'respondent_response_to_allegations_of_harm_and_violence',
@@ -164,82 +164,82 @@ export const getRespondentDocuments = (sectionTitles, taskListItems, userCase) =
       {
         id: 'respondent_allegation_of_harm_and_violence',
         text: getText(taskListItems.respondent_allegation_of_harm_and_violence, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_ALLEGATION_OF_HARM_VOILENCE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_ALLEGATION_OF_HARM_VOILENCE : '#',
       },
       {
         id: 'applications_made_in_these_proceedings_respondent',
         text: getText(taskListItems.applications_made_in_these_proceedings_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_APP_MADE_IN_PRCEEDINGS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_APP_MADE_IN_PRCEEDINGS : '#',
       },
       {
         id: 'previous_orders_submitted_respondent',
         text: getText(taskListItems.previous_orders_submitted_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_PREVIOUS_ORDERS_SUBMITTED : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_PREVIOUS_ORDERS_SUBMITTED : '#',
       },
       {
         id: 'letters_from_school_respondent',
         text: getText(taskListItems.letters_from_school_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_LETTER_FROM_SCHOOL : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_LETTER_FROM_SCHOOL : '#',
       },
       {
         id: 'respondent_position_statements',
         text: getText(taskListItems.respondent_position_statements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_POSITION_STATEMENT : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_POSITION_STATEMENT : '#',
       },
       {
         id: 'respondent_witness_statements',
         text: getText(taskListItems.respondent_witness_statements, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_WITNESS_STATEMENTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_WITNESS_STATEMENTS : '#',
       },
       {
         id: 'other_people_witness_statements_respondent',
         text: getText(taskListItems.other_people_witness_statements_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_OTHER_PEOPLE_WITNESS_STATEMENTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_OTHER_PEOPLE_WITNESS_STATEMENTS : '#',
       },
       {
         id: 'digital_downloads_respondent',
         text: getText(taskListItems.digital_downloads_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_DIGITAL_DOWNLOADS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_DIGITAL_DOWNLOADS : '#',
       },
       {
         id: 'photographic_evidence_respondent',
         text: getText(taskListItems.photographic_evidence_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_PHOTOGRAPHIC_EVIDENCE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_PHOTOGRAPHIC_EVIDENCE : '#',
       },
       {
         id: 'mobile_phone_screenshots_respondent',
         text: getText(taskListItems.mobile_phone_screenshots_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_MOBILE_SCREENSHOTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_MOBILE_SCREENSHOTS : '#',
       },
       {
         id: 'medical_records_respondent',
         text: getText(taskListItems.medical_records_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_MEDICAL_RECORDS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_MEDICAL_RECORDS : '#',
       },
       {
         id: 'medical_reports_respondent',
         text: getText(taskListItems.medical_reports_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_MEDICAL_REPORTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_MEDICAL_REPORTS : '#',
       },
       {
         id: 'paternity_test_reports_respondent',
         text: getText(taskListItems.paternity_test_reports_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_PATERNITY_TEST_REPORTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_PATERNITY_TEST_REPORTS : '#',
       },
       {
         id: 'drug_alcohol_tests_respondent',
         text: getText(taskListItems.drug_alcohol_tests_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_DRUG_ALCOHOL_TESTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_DRUG_ALCOHOL_TESTS : '#',
       },
       {
         id: 'police_disclosures_respondent',
         text: getText(taskListItems.police_disclosures_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_POLICE_DISCLOSURE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_POLICE_DISCLOSURE : '#',
       },
       {
         id: 'witness_availability_respondent',
         text: getText(taskListItems.witness_availability_respondent, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_WITNESS_AVAILABILITY : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_WITNESS_AVAILABILITY : '#',
       },
     ],
   };
@@ -252,22 +252,22 @@ export const getCafcassDocuments = (sectionTitles, taskListItems, userCase) => {
       {
         id: 'safeguarding_letter',
         text: getText(taskListItems.safeguarding_letter, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_SAFEGUARDING_LETTER : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_SAFEGUARDING_LETTER : '#',
       },
       {
         id: 'section7_report',
         text: getText(taskListItems.section7_report, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_SECTION7_REPORT : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_SECTION7_REPORT : '#',
       },
       {
         id: 'section37_report',
         text: getText(taskListItems.section37_report, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_SECTION37_REPORT : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_SECTION37_REPORT : '#',
       },
       {
         id: 'risk_assessment_16a',
         text: getText(taskListItems.risk_assessment_16a, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_RISK_ASSESSMENT : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_RISK_ASSESSMENT : '#',
       },
     ],
   };
@@ -280,22 +280,22 @@ export const getOtherDocuments = (sectionTitles, taskListItems, userCase) => {
       {
         id: 'important_address_and_contact_details',
         text: getText(taskListItems.important_address_and_contact_details, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_IMP_ADDRESS_CONTACT_INFO : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_IMP_ADDRESS_CONTACT_INFO : '#',
       },
       {
         id: 'dna_reports',
         text: getText(taskListItems.dna_reports, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_DNA_REPORTS : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_DNA_REPORTS : '#',
       },
       {
         id: 'privacy_notice',
         text: getText(taskListItems.privacy_notice, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_PRIVACY_NOTICE : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_PRIVACY_NOTICE : '#',
       },
       {
         id: 'special_measures',
         text: getText(taskListItems.special_measures, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_SPECIAL_MEASURES : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_SPECIAL_MEASURES : '#',
       },
     ],
   };
@@ -308,12 +308,12 @@ export const getAttendingTheHearingDocs = (sectionTitles, taskListItems, userCas
       {
         id: 'notice_of_hearing',
         text: getText(taskListItems.notice_of_hearing, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_NOTICE_OF_HEARING : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_NOTICE_OF_HEARING : '#',
       },
       {
         id: 'support_you_need_during_your_case',
         text: getText(taskListItems.support_you_need_during_your_case, userCase),
-        href: userCase.allegationOfHarm ? URL.APPLICANT + URL.RESPONDENT_SUPPORT_NEEDED : '#',
+        href: userCase.allegationsOfHarmYesNo ? URL.APPLICANT + URL.RESPONDENT_SUPPORT_NEEDED : '#',
       },
     ],
   };
