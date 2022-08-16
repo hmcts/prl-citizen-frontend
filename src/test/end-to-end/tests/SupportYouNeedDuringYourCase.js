a1c8ebe278337fcc1af6c544c9f1739bee3227c5
Poojitha Nagappa <poojithanag@gmail.com>
2022-08-16 14:43:22 +0100

    Fix Build

--- src/test/end-to-end/tests/SupportYouNeedDuringYourCase.js
+++
@@ -1,7 +0,0 @@
-Feature('International Element');
-
-Scenario('International Element - basic journey @cross-browser', async ({ I }) => {
-  await I.enterPinPageHappyPath();
-  await I.loginAsCitizen();
-  await I.supportYouNeedDuringYourCase();
-}).retry({ retries: 3, minTimeout: 30000 });
