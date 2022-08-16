076b9a2387367ab72bc23fdf6c4968fa26454199
Poojitha Nagappa <poojithanag@gmail.com>
2022-08-16 16:51:02 +0100

    Enable 2 tests

--- src/test/end-to-end/tests/SupportYouNeedDuringYourCaseApplicant.js
+++ src/test/end-to-end/tests/SupportYouNeedDuringYourCaseApplicant.js
@@ -3,5 +3,5 @@ Feature('Support You Need During Your Case- Applicant');
 Scenario('Support You Need During Your Case- Applicant - basic journey @cross-browser', async ({ I }) => {
   await I.enterPinPageHappyPath();
   await I.loginAsCitizen();
-  await I.supportYouNeedDuringYourCaseRespondent();
+  await I.supportYouNeedDuringYourCaseApplicant();
 }).retry({ retries: 3, minTimeout: 30000 });
--- src/test/end-to-end/tests/SupportYouNeedDuringYourCaseRespondent.js
+++ src/test/end-to-end/tests/SupportYouNeedDuringYourCaseRespondent.js
@@ -3,5 +3,5 @@ Feature('Support You Need During Your Case- Respondent');
 Scenario('Support You Need During Your Case- Respondent - basic journey @cross-browser', async ({ I }) => {
   await I.enterPinPageHappyPath();
   await I.loginAsCitizen();
-  //await I.supportYouNeedDuringYourCase();
+  await I.supportYouNeedDuringYourCaseRespondent();
 }).retry({ retries: 3, minTimeout: 30000 });
