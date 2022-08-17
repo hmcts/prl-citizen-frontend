223667172798689f68b1dabe8e92c63174b04582
cshiwarale <chetan.hiwarale@hmcts.net>
2022-08-17 15:25:58 +0100

    Fix for merge conflict issues

---
+++ src/main/steps/applicant/support-you-need-during-case/summary/utils.test.ts
@@ -0,0 +1,98 @@
+import mockUserCase from '../../../../../test/unit/utils/mockUserCase';
+import { CONSENT, MIAM_START, PROCEEDINGS_COURT_PROCEEDINGS, PROCEEDINGS_START } from '../../../urls';
+
+import { summaryList } from './utils';
+
+const enContent = {
+  section: 'Check your details',
+  title: 'Read the information to make sure it is correct, and add any missing details',
+  sectionTitles: {
+    applicationDetails: 'Application details',
+  },
+  keys: {
+    miamStart: 'What is a Mediation Information and Assessment Meeting (MIAM)?',
+    applicationReceivedDate: 'When did you receive the application?',
+    invalidApplicationReceivedDate: 'When did you receive the application?',
+  },
+  fieldType: {
+    miamStart: 'string',
+    applicationReceivedDate: 'Date',
+    invalidApplicationReceivedDate: 'Date',
+    proceedingsStart: 'String',
+    proceedingsStartOrder: 'String',
+    emergencyOrderOptions: 'YesOrNo',
+    'emergencyOrder.caseNoDetails': 'String',
+    'emergencyOrder.orderDateDetails': 'Date',
+    'emergencyOrder.orderTimeDetails': 'String',
+    'emergencyOrder.currentOrderDetails': 'YesOrNo',
+    'emergencyOrder.issueOrderDetails': 'String',
+  },
+  errors: {},
+};
+
+const urls = {
+  miamStart: MIAM_START,
+  proceedingsStart: PROCEEDINGS_START,
+  proceedingsStartOrder: PROCEEDINGS_START,
+  emergencyOrderOptions: PROCEEDINGS_COURT_PROCEEDINGS,
+  applicationReceivedDate: CONSENT,
+  invalidApplicationReceivedDate: CONSENT,
+};
+
+describe('common > summary > utils', () => {
+  describe('SummaryList', () => {
+    test.each([
+      {
+        userCase: mockUserCase,
+        expected: {
+          title: 'applicationDetails',
+          rows: [
+            {
+              actions: {
+                items: [
+                  {
+                    href: '/respondent/miam/miam-start',
+                    text: 'Edit',
+                    visuallyHiddenText: 'What is a Mediation Information and Assessment Meeting (MIAM)?',
+                  },
+                ],
+              },
+              key: { text: 'What is a Mediation Information and Assessment Meeting (MIAM)?' },
+              value: { text: 'Yes' },
+            },
+            {
+              actions: {
+                items: [
+                  {
+                    href: '/respondent/consent-to-application',
+                    text: 'Edit',
+                    visuallyHiddenText: 'When did you receive the application?',
+                  },
+                ],
+              },
+              key: { text: 'When did you receive the application?' },
+              value: { text: '11 March 2022' },
+            },
+            {
+              actions: {
+                items: [
+                  {
+                    href: '/respondent/consent-to-application',
+                    text: 'Edit',
+                    visuallyHiddenText: 'When did you receive the application?',
+                  },
+                ],
+              },
+              key: { text: 'When did you receive the application?' },
+              value: {},
+            },
+          ],
+        },
+      },
+    ])('return correct summary list items when %#', ({ userCase, expected }) => {
+      expect(summaryList(enContent, userCase, urls, 'applicationDetails', enContent.fieldType, 'en')).toStrictEqual(
+        expected
+      );
+    });
+  });
+});
