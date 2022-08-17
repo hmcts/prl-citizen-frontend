96df3743af36f7cef5a0dd3eeec55c1f1a5d5377
swaroopa86 <pendyalaswaroopa86@gmail.com>
2022-08-17 10:11:22 +0100

    additional screen is added for upload docs

--- src/main/steps/respondent/upload-document/success-page/content.ts
+++ src/main/steps/respondent/upload-document/success-page/content.ts
@@ -1,18 +1,16 @@
-import config from 'config';
 import { YesOrNo } from '../../../../app/case/definition';
 import { TranslationFn } from '../../../../app/controller/GetController';
 import { FormContent } from '../../../../app/form/Form';
 
-const email: string = config.get('services.citizen.uploadDocsEmail');
 const en = {
   section: 'How your documents will be shared',
-  email: email,
+  status: 'Your documents have been uploaded',
   continue: 'Continue',
 };
 
 const cy: typeof en = {
   section: 'How your documents will be shared',
-  email: email,
+  status: 'Your documents have been uploaded',
   continue: 'Continue',
 };
 
