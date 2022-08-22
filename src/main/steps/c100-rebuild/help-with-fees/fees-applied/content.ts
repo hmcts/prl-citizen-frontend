770aa3cc7ff3fb77b53d97ab5bcac4100e2779c6
channa-patil <channabasappagouda.patil@hmcts.net>
2022-08-22 23:52:12 +0100

    C100 Rebuild - PRLC100-71,PRLC100-70 Added changes for help with fees screens

---
+++ src/main/steps/c100-rebuild/help-with-fees/fees-applied/content.ts
@@ -0,0 +1,89 @@
+import { YesOrNo } from '../../../../app/case/definition';
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn, isTextAreaValid } from '../../../../app/form/validation';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const en = () => ({
+  feesAppliedDetailsTitle: 'Have you already applied for help with your application fee?',
+  hwfReferenceNumberLabel: 'Enter your help with fees reference number',
+  hwfReferenceNumberHint: `You received this number when you applied for help with fees.<br/>
+  For example, HWF-A1B-23C`,
+  one: 'Yes',
+  two: 'No',
+  errors: {
+    feesAppliedDetails: {
+      required: 'Select yes if the other people in the application know your contact details',
+    },
+    hwfReferenceNumber: {
+      required: 'provide details',
+    },
+  },
+});
+
+const cy = () => ({
+  feesAppliedDetailsTitle: 'Have you already applied for help with your application fee? - welsh',
+  hwfReferenceNumberLabel: 'Enter your help with fees reference number - welsh',
+  hwfReferenceNumberHint: `You received this number when you applied for help with fees - welsh.<br/>
+  For example, HWF-A1B-23C - welsh`,
+  one: 'Yes - welsh',
+  two: 'No - welsh',
+  errors: {
+    feesAppliedDetails: {
+      required: 'Select yes if the other people in the application know your contact details - welsh',
+    },
+    hwfReferenceNumber: {
+      required: 'provide details - welsh',
+    },
+  },
+});
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    feesAppliedDetails: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      section: l => l.section,
+      values: [
+        {
+          label: l => l.one,
+          value: YesOrNo.YES,
+          subFields: {
+            hwfReferenceNumber: {
+              type: 'text',
+              label: l => l.hwfReferenceNumberLabel,
+              labelSize: 'm',
+              hint: l => l.hwfReferenceNumberHint,
+              classes: 'govuk-input--width-10',
+              validator: value => isFieldFilledIn(value) || isTextAreaValid(value),
+            },
+          },
+        },
+        {
+          label: l => l.two,
+          value: YesOrNo.NO,
+        },
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  onlycontinue: {
+    text: l => l.onlycontinue,
+  },
+  saveAndComeLater: {
+    text: l => l.saveAndComeLater,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language]();
+  return {
+    ...translations,
+    form,
+  };
+};
---
+++ src/main/steps/c100-rebuild/help-with-fees/hwf-guidance/content.test.ts
@@ -0,0 +1,85 @@
+import languageAssertions from '../../../../../test/unit/utils/languageAssertions';
+import { FormContent, FormFields, FormOptions, LanguageLookup } from '../../../../app/form/Form';
+import { isFieldFilledIn, isTextAreaValid } from '../../../../app/form/validation';
+import { CommonContent, generatePageContent } from '../../../common/common.content';
+
+import { generateContent } from './content';
+
+jest.mock('../../../../app/form/validation');
+
+const en = {
+  hwfGuidanceTitle: 'You need to apply for help with your child arrangements application fee',
+  hwfApplyLinkHint1: 'You need to',
+  hwfApplyLinkHint2: 'before you continue with this child arrangements application.',
+  applyForHwf: 'apply for help with fees (opens in a new tab)',
+  hwfGuidanceC100Hint: "Enter 'C100' when you are asked for your court or tribunal form number.",
+  hwfGuidanceHint2: `After you have applied for help with fees, you will receive a reference number.
+  Add this reference number below to proceed with the child arrangements application.`,
+  hwfReferenceNumberLabel: 'Enter your help with fees reference number',
+  hwfReferenceNumberHint: 'For example, HWF-A1B-23C',
+  errors: {
+    hwfGuidanceRefNumber: {
+      required: 'provide details',
+    },
+  },
+};
+
+const cy = {
+  hwfGuidanceTitle: 'You need to apply for help with your child arrangements application fee - welsh',
+  hwfApplyLinkHint1: 'You need to - welsh',
+  hwfApplyLinkHint2: 'before you continue with this child arrangements application. - welsh',
+  applyForHwf: 'apply for help with fees (opens in a new tab) - welsh',
+  hwfGuidanceC100Hint: "Enter 'C100' when you are asked for your court or tribunal form number. - welsh",
+  hwfGuidanceHint2: `After you have applied for help with fees, you will receive a reference number.
+  Add this reference number below to proceed with the child arrangements application. - welsh`,
+  hwfReferenceNumberLabel: 'Enter your help with fees reference number - welsh',
+  hwfReferenceNumberHint: 'For example, HWF-A1B-23C - welsh',
+  errors: {
+    hwfGuidanceRefNumber: {
+      required: 'provide details - welsh',
+    },
+  },
+};
+
+/* eslint-disable @typescript-eslint/ban-types */
+describe('help with fess > hwf guidance', () => {
+  const commonContent = { language: 'en', userCase: { applyingWith: 'alone' } } as unknown as CommonContent;
+  let generatedContent;
+  let form;
+  let fields;
+  beforeEach(() => {
+    generatedContent = generateContent(commonContent);
+    form = generatedContent.form as FormContent;
+    fields = form.fields as FormFields;
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  test('should return correct english content', () => {
+    languageAssertions('en', en, () => generateContent(commonContent));
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  test('should return correct welsh content', () => {
+    languageAssertions('cy', cy, () => generateContent({ ...commonContent, language: 'cy' }));
+  });
+
+  test('should contain applyingWith field', () => {
+    const applyingWithField = fields.hwfGuidanceRefNumber as FormOptions;
+    expect(applyingWithField.type).toBe('text');
+    (applyingWithField.validator as Function)('test text');
+    expect(isFieldFilledIn).toHaveBeenCalledWith('test text');
+    expect(isTextAreaValid).toHaveBeenCalledWith('test text');
+  });
+
+  test('should contain onlycontinue button', () => {
+    expect(
+      (form?.onlycontinue?.text as LanguageLookup)(generatePageContent({ language: 'en' }) as Record<string, never>)
+    ).toBe('Continue');
+  });
+
+  test('should contain saveAndComeLater button', () => {
+    expect(
+      (form?.saveAndComeLater?.text as LanguageLookup)(generatePageContent({ language: 'en' }) as Record<string, never>)
+    ).toBe('Save and come back later');
+  });
+});
