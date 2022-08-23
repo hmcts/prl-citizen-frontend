834f00343d9787951366658e748226ffbcea3927
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-08-23 17:07:42 +0100

    route corrected for miam certificate

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -17,21 +17,19 @@ import {
   // CSRF_TOKEN_ERROR_URL,
   ACCESSIBILITY_STATEMENT,
   APPLICANT,
+  APPLICANT_ALLEGATION_OF_HARM_VOILENCE,
   APPLICANT_CA_DA_REQUEST,
   APPLICANT_MIAM_CERTIFICATE,
   APPLICANT_ORDERS_FROM_THE_COURT,
   CONTACT_US,
   COOKIES_PAGE,
   HOME_URL,
-  // KEEP_ALIVE_URL,
   PRIVACY_POLICY,
+  RESPONDENT,
   RESPONDENT_ORDERS_FROM_THE_COURT,
   TERMS_AND_CONDITIONS,
   YOUR_APPLICATION_FL401,
   YOUR_APPLICATION_WITNESS_STATEMENT,
-  // SAVE_AND_SIGN_OUT,
-  // TIMED_OUT_URL,
-  // RESPONDENT_TASK_LIST_URL
 } from './steps/urls';
 
 export class Routes {
@@ -56,7 +54,9 @@ export class Routes {
     app.get(`${APPLICANT_ORDERS_FROM_THE_COURT}/:uid`, errorHandler(documentManagerController.get));
     app.get(`${RESPONDENT_ORDERS_FROM_THE_COURT}/:uid`, errorHandler(documentManagerController.get));
 
-    app.get(APPLICANT_MIAM_CERTIFICATE, errorHandler(documentManagerController.get));
+    app.get(`${APPLICANT}${APPLICANT_MIAM_CERTIFICATE}`, errorHandler(documentManagerController.get));
+    app.get(`${RESPONDENT}${APPLICANT_MIAM_CERTIFICATE}`, errorHandler(documentManagerController.get));
+    app.get(`${APPLICANT}${APPLICANT_ALLEGATION_OF_HARM_VOILENCE}`, errorHandler(documentManagerController.get));
 
     for (const step of stepsWithContent) {
       const files = fs.readdirSync(`${step.stepDir}`);
