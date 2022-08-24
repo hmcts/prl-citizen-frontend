49e11c7d6b39a3439081b29ce6bac98fbc38bd04
channa-patil <channabasappagouda.patil@hmcts.net>
2022-08-24 13:35:19 +0100

    C100 children - corrected test case

--- src/main/steps/c100-rebuild/child-details/add-childern/content.test.ts
+++ src/main/steps/c100-rebuild/child-details/add-childern/content.test.ts
@@ -13,10 +13,10 @@ const en = {
   lastName: 'Last name(s)',
   errors: {
     'firstname-1': {
-      required: 'Select first name',
+      required: 'Enter the first name',
     },
     'lastname-1': {
-      required: 'Select last name',
+      required: 'Enter the last name',
     },
   },
 };
@@ -29,10 +29,10 @@ const cy = {
   lastName: 'Last name(s) - welsh',
   errors: {
     'firstname-1': {
-      required: 'Select first name - welsh',
+      required: 'Enter the first name - welsh',
     },
     'lastname-1': {
-      required: 'Select last name - welsh',
+      required: 'Enter the last name - welsh',
     },
   },
 };
