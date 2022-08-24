e08b400db15fb0ed59558fd70c5b64b8b1e560b7
sangeetha <sangeetha.ponnusamy@hmcts.net>
2022-08-24 15:45:57 +0100

    feat(PRLC100-73):rebase

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -5,7 +5,7 @@ import { C100Sequence } from './c100sequence';
 
 describe('C100Sequence', () => {
   test('should contain 1 entries in c100 screen sequence', () => {
-    expect(C100Sequence).toHaveLength(18);
+    expect(C100Sequence).toHaveLength(19);
     expect(C100Sequence[0].url).toBe('/c100-rebuild/confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
     expect(C100Sequence[0].getNextStep({ detailsKnown: YesOrNo.YES })).toBe(
@@ -199,5 +199,9 @@ describe('C100Sequence', () => {
         disabilityRequirements: ['helpTravellingMovingBuildingSupport', 'communicationHelp'],
       })
     ).toBe('/c100-rebuild/reasonable-adjustments/disability-requirements/communication-help');
+
+    expect(C100Sequence[18].url).toBe('/c100-rebuild/other-proceedings/current-previous-proceedings');
+    expect(C100Sequence[18].showInSection).toBe('c100');
+    expect(C100Sequence[18].getNextStep({})).toBe('/c100-rebuild/other-proceedings/current-previous-proceedings');
   });
 });
