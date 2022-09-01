32a8dd00c93f7e90f6dee1acbd5459e697177876
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-09-01 16:39:30 +0100

    applicant task list test case fixed

--- src/main/steps/applicant/task-list/content.test.ts
+++ src/main/steps/applicant/task-list/content.test.ts
@@ -101,9 +101,9 @@ describe('task-list > content', () => {
         {
           items: [
             {
-              href: '/applicant/yourdocuments/alldocuments/alldocuments',
-              id: 'view-all-documents',
-              status: SectionStatus.READY_TO_VIEW,
+              href: '/applicant/upload-document',
+              id: 'upload-document',
+              status: SectionStatus.TO_DO,
               text: applicant_tasklist_items_en.upload_document,
             },
             {
@@ -125,132 +125,6 @@ describe('task-list > content', () => {
             },
           ],
           title: applicant_en.ordersFromTheCourt,
-        },
-      ],
-    },
-  ])('should generate correct task list %#', ({ userCase, expected }) => {
-    const { sections: taskListItems } = generateContent({ ...commonContent, userCase });
-    expect(taskListItems).toEqual(expected);
-  });
-
-  test.skip.each([
-    {
-      userCase: {
-        ...mockUserCase,
-        orderCollection: [],
-      },
-      expected: [
-        {
-          items: [
-            {
-              href: '/applicant/keep-details-private/details_known',
-              id: 'keep-your-details-private',
-              status: 'TO_DO',
-              text: 'Keep your details private',
-            },
-            {
-              href: '/applicant/confirm-contact-details/checkanswers',
-              id: 'confirm-or-edit-your-contact-details',
-              status: 'TO_DO',
-              text: 'Confirm or edit your contact details',
-            },
-          ],
-          title: 'About you',
-        },
-        {
-          items: [
-            {
-              href: '/applicant/public/docs/FL401-Final-Document.pdf',
-              id: 'your-application',
-              status: 'DOWNLOAD',
-              text: 'Application submitted (PDF)',
-            },
-            {
-              href: '/applicant/public/docs/witness-statement-Final-Document.pdf',
-              id: 'your-application-witness-statment',
-              status: 'DOWNLOAD',
-              text: 'Witness statement (PDF)',
-            },
-          ],
-          title: applicant_en.yourApplication,
-        },
-        {
-          items: [
-            {
-              href: '/applicant/yourdocuments/alldocuments/alldocuments',
-              id: 'view-all-documents',
-              status: 'READY_TO_VIEW',
-              text: 'View all documents',
-            },
-          ],
-          title: 'Your documents',
-        },
-        {
-          items: [
-            {
-              href: '#',
-              id: 'view-all-orders-from-the-court',
-              status: SectionStatus.NOT_AVAILABLE_YET,
-              text: applicant_tasklist_items_en.view_all_orders_from_the_court,
-            },
-          ],
-          title: applicant_en.ordersFromTheCourt,
-        },
-        {
-          items: [
-            {
-              href: '/applicant/keep-details-private/details_known',
-              id: 'application-submitted',
-              text: 'Application submitted (PDF)',
-              status: 'TO_DO',
-            },
-            {
-              href: '/applicant/confirm-contact-details/checkanswers',
-              id: 'witness-statement',
-              text: 'Witness statement (PDF)',
-              status: 'IN_PROGRESS',
-            },
-          ],
-          title: 'Your application',
-        },
-        {
-          items: [
-            {
-              href: '/applicant/keep-details-private/details_known',
-              id: 'check-details-of-your-court-hearings',
-              status: 'TO_DO',
-              text: 'Check details of your court hearings',
-            },
-          ],
-          title: 'Your court hearings',
-        },
-        {
-          items: [
-            {
-              href: '/applicant/keep-details-private/details_known',
-              id: 'upload-document',
-              status: 'TO_DO',
-              text: 'Upload documents',
-            },
-            {
-              href: '/applicant/confirm-contact-details/checkanswers',
-              id: 'view-all-documents',
-              status: 'IN_PROGRESS',
-              text: 'View all documents',
-            },
-          ],
-          title: 'Your documents',
-        },
-        {
-          items: [
-            {
-              href: '/applicant/keep-details-private/details_known',
-              id: 'orders',
-              status: 'TO_DO',
-              text: 'View all orders from the court',
-            },
-          ],
-          title: 'Orders from the court',
         },
       ],
     },
