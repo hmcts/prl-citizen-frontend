a1ff6d2b82003fccef619942803eea31820f5f25
Abdul Daim <abdul.daim@hmcts.net>
2022-09-06 12:01:28 +0100

    added payment charging system

--- src/main/modules/payments/paymentHelper.ts
+++ src/main/modules/payments/paymentHelper.ts
@@ -1,5 +1,6 @@
 import { getTokenFromApi } from '../../app/auth/service/get-service-auth-token';
 import { AppRequest } from '../../app/controller/AppRequest';
+import { PAYMENT_RETURN_URL } from '../../steps/urls';
 
 /* A type that is used to define the shape of the object that is returned from the function. */
 type ISystemCredentailsToApiData = {
@@ -13,9 +14,9 @@ type ISystemCredentailsToApiData = {
 /* The class is used to get the data needed to make a payment to the api */
 export class PaymentHelper {
   SystemCredentailsToApiData = async (req: AppRequest): Promise<ISystemCredentailsToApiData> => {
-    const { id } = req.session.userCase;
+    // const { id } = req.session.userCase;
     const { accessToken } = req.session.user;
-    const returnURL = '/reciever/callback';
+    const returnURL = `${req.protocol}://${req.get('host')}${PAYMENT_RETURN_URL}`;
     let token = '';
     try {
       const ServiceAuthToken = await getTokenFromApi();
@@ -27,8 +28,8 @@ export class PaymentHelper {
       Authorization: accessToken,
       ServiceAuthorization: token,
       returnUrL: returnURL,
-      caseId: id,
-      applicantCaseName: '',
+      caseId: '1656350492135029',
+      applicantCaseName: 'Test',
     };
   };
 }
