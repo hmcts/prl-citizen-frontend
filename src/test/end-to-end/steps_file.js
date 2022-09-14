c48725eef5f24727d951321ca23e60a1c672a7da
Jahnavi <jahnavi1satya@gmail.com>
2022-09-14 08:55:07 +0100

    Update steps_file.js

--- src/test/end-to-end/steps_file.js
+++ src/test/end-to-end/steps_file.js
@@ -5,6 +5,8 @@ const CurrentOrPreviousProceedings = require('./pages/CurrentOrPreviousProceedin
 const ConsentToApplication = require('./pages/ConsentToApplication');
 const SupportYouNeedApplicant = require('./pages/SupportYouNeedDuringYourCase-Applicant');
 const SupportYouNeedRespondent = require('./pages/SupportYouNeedDuringYourCase-RespondentCADA');
+const UploadDocuments = require('./pages/UploadDocuments');
+
 
 module.exports = () => {
   return actor({
@@ -29,6 +31,9 @@ module.exports = () => {
     },
     supportYouNeedDuringYourCaseRespondent() {
       return SupportYouNeedRespondent.supportYouNeedHappyPath();
-    }
+    },
+    uploadDocuments () {
+        return UploadDocuments.clickUploadDocuments();
+      }
   });
 };
