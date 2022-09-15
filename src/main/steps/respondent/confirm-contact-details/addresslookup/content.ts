4aef42e7428c627a77bc965eb4e6296e070dc5d8
abdullahi.afrah <abdullahi.afrah@hmcts.net>
2022-09-15 10:54:28 +0100

    0 errors

--- src/main/steps/respondent/confirm-contact-details/addresslookup/content.ts
+++ src/main/steps/respondent/confirm-contact-details/addresslookup/content.ts
@@ -1,8 +1,7 @@
 import { TranslationFn } from '../../../../app/controller/GetController';
 import { generateContent as contactDetailsGenerateContent } from '../../../common/confirm-contact-details/addresslookup/content';
 
-
- export { form } from '../../../common/confirm-contact-details/addresslookup/content';
+export { form } from '../../../common/confirm-contact-details/addresslookup/content';
 
 export const generateContent: TranslationFn = content => {
   const contactDetailsContent = contactDetailsGenerateContent(content);
