a800f07931dba8e15fc17cb0d8a8cb138ad564d3
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-09-21 14:56:09 +0100

    welsh content added for legal representation

--- src/main/steps/tasklistresponse/legalrepresentation/solicitordirect/content.ts
+++ src/main/steps/tasklistresponse/legalrepresentation/solicitordirect/content.ts
@@ -1,8 +1,28 @@
 import { TranslationFn } from '../../../../app/controller/GetController';
 
-const en = {};
+const en = {
+  title: 'Transfer your case to your legal representative',
+  line1: 'To tranfer your case to your legal representative, provide them with your Case number.',
+  line2:
+    "Once your case is passed to your representative, you won't be able to edit your response. They will handle your case and receive any updates from the court.",
+  listItem: 'Your Case number is: ',
+  warning: 'Warning',
+  line3:
+    'Do not respond to this application yourself if you plan to have a legal representative complete the response.',
+  continue: 'Continue',
+};
 
-const cy: typeof en = {};
+const cy: typeof en = {
+  title: 'Transfer your case to your legal representative - welsh',
+  line1: 'To tranfer your case to your legal representative, provide them with your Case number. - welsh',
+  line2:
+    "Once your case is passed to your representative, you won't be able to edit your response. They will handle your case and receive any updates from the court. - welsh",
+  listItem: 'Your Case number is:  - welsh',
+  warning: 'Warning - welsh',
+  line3:
+    'Do not respond to this application yourself if you plan to have a legal representative complete the response. - welsh',
+  continue: 'Continue',
+};
 
 const languages = {
   en,
--- src/main/steps/tasklistresponse/legalrepresentation/solicitornotdirect/content.ts
+++ src/main/steps/tasklistresponse/legalrepresentation/solicitornotdirect/content.ts
@@ -1,8 +1,36 @@
 import { TranslationFn } from '../../../../app/controller/GetController';
 
-const en = {};
+const en = {
+  title: 'Complete your response',
+  line1: "Complete your response to the applicant's request for child arrangements.",
+  line2: 'Once you have completed all of the main sections, you will be able to review and submit your answers.',
+  medheading: 'Transfer your case to your legal representative',
+  line3: 'Once you have submitted your response, you have the option to pass your case over to a legal representative.',
+  line4:
+    "You will need to provide them with your Case number. Once the case is transferred, you won't be able to edit your response.",
+  listItem: 'Your Case number is: ',
+  warning: 'Warning',
+  respond:
+    'Do not respond to the application yourself, if you plan to have a legal representative complete the response.',
+  continue: 'Continue',
+};
 
-const cy: typeof en = {};
+const cy: typeof en = {
+  title: 'Complete your response - welsh',
+  line1: "Complete your response to the applicant's request for child arrangements. - welsh",
+  line2:
+    'Once you have completed all of the main sections, you will be able to review and submit your answers. - welsh',
+  medheading: 'Transfer your case to your legal representative - welsh',
+  line3:
+    'Once you have submitted your response, you have the option to pass your case over to a legal representative. - welsh',
+  line4:
+    "You will need to provide them with your Case number. Once the case is transferred, you won't be able to edit your response. - welsh",
+  listItem: 'Your Case number is: - welsh',
+  warning: 'Warning - welsh',
+  respond:
+    'Do not respond to the application yourself, if you plan to have a legal representative complete the response. - welsh',
+  continue: 'Continue',
+};
 
 const languages = {
   en,
