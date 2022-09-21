da2d6e9622faa534664b62db35d5190915848805
Aloknath Datta <alokdatta.uit@gmail.com>
2022-09-21 20:09:56 +0100

    Updated the charts file

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -7,7 +7,9 @@ import { get, set } from 'lodash';
 
 export class PropertiesVolume {
   enableFor(app: Application): void {
+    console.log('app.locals.developmentMode::' + app.locals.developmentMode);
     if (!app.locals.developmentMode) {
+      console.log('inside generic env setup');
       propertiesVolume.addTo(config);
       this.setSecret('secrets.prl.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
       this.setSecret('secrets.prl.prl-citizen-frontend-idam-client-secret', 'services.idam.citizenClientSecret');
@@ -18,6 +20,7 @@ export class PropertiesVolume {
       this.setSecret('secrets.prl.system-update-user-password', 'services.idam.systemPassword');
       this.setSecret('secrets.prl.citizen-upload-docs-email', 'services.citizen.uploadDocsEmail');
     } else {
+      console.log('inside develop env setup');
       this.setLocalSecret('prl-citizen-frontend-idam-client-secret', 'services.idam.citizenClientSecret');
       this.setLocalSecret('microservicekey-prl-cos-api', 'services.authProvider.secret');
       this.setLocalSecret('system-update-user-username', 'services.idam.systemUsername');
