7cd343d13cf2a8853b0e41c528591eb31c3a2bde
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-09-22 11:16:00 +0100

    added banners for welsh

--- src/main/steps/respondent/task-list/content.ts
+++ src/main/steps/respondent/task-list/content.ts
@@ -24,43 +24,7 @@ const en = () => ({
   },
   sectionTitles: respondent_en,
   taskListItems: respondent_tasklist_items_en,
-});
-
-const cy = () => ({
-  title: '',
-  statuses: {
-    [SectionStatus.COMPLETED]: 'Wedi cwblhau',
-    [SectionStatus.IN_PROGRESS]: 'Yn mynd rhagddo',
-    [SectionStatus.TO_DO]: 'Heb Ddechrau',
-    [SectionStatus.READY_TO_VIEW]: "barod i'w weld",
-    [SectionStatus.NOT_AVAILABLE_YET]: 'Ddim ar gael eto',
-  },
-  sectionTitles: respondent_cy,
-  taskListItems: respondent_tasklist_items_cy,
-});
-
-const languages = {
-  en,
-  cy,
-};
-
-export const generateContent: TranslationFn = content => {
-  const translations = languages[content.language]();
-  const banners: Banner[] =
-    content.userCase?.caseTypeOfApplication === 'C100'
-      ? getC100Banners(content.userCase)
-      : getFl401Banners(content.userCase);
-  return {
-    ...translations,
-    sections: generateRespondentTaskList(translations.sectionTitles, translations.taskListItems, content.userCase),
-    banners,
-  };
-};
-
-const getC100Banners = userCase => {
-  console.log(userCase.caseTypeOfApplication);
-  const banners: Banner[] = [];
-  banners.push({
+  caRespondentServedBanner: {
     bannerHeading: 'Respond to an application about a child',
     bannerContent: [
       {
@@ -79,8 +43,8 @@ const getC100Banners = userCase => {
         text: 'Respond to the application',
       },
     ],
-  });
-  banners.push({
+  },
+  cafcassBanner: {
     bannerHeading: 'Cafcass will contact you **',
     bannerContent: [
       {
@@ -98,35 +62,133 @@ const getC100Banners = userCase => {
         text: 'Find out about Cafcass Cymru ',
       },
     ],
-  });
+  },
+  daRespondentBanner: {
+    bannerHeading:
+      'You have been named as the respondent in a domestic abuse application and have an order from the court',
+    bannerContent: [
+      {
+        line1:
+          'This means that another person (the applicant) has applied to a court for protection from domestic abuse.',
+        line2: 'The court has considered their concerns. The order tells you what the court has decided.',
+      },
+    ],
+    bannerLinks: [
+      {
+        href: RESPONDENT_ORDERS_FROM_THE_COURT,
+        text: 'Read the order (PDF)',
+      },
+      {
+        href: `${APPLICANT}${APPLICANT_CA_DA_REQUEST}`,
+        text: 'Read the application (PDF)',
+      },
+    ],
+  },
+});
+
+const cy = () => ({
+  title: '',
+  statuses: {
+    [SectionStatus.COMPLETED]: 'Wedi cwblhau',
+    [SectionStatus.IN_PROGRESS]: 'Yn mynd rhagddo',
+    [SectionStatus.TO_DO]: 'Heb Ddechrau',
+    [SectionStatus.READY_TO_VIEW]: "barod i'w weld",
+    [SectionStatus.NOT_AVAILABLE_YET]: 'Ddim ar gael eto',
+  },
+  sectionTitles: respondent_cy,
+  taskListItems: respondent_tasklist_items_cy,
+  caRespondentServedBanner: {
+    bannerHeading: 'Respond to an application about a child',
+    bannerContent: [
+      {
+        line1: 'Another person (the applicant) has applied to the court to make a decision about a child.',
+        line2:
+          'You should respond within 14 days of receiving the application unless the court has asked you to respond sooner.',
+      },
+    ],
+    bannerLinks: [
+      {
+        href: `${APPLICANT}${APPLICANT_CA_DA_REQUEST}`,
+        text: 'Check the application (PDF)',
+      },
+      {
+        href: RESPOND_TO_APPLICATION,
+        text: 'Respond to the application',
+      },
+    ],
+  },
+  cafcassBanner: {
+    bannerHeading: 'Cafcass will contact you **',
+    bannerContent: [
+      {
+        line1:
+          'The Children and Family Court advisory and Support Service (Cafcass or Cafcass Cymru) will contact you to consider the needs of the children.',
+      },
+    ],
+    bannerLinks: [
+      {
+        href: FIND_OUT_ABOUT_CAFCASS,
+        text: 'Find out about Cafcass',
+      },
+      {
+        href: FIND_OUT_ABOUT_CAFCASS_CYMRU,
+        text: 'Find out about Cafcass Cymru ',
+      },
+    ],
+  },
+  daRespondentBanner: {
+    bannerHeading:
+      'You have been named as the respondent in a domestic abuse application and have an order from the court',
+    bannerContent: [
+      {
+        line1:
+          'This means that another person (the applicant) has applied to a court for protection from domestic abuse.',
+        line2: 'The court has considered their concerns. The order tells you what the court has decided.',
+      },
+    ],
+    bannerLinks: [
+      {
+        href: RESPONDENT_ORDERS_FROM_THE_COURT,
+        text: 'Read the order (PDF)',
+      },
+      {
+        href: `${APPLICANT}${APPLICANT_CA_DA_REQUEST}`,
+        text: 'Read the application (PDF)',
+      },
+    ],
+  },
+});
+
+const languages = {
+  en,
+  cy,
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language]();
+  const banners: Banner[] =
+    content.userCase?.caseTypeOfApplication === 'C100'
+      ? getC100Banners(translations)
+      : getFl401Banners(content.userCase, translations);
+  return {
+    ...translations,
+    sections: generateRespondentTaskList(translations.sectionTitles, translations.taskListItems, content.userCase),
+    banners,
+  };
+};
+
+const getC100Banners = translations => {
+  const banners: Banner[] = [];
+  banners.push(translations.caRespondentServedBanner);
+  banners.push(translations.cafcassBanner);
   return banners;
 };
 
-const getFl401Banners = userCase => {
+const getFl401Banners = (userCase, translations) => {
   const banners: Banner[] = [];
   // please add all the banners before this if condition, the following banner is added only if no other is present
   if (banners.length === 0 && userCase.orderWithoutGivingNoticeToRespondent?.orderWithoutGivingNotice === YesOrNo.YES) {
-    banners.push({
-      bannerHeading:
-        'You have been named as the respondent in a domestic abuse application and have an order from the court',
-      bannerContent: [
-        {
-          line1:
-            'This means that another person (the applicant) has applied to a court for protection from domestic abuse.',
-          line2: 'The court has considered their concerns. The order tells you what the court has decided.',
-        },
-      ],
-      bannerLinks: [
-        {
-          href: RESPONDENT_ORDERS_FROM_THE_COURT,
-          text: 'Read the order (PDF)',
-        },
-        {
-          href: `${APPLICANT}${APPLICANT_CA_DA_REQUEST}`,
-          text: 'Read the application (PDF)',
-        },
-      ],
-    });
+    banners.push(translations.daRespondentBanner);
   }
   return banners;
 };
