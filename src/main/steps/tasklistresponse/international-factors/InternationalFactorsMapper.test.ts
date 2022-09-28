dc9eaff5b3d5685a4065d54a5d675b0945e61930
praveenrach <praveen.rachapally@HMCTS.NET>
2022-09-28 13:21:24 +0100

    added unit tests - sonar coverage

--- src/main/steps/tasklistresponse/international-factors/InternationalFactorsMapper.test.ts
+++ src/main/steps/tasklistresponse/international-factors/InternationalFactorsMapper.test.ts
@@ -55,6 +55,51 @@ describe('InternationalFactorsMapper', () => {
     );
   });
 
+  test('Should set international elements all fields Yes with provided values and values from BE', async () => {
+    req.session.user.id = '0c09b130-2eba-4ca8-a910-1f001bac01e7';
+
+    req.session.userCase.start = 'Yes';
+    req.session.userCase.iFactorsStartProvideDetails = 'test_data';
+    req.session.userCase.parents = 'Yes';
+    req.session.userCase.iFactorsParentsProvideDetails = 'test_data1';
+    req.session.userCase.jurisdiction = 'Yes';
+    req.session.userCase.iFactorsJurisdictionProvideDetails = 'test_data2';
+    req.session.userCase.request = 'Yes';
+    req.session.userCase.iFactorsRequestProvideDetails = 'test_data3';
+
+    const response = {
+      citizenInternationalElements: {
+        childrenLiveOutsideOfEnWl: 'Yes',
+        childrenLiveOutsideOfEnWlDetails: 'DUMMY_VALUE1',
+        parentsAnyOneLiveOutsideEnWl: 'No',
+        anotherPersonOrderOutsideEnWl: 'Yes',
+        anotherPersonOrderOutsideEnWlDetails: 'DUMMY_VALUE2',
+        anotherCountryAskedInformation: 'No',
+      },
+    };
+    respondents[0].value.response = response;
+    req.session.userCase.respondents = respondents;
+
+    await setInternationalFactorsDetails(respondents[0], req);
+
+    expect(respondents[0].value.response.citizenInternationalElements.childrenLiveOutsideOfEnWl).toEqual('Yes');
+    expect(respondents[0].value.response.citizenInternationalElements.childrenLiveOutsideOfEnWlDetails).toEqual(
+      'test_data'
+    );
+    expect(respondents[0].value.response.citizenInternationalElements.parentsAnyOneLiveOutsideEnWl).toEqual('Yes');
+    expect(respondents[0].value.response.citizenInternationalElements.parentsAnyOneLiveOutsideEnWlDetails).toEqual(
+      'test_data1'
+    );
+    expect(respondents[0].value.response.citizenInternationalElements.anotherPersonOrderOutsideEnWl).toEqual('Yes');
+    expect(respondents[0].value.response.citizenInternationalElements.anotherPersonOrderOutsideEnWlDetails).toEqual(
+      'test_data2'
+    );
+    expect(respondents[0].value.response.citizenInternationalElements.anotherCountryAskedInformation).toEqual('Yes');
+    expect(respondents[0].value.response.citizenInternationalElements.anotherCountryAskedInformationDetaails).toEqual(
+      'test_data3'
+    );
+  });
+
   test('Should set international elements all fields NO with provided values', async () => {
     req.session.user.id = '0c09b130-2eba-4ca8-a910-1f001bac01e7';
     req.session.userCase.start = 'No';
