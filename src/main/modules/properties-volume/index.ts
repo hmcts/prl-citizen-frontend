4fcbd8a14070aee4fff06bed51028c3c1dd743e4
Aloknath Datta <alokdatta.uit@gmail.com>
2022-10-03 18:41:01 +0100

    trying to fix the error

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -7,41 +7,40 @@ import { get, set } from 'lodash';
 
 export class PropertiesVolume {
   enableFor(app: Application): void {
-    console.log('app.locals.developmentMode::' + app.locals.developmentMode);
     if (!app.locals.developmentMode) {
-      console.log('inside generic env setup');
       propertiesVolume.addTo(config);
       this.setSecret('secrets.prl.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
       this.setSecret('secrets.prl.prl-citizen-frontend-idam-client-secret', 'services.idam.citizenClientSecret');
       this.setSecret('secrets.prl.microservicekey-prl-citizen-frontend', 'services.authProvider.secret');
       this.setSecret('secrets.prl.postcode-lookup-token', 'services.postcodeLookup.token');
       this.setSecret('secrets.prl.prl-cos-idam-client-secret', 'services.idam.cosApiClientSecret');
-      //this.setSecret('secrets.prl.microservicekey-prl-cos-api', 'services.authProvider.secret');
-      this.setSecret('secrets.prl.system-update-user-username', 'services.idam.systemUsername');
-      this.setSecret('secrets.prl.system-update-user-password', 'services.idam.systemPassword');
-      //this.setSecret('secrets.prl.citizen-upload-docs-email', 'services.citizen.uploadDocsEmail');
+      this.setSecret('secrets.prl.microservicekey-prl-cos-api', 'services.authProvider.secret');
+      this.setSecret('secrets.prl.idam-solicitor-username', 'services.idam.systemUsername');
+      this.setSecret('secrets.prl.idam-solicitor-password', 'services.idam.systemPassword');
+      set(config, 'services.case.url', 'https://manage-case.aat.platform.hmcts.net/cases');
     } else {
-      console.log('inside develop env setup');
-      this.setLocalSecret('prl-cos-idam-client-secret', 'services.idam.cosApiClientSecret');
       this.setLocalSecret('prl-citizen-frontend-idam-client-secret', 'services.idam.citizenClientSecret');
+      this.setLocalSecret('microservicekey-prl-citizen-frontend', 'services.authProvider.secret');
+      this.setLocalSecret('prl-cos-idam-client-secret', 'services.idam.cosApiClientSecret');
       this.setLocalSecret('microservicekey-prl-cos-api', 'services.authProvider.secret');
-      this.setLocalSecret('system-update-user-username', 'services.idam.systemUsername');
-      this.setLocalSecret('system-update-user-password', 'services.idam.systemPassword');
-      //this.setLocalEndpoints();
+      this.setLocalSecret('idam-solicitor-username', 'services.idam.systemUsername');
+      this.setLocalSecret('idam-solicitor-password', 'services.idam.systemPassword');
+      //this.setLocalSecret('adoption-pcq-token', 'services.equalityAndDiversity.tokenKey');
+      this.setSecret('secrets.prl.system-update-user-username', 'services.idam.systemUsername');
+      this.setSecret('secrets.prl.system-update-user-username', 'services.idam.systemPassword');
+      this.setSecret('secrets.prl.postcode-lookup-token', 'services.postcodeLookup.token');
+      this.setLocalSecret('microservicekey-prl-cos-api', 'services.authProvider.secret');
+      this.setLocalSecret('idam-solicitor-username', 'services.idam.systemUsername');
+      this.setLocalSecret('idam-solicitor-password', 'services.idam.systemPassword');
+      // this.setSecret('secrets.prl.system-update-user-username', 'services.idam.systemUsername');
+      // this.setSecret('secrets.prl.system-update-user-username', 'services.idam.systemPassword');
+      // this.setLocalEndpoints();
     }
   }
 
   private setSecret(fromPath: string, toPath: string): void {
-    console.log('***** setting values *********');
-    console.log('fromPath is ' + fromPath);
     if (config.has(fromPath)) {
-      console.log('config is available ' + config.get(fromPath));
-      console.log('fromPath is available ' + fromPath);
-      console.log('toPath is available ' + toPath);
-      console.log('get(config, fromPath) ' + get(config, fromPath));
       set(config, toPath, get(config, fromPath));
-      console.log('validating set ' + config.get(toPath));
-      console.log('its a success');
     }
   }
 
@@ -50,7 +49,6 @@ export class PropertiesVolume {
    */
   private setLocalSecret(secret: string, toPath: string): void {
     const result = execSync(`az keyvault secret show --vault-name prl-aat -o tsv --query value --name ${secret}`);
-    console.log('******* config is ' + config);
     set(config, toPath, result.toString().replace('\n', ''));
   }
 }
