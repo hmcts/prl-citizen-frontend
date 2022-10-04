4980a6cbba0c89192b9e860907a63fc3b85d5883
Abdul Daim <abdul.daim@hmcts.net>
2022-10-04 12:09:07 +0100

    childern details refcatoring

--- src/main/steps/c100-rebuild/child-details/parental-responsibility/content.ts
+++ src/main/steps/c100-rebuild/child-details/parental-responsibility/content.ts
@@ -1,13 +1,15 @@
+import { childrenDetails } from '../../../../app/controller/AppRequest';
 import { TranslationFn } from '../../../../app/controller/GetController';
 import { FormContent } from '../../../../app/form/Form';
-//import { isFieldFilledIn } from '../../../../app/form/validation';
+import { isFieldFilledIn } from '../../../../app/form/validation';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const en = () => ({
   pageTitle: 'Parental responsibility for',
-  labelText: 'State everyone who has parental responsibility for  and how they have parental responsibility.',
-  hintText: `<p>For example 'child's mother', or 'child's father who was married to the mother when the child was born.</p>
- <p><a target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">See section E of leaflet CB1 for more information</a></p>`,
+  labelText:
+    '<label class="govuk-label" for="parentalResponsibility"> State everyone who has parental responsibility for  and how they have parental responsibility.</label>',
+  hintText: `<div id="parentalResponsibility-hint" class="govuk-hint "><p>For example 'child's mother', or 'child's father who was married to the mother when the child was born.</p>
+ <p><a target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">See section E of leaflet CB1 for more information</a></p></div>`,
   errors: {
     parentalResponsibility: {
       required: 'Enter an answer',
@@ -17,9 +19,10 @@ const en = () => ({
 
 const cy = () => ({
   pageTitle: 'Parental responsibility for  - welsh',
-  labelText: 'State everyone who has parental responsibility for  and how they have parental responsibility. - welsh',
-  hintText: `<p>For example 'child's mother', or 'child's father who was married to the mother when the child was born.</p>
-  <p><a target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">See section E of leaflet CB1 for more information</a></p> - welsh`,
+  labelText:
+    '<label class="govuk-label" for="parentalResponsibility"> State everyone who has parental responsibility for  and how they have parental responsibility. - welsh</label',
+  hintText: `<div id="parentalResponsibility-hint" class="govuk-hint "><p>For example 'child's mother', or 'child's father who was married to the mother when the child was born.</p>
+  <p><a target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">See section E of leaflet CB1 for more information</a></p> - welsh</div>`,
   errors: {
     parentalResponsibility: {
       required: 'Enter an answer  - welsh',
@@ -33,11 +36,45 @@ const languages = {
 };
 
 export const form: FormContent = {
-  fields: {},
+  fields: {
+    childCaption: {
+      type: 'html',
+      label: label => label.labelText,
+    },
+    childhintText: {
+      type: 'html',
+      label: label => label.hintText,
+    },
+    parentalResponsibility: {
+      type: 'input',
+      classes: 'govuk-input govuk-!-margin-top-4',
+      validator: isFieldFilledIn,
+    },
+  },
+  submit: {
+    text: l => l.onlycontinue,
+  },
+  saveAndComeLater: {
+    text: l => l.saveAndComeLater,
+  },
 };
 
 export const generateContent: TranslationFn = content => {
+  const sessionDataOfChildren = content.additionalData?.req.session;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const ChilrenData: childrenDetails[] =
+    sessionDataOfChildren['userCase'].hasOwnProperty('children') && sessionDataOfChildren['userCase']['children']
+      ? sessionDataOfChildren['userCase']['children']
+      : ([] as childrenDetails[]);
+  const { childId } = content.additionalData?.req.query;
+  const findChildData = ChilrenData.filter(child => child['id'] === childId)?.[0];
+  const parentialResponsibilityStatement = findChildData['parentialResponsibility']?.['statement'];
+  form.fields['parentalResponsibility']['value'] = parentialResponsibilityStatement;
   const translations = languages[content.language]();
+  const firstName = findChildData['firstname'];
+  const lastName = findChildData['lastname'];
+  translations['firstName'] = firstName;
+  translations['lastName'] = lastName;
   return {
     ...translations,
     form,
--- src/main/steps/c100-rebuild/child-details/parental-responsibility/postController.ts
+++ src/main/steps/c100-rebuild/child-details/parental-responsibility/postController.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import autobind from 'autobind-decorator';
 import { Response } from 'express';
 
+import { Case } from '../../../../app/case/case';
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../app/controller/PostController';
 import { Form, FormFields, FormFieldsFn } from '../../../../app/form/Form';
@@ -20,38 +22,40 @@ export default class ParentResponsibility extends PostController<AnyObject> {
     const form = new Form(<FormFields>this.fields);
     const { saveAndSignOut, saveBeforeSessionTimeout, _csrf, ...formData } = form.getParsedBody(req.body);
     req.session.errors = form.getErrors(formData);
+    this.errorsAndRedirect(req, res, formData, form);
 
-    if (req.query.hasOwnProperty('childId')) {
+    if (req.query.hasOwnProperty('childId') && req.session.userCase.children) {
       const { childId } = req.query;
-      const currentChild = req.session.settings.ListOfChild.findIndex(childWithId => childWithId.id === childId);
+      const currentChild = req.session.userCase.children.findIndex(childWithId => childWithId.id === childId);
       if (currentChild > -1) {
-        if (req.body.parentalResponsibility === '') {
-          req.session.errors.push({
-            propertyName: 'parentalResponsibility',
-            errorType: 'required',
-          });
-          req.session.settings.ListOfChild[currentChild].parentialResponsibility = {
-            statement: '',
-          };
-          const redirectUrl = C100_children_DETAILS_PARENTIAL_RESPONSIBILITY + `?childId=${childId}`;
-          super.redirect(req, res, redirectUrl);
+        req.session.userCase.children[currentChild].parentialResponsibility = {
+          statement: req.body.parentalResponsibility,
+        };
+        if (currentChild + 1 >= req.session.userCase.children.length) {
+          super.redirect(req, res, C100_children_FURTHER_INFORMATION);
         } else {
-          req.session.settings.ListOfChild[currentChild].parentialResponsibility = {
-            statement: req.body.parentalResponsibility,
-          };
-          if (currentChild + 1 >= req.session.settings.ListOfChild.length) {
-            super.redirect(req, res, C100_children_FURTHER_INFORMATION);
-          } else {
-            const nextChildId = req.session.settings['ListOfChild'][currentChild + 1];
-            const redirectUrl = C100_children_DETAILS_PERSONAL_DETAILS + `?childId=${nextChildId.id}`;
-            super.redirect(req, res, redirectUrl);
-          }
+          const nextChildId = req.session.userCase['children'][currentChild + 1];
+          const redirectUrl = C100_children_DETAILS_PERSONAL_DETAILS + `?childId=${nextChildId.id}`;
+          super.redirect(req, res, redirectUrl);
         }
       } else {
         res.render('error');
       }
     } else {
       res.render('error');
+    }
+  }
+  protected errorsAndRedirect(req: AppRequest<AnyObject>, res: Response, formData: Partial<Case>, form: Form) {
+    req.session.errors = form.getErrors(formData);
+    if (req.session.errors.length) {
+      const { childId } = req.query;
+      const currentChild = req.session.userCase.children
+        ? req.session.userCase.children.findIndex(childWithId => childWithId.id === childId)
+        : 0;
+
+      console.log(currentChild);
+      const redirectUri = C100_children_DETAILS_PARENTIAL_RESPONSIBILITY + `?childId=${childId}`;
+      return super.redirect(req, res, redirectUri);
     }
   }
 }
