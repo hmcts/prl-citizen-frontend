986caca8746c0e09787dedb68f1786fa18c40b6b
sangeetha <sangeetha.ponnusamy@hmcts.net>
2022-10-10 17:49:35 +0100

    fix(PRLC100-217):test coverage improvement

--- jest.config.js
+++ jest.config.js
@@ -10,15 +10,4 @@ module.exports = {
   moduleNameMapper: {
     '@hmcts/nodejs-logging': '<rootDir>/src/test/unit/mocks/hmcts/nodejs-logging',
   },
-  coverageThreshold: {
-
-    "src/**/c100-rebuild/**": {
-
-      "functions": 95,
-
-      "lines": 95
-
-    }
-
-  },
 };
