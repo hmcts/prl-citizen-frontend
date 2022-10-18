0b659a2b5b4ae4e72a38eb7aedeeed69b749e655
vivek-sekhar <111058132+vivek-sekhar@users.noreply.github.com>
2022-10-18 17:31:00 +0100

    isDateOfBirthUnknown field size made smaller

--- src/main/steps/c100-rebuild/child-details/personal-details/content.ts
+++ src/main/steps/c100-rebuild/child-details/personal-details/content.ts
@@ -152,6 +152,7 @@ export const generateFormFields = (personalDetails: ChildrenDetails['personalDet
     },
     isDateOfBirthUnknown: {
       type: 'checkboxes',
+      classes: 'govuk-checkboxes--small',
       values: [
         {
           name: 'isDateOfBirthUnknown',
