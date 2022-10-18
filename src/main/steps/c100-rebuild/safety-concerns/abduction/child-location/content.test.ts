d2c644ce9440d165e9d42c59cb08e0d339253a5d
ShakiraKP <103694861+ShakiraKP@users.noreply.github.com>
2022-10-18 10:30:56 +0100

    add welsh flag

--- src/main/steps/c100-rebuild/safety-concerns/abduction/child-location/content.test.ts
+++ src/main/steps/c100-rebuild/safety-concerns/abduction/child-location/content.test.ts
@@ -30,24 +30,25 @@ const en = {
 };
 
 const cy = {
-  section: 'Safety concerns',
-  title: 'Why do you think the children may be abducted or kept outside the UK without your consent?',
+  section: 'Safety concerns - welsh',
+  title: 'Why do you think the children may be abducted or kept outside the UK without your consent? - welsh',
   warningText: {
-    text: 'Contact the police or social services if a child you’re responsible for is at risk of being taken out of the UK without your consent.',
-    iconFallbackText: 'Warning',
+    text: 'Contact the police or social services if a child you’re responsible for is at risk of being taken out of the UK without your consent. - welsh',
+    iconFallbackText: 'Warning - welsh',
   },
-  safetyConcernsText: 'Briefly explain your concerns, including:',
-  safetyConcernsBullet1: 'Who might take them',
-  safetyConcernsBullet2: 'Where they might be taken or kept',
-  childsCurrentLocationText: 'Where are the children now?',
+  safetyConcernsText: 'Briefly explain your concerns, including: - welsh',
+  safetyConcernsBullet1: 'Who might take them - welsh',
+  safetyConcernsBullet2: 'Where they might be taken or kept - welsh',
+  childsCurrentLocationText: 'Where are the children now? - welsh',
   childsCurrentLocationHint:
-    'If they’re outside England or Wales, include what country they’re in and how long they’ve been there. You don’t need to include any addresses.',
+    'If they’re outside England or Wales, include what country they’re in and how long they’ve been there. You don’t need to include any addresses. - welsh',
   errors: {
     c1A_abductionReasonOutsideUk: {
-      required: 'Explain why you think the children may be abducted or kept outside of the UK without your consent',
+      required:
+        'Explain why you think the children may be abducted or kept outside of the UK without your consent - welsh',
     },
     c1A_childsCurrentLocation: {
-      required: 'Describe where the children are now',
+      required: 'Describe where the children are now - welsh',
     },
   },
 };
