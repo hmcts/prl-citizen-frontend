15e42b4d842801ab365b415dd7e8c1bd48f9fe83
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-10-19 11:12:03 +0100

    Updated get case controller under controllers

--- src/main/app/controller/GetCaseController.ts
+++ src/main/app/controller/GetCaseController.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 
-import { APPLICANT_TASK_LIST_URL } from '../../steps/urls';
-import { getSystemUser } from '../auth/user/oidc';
+import { CaseWithId } from '../../app/case/case';
+import { APPLICANT_TASK_LIST_URL, RESPONDENT_TASK_LIST_URL } from '../../steps/urls';
 import { CosApiClient } from '../case/CosApiClient';
 
 import { AppRequest } from './AppRequest';
@@ -9,14 +9,24 @@ import { AppRequest } from './AppRequest';
 export class GetCaseController {
   //constructor(protected readonly view: string, protected readonly content: TranslationFn) {}
 
-  public async getCase(req: AppRequest, res: Response): Promise<void> {
+  public async getApplicantCase(req: AppRequest, res: Response): Promise<void> {
+    req.session.userCase = await this.assignUserCase(req);
+    req.session.save(() => res.redirect(APPLICANT_TASK_LIST_URL));
+  }
+
+  public async getRespondentCase(req: AppRequest, res: Response): Promise<void> {
+    req.session.userCase = await this.assignUserCase(req);
+    req.session.save(() => res.redirect(RESPONDENT_TASK_LIST_URL));
+  }
+
+  private async assignUserCase(req: AppRequest): Promise<CaseWithId> {
     if (req.params?.caseId) {
-      const caseworkerUser = await getSystemUser();
+      const caseworkerUser = req.session.user;
       const caseReference = req.params?.caseId;
       const client = new CosApiClient(caseworkerUser.accessToken, 'https://return-url');
       const caseDataFromCos = await client.retrieveByCaseId(caseReference, caseworkerUser);
       req.session.userCase = caseDataFromCos;
     }
-    req.session.save(() => res.redirect(APPLICANT_TASK_LIST_URL));
+    return req.session.userCase;
   }
 }
