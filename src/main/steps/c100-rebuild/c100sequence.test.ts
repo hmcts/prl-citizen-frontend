31e9dacf63039f5ca9f94a24fb17d0d2931ffc18
ShakiraKP <103694861+ShakiraKP@users.noreply.github.com>
2022-10-19 23:50:45 +0100

    remove duplicate in urls.ts

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -103,7 +103,6 @@ const childrenMockData = mockRequest({
 
 describe('C100Sequence', () => {
   test('should contain 1 entries in c100 screen sequence', () => {
-
     expect(C100Sequence).toHaveLength(84);
     expect(C100Sequence[0].url).toBe('/c100-rebuild/confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -378,10 +378,8 @@ export const C100_HEARING_URGENCY_URGENT: PageLink = `${C100_HEARING_URGENCY}/ur
 export const C100_HEARING_URGENCY_URGENT_DETAILS: PageLink = `${C100_HEARING_URGENCY}/urgent-details`;
 
 /**@C100 Screening Questions */
-export const C100_SCREENING_QUESTIONS: PageLink = `${C100_URL}/screening-questions`;
 export const C100_SCREENING_QUESTIONS_CONSENT_AGREEMENT: PageLink = `${C100_SCREENING_QUESTIONS}/consent-agreement`;
 export const C100_SCREENING_QUESTIONS_ALTERNATIVE_SOLUTIONS: PageLink = `${C100_SCREENING_QUESTIONS}/alternative-resolution/going-to-court`;
 export const C100_SCREENING_QUESTIONS_LEGAL_RESPRESENTATION: PageLink = `${C100_SCREENING_QUESTIONS}/legal-representation`;
 export const C100_SCREENING_QUESTIONS_PERMISSIONS_REQUEST: PageLink = `${C100_SCREENING_QUESTIONS}/permissions-request`;
 export const C100_SCREENING_QUESTIONS_PERMISSIONS_WHY: PageLink = `${C100_SCREENING_QUESTIONS}/permissions-why`;
-
