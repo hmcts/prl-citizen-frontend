086abb8a0410d0ba963a3e9daa65e590c729b660
ShakiraKP <103694861+ShakiraKP@users.noreply.github.com>
2022-10-19 17:22:52 +0100

    update label text

--- src/main/steps/c100-rebuild/screening-questions/permission/content.ts
+++ src/main/steps/c100-rebuild/screening-questions/permission/content.ts
@@ -4,16 +4,13 @@ import { FormContent } from '../../../../app/form/Form';
 import { isFieldFilledIn } from '../../../../app/form/validation';
 
 const en = () => ({
-  title: 'Is there any reason why you would need permission from the court to make this application?',
+  title: 'Is there any reason that you would need permission from the court to make this application?',
   paragraphsOne: [
     'In some cases, the court will need to give permission before the application can be made.',
     'You will not need permission if you are the:',
   ],
   bulletPoints: ['parent', 'guardian', 'special guardian'],
   paragraphsTwo: [
-    'You may, or may not need permission from the court.',
-    'For example, you may need permission if you are in the children’s lives, but not their parent or guardian.',
-    'You will need the court’s permission if you are one of the children, and the application is about you.',
     'If you are not sure if you need permission, <a  target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">see Section C of the Form CB1 guidance on GOV.UK</a>',
   ],
   one: 'Yes',
@@ -27,16 +24,13 @@ const en = () => ({
 });
 
 const cy = () => ({
-  title: 'Is there any reason why you would need permission from the court to make this application? - welsh',
+  title: 'Is there any reason that you would need permission from the court to make this application? - welsh',
   paragraphsOne: [
     'In some cases, the court will need to give permission before the application can be made. - welsh',
     'You will not need permission if you are the: - welsh',
   ],
   bulletPoints: ['parent - welsh', 'guardian - welsh', 'special guardian - welsh'],
   paragraphsTwo: [
-    'You may, or may not need permission from the court. - welsh',
-    'For example, you may need permission if you are in the children’s lives, but not their parent or guardian. - welsh',
-    'You will need the court’s permission if you are one of the children, and the application is about you. - welsh',
     'If you are not sure if you need permission, <a  target="_blank" href="https://www.gov.uk/government/publications/family-court-applications-that-involve-children-cb1">see Section C of the Form CB1 guidance on GOV.UK</a> - welsh',
   ],
   one: 'Yes - welsh',
