0482a61a71b233a3118e2bb0552ddf4d861c5c3d
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2022-10-24 11:54:01 +0100

    added other-proceeding screen layout and added data

--- src/main/steps/c100-rebuild/check-your-answers/util/currentandprevious-proceedings.util.ts
+++ src/main/steps/c100-rebuild/check-your-answers/util/currentandprevious-proceedings.util.ts
@@ -1,30 +1,50 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export const DATE_FORMATTOR = (date): string => {
+  if (date['year'] !== '' && date['month'] !== '' && date['day'] !== '') {
+    const formated_Date = new Date(date['year'], date['month'], date['day']);
+    const month = formated_Date.toLocaleString('default', { month: 'long' });
+    return formated_Date.getDate() + ' ' + month + ' ' + formated_Date.getFullYear();
+  } else {
+    return '';
+  }
+};
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export const IndividualOrderFieldsParser = (keys, order) => {
-  console.log(order);
+  delete order['id'];
   const Mapper = {
     ['orderDetail']: {
-      val: keys['courtIssuedLabel'],
+      question: keys['courtIssuedLabel'],
     },
     ['caseNo']: {
-      val: keys['caseNumberLabel'],
+      question: keys['caseNumberLabel'],
     },
     ['orderDate']: {
-      val: keys['orderDateLabel'],
+      question: keys['orderDateLabel'],
     },
     ['currentOrder']: {
-      val: keys['isCurrentOrderLabel'],
+      question: keys['isCurrentOrderLabel'],
     },
     ['orderCopy']: {
-      val: keys['copyOfOrderLabel'],
+      question: keys['copyOfOrderLabel'],
+    },
+    ['orderEndDate']: {
+      question: keys['orderEndDateLabel'],
     },
   };
   let Val = '';
   Object.entries(order).forEach(entry => {
     const key = entry[0];
     const value = entry[1];
-    const keyDetails = '<h4>' + Mapper[key]?.val + '</h4>';
-    const valueDetails = '<p>' + value + '</p>';
-    Val += keyDetails + valueDetails;
+    if (typeof entry[1] === 'object' && entry[1] !== null) {
+      const keyDetails = '<h4>' + Mapper[key]?.question + '</h4>';
+      const valueDetails = '<p>' + DATE_FORMATTOR(value) + '</p>';
+      Val += keyDetails + valueDetails;
+    } else {
+      const keyDetails = '<h4>' + Mapper[key]?.question + '</h4>';
+      const valueDetails = '<p>' + value + '</p>';
+      Val += keyDetails + valueDetails;
+    }
   });
 
   return Val;
@@ -38,10 +58,11 @@ export const OPotherProceedingsSessionParserUtil = (UserCase, keys, URLS, sessio
     if (UserCase['op_otherProceedings']?.['order'].hasOwnProperty(`${order}s`)) {
       const orderDetails = UserCase['op_otherProceedings']?.['order'][`${order}s`];
       orderDetails.forEach((nestedOrder, index) => {
+        const IndexNumber = index > 0 ? index + 1 : '';
         orderSessionStorage.push({
-          key: `${keys[`${order}Label`]} ${index + 1}`,
+          key: `${keys[`${order}Label`]} ${IndexNumber}`,
           valueHtml: IndividualOrderFieldsParser(keys, nestedOrder),
-          changeUrl: '',
+          changeUrl: URLS['C100_OTHER_PROCEEDINGS_ORDER_DETAILS'] + `?orderType=${order}`,
         });
       });
     }
