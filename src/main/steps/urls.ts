ffd00d0cb5d42615ce2a0636fdfa2885ce20be0e
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-10-25 23:16:37 +0100

    removed duplicates after merge from base branch

--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -374,7 +374,6 @@ export const C100_HEARING_URGENCY_URGENT_DETAILS: PageLink = `${C100_HEARING_URG
 
 /**@C100 Screening Questions */
 export const C100_SCREENING_QUESTIONS: PageLink = `${C100_URL}/screening-questions`;
-export const C100_SCREENING_QUESTIONS_ALTERNATIVE_ROUTES: PageLink = `${C100_SCREENING_QUESTIONS}/alternative-routes`;
 export const C100_SCREENING_QUESTIONS_CONSENT_AGREEMENT: PageLink = `${C100_SCREENING_QUESTIONS}/consent-agreement`;
 export const C100_SCREENING_QUESTIONS_ALTERNATIVE_RESOLUTION: PageLink = `${C100_SCREENING_QUESTIONS}/alternative-resolution`;
 export const C100_SCREENING_QUESTIONS_LEGAL_RESPRESENTATION: PageLink = `${C100_SCREENING_QUESTIONS}/legal-representation`;
