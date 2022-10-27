8ed69d3c979abb58446bb65c51675d62cb83189c
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-10-27 16:30:10 +0100

    PRLC100-37 Other Person in the Case

--- src/main/steps/c100-rebuild/other-person-details/navigationController.ts
+++ src/main/steps/c100-rebuild/other-person-details/navigationController.ts
@@ -1,22 +1,19 @@
 import { Case } from '../../../app/case/case';
 import { C100RebuildPartyDetails } from '../../../app/case/definition';
 import { applyParms } from '../../common/url-parser';
-import {
-    C100_OTHER_PERSON_DETAILS_ADD,
-  PageLink,
-} from '../../urls';
+import { C100_OTHER_PERSON_DETAILS_ADD, PageLink } from '../../urls';
 
 class OtherPersonsDetailsNavigationController {
   private otherPersonsDetails: C100RebuildPartyDetails[] | [] = [];
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public getNextUrl(currentPageUrl: PageLink, caseData: Partial<Case>, params?: Record<string, any>): PageLink {
-    this.otherPersonsDetails = caseData?.cd_children as C100RebuildPartyDetails[];
+    this.otherPersonsDetails = caseData?.oprs_otherPersons as C100RebuildPartyDetails[];
     let nextUrl;
 
     switch (currentPageUrl) {
       case C100_OTHER_PERSON_DETAILS_ADD: {
-        nextUrl = applyParms(C100_OTHER_PERSON_DETAILS_ADD, { otherPersonId : this.otherPersonsDetails[0].id });
+        nextUrl = applyParms(C100_OTHER_PERSON_DETAILS_ADD, { otherPersonId: this.otherPersonsDetails[0].id });
         break;
       }
       default: {
