b1c1a249b62848cac1b28a8ace6148984d065473
sealDebolina <debolina.seal@hmcts.net>
2022-10-28 12:20:32 +0100

    Welsh translation

--- src/main/steps/c100-rebuild/hearing-without-notice/hearing-part1/content.ts
+++ src/main/steps/c100-rebuild/hearing-without-notice/hearing-part1/content.ts
@@ -22,15 +22,15 @@ const en = () => ({
 });
 
 const cy = () => ({
-  title: 'Are you asking for a without notice hearing? - welsh',
+  title: "Ydych chi'n gofyn am wrandawiad heb rybudd?",
   line1:
-    'Hearings which take place without notice to the other people will only be justified where your case is exceptionally urgent, or there is good reason not to tell the other people about your application (either because they could take steps to obstruct the application or because doing so may expose you or the children to a risk of harm). - welsh',
+    "Bydd gwrandawiadau sy'n digwydd heb rybudd i'r bobl eraill ond yn cael eu cyfiawnhau lle mae brys eithriadol ynglŷn â'ch achos, neu fod rheswm da dros beidio â dweud wrth y bobl eraill am eich cais (naill ai oherwydd y gallent gymryd camau i rwystro'r cais neu oherwydd y gallai gwneud hynny beri niwed i chi neu’r plant).",
   warningText: {
-    text: 'If you ask for a without notice hearing, the court may require you to attend on the same day as you submit your application. They will contact you to tell you when you need to go to the court. - welsh',
+    text: "Os gofynnwch am wrandawiad heb rybudd, efallai y bydd y llys yn gofyn i chi fynychu ar yr un diwrnod ag y byddwch yn cyflwyno eich cais. Byddant yn cysylltu â chi i ddweud pryd y bydd angen i chi fynd i'r llys.",
     iconFallbackText: 'Warning',
   },
-  one: 'Yes - welsh',
-  two: 'No - welsh',
+  one: 'Ydw',
+  two: 'Nac ydw',
   errors: {
     hwn_hearingPart1: {
       required: "Select yes if you're asking for a without notice - welsh",
--- src/main/steps/c100-rebuild/hearing-without-notice/hearing-part2/content.ts
+++ src/main/steps/c100-rebuild/hearing-without-notice/hearing-part2/content.ts
@@ -37,18 +37,18 @@ const en = () => ({
 });
 
 const cy = () => ({
-  title: 'Details of without notice hearing - welsh',
-  line1: 'Give details of why you’re asking for a without notice hearing - welsh',
-  hint: 'A judge will need to be sure that there is a good reason why the other people in the application should not be told about the application before the hearing takes place. - welsh',
+  title: 'Manylion y gwrandawiad heb rybudd',
+  line1: 'Rhowch fanylion pam rydych yn gofyn am wrandawiad heb rybudd',
+  hint: "Bydd angen i farnwr fod yn sicr bod rheswm da na ddylid dweud wrth bobl eraill sydd ynghlwm â’r cais am y cais cyn i'r gwrandawiad ddigwydd.",
   doYouNeedAWithoutNoticeHearingLabel:
-    'Are you asking for a without notice hearing because the other person or people may do something that would obstruct the order you are asking for if they knew about the application? - welsh',
+    "Ydych chi'n gofyn am wrandawiad heb rybudd am gall yr unigolyn neu unigolion arall / eraill wneud rhywbeth a fyddai'n rhwystro'r gorchymyn rydych chi'n gofyn amdano pe bydden nhw yn gwybod am y cais?",
   doYouRequireAHearingWithReducedNoticeLabel:
-    'Are you asking for a without notice hearing because there is literally no time to give notice of the application to the other person or people? - welsh',
+    "Ydych chi'n gofyn am wrandawiad heb rybudd oherwydd, yn llythrennol, nad oes amser i chi roi hysbysiad o gais i'r unigolyn arall / unigolion eraill?",
   doYouRequireAHearingWithReducedNoticeHint:
-    'This may be relevant in cases of exceptional urgency where the order is needed to prevent a threatened wrongful act. In some cases you may still be expected to have tried to give informal notice for example by telephone, text message, or email. - welsh',
-  one: 'Yes - welsh',
-  two: 'No - welsh',
-  provideDetails: 'Provide details -welsh',
+    'Gall hyn fod yn berthnasol mewn achosion lle mae brys eithriadol a lle mae angen y gorchymyn i atal drwgweithred y bygythiwyd ei chyflawni. Mewn rhai achosion efallai y bydd disgwyl i chi fod wedi ceisio rhoi rhybudd anffurfiol er enghraifft dros y ffôn, neges destun, neu e-bost.',
+  one: 'Ydw',
+  two: 'Nac ydw',
+  provideDetails: 'Rhowch fanylion',
   errors: {
     hwn_reasonsForApplicationWithoutNotice: {
       required: 'Enter details - welsh',
