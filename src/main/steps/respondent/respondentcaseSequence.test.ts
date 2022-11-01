30b404662170d0aeb6866e8335538f296ad741fb
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2022-11-01 21:18:34 +0000

    removed unnecessary comments from respondentseq

--- src/main/steps/respondent/respondentcaseSequence.test.ts
+++ src/main/steps/respondent/respondentcaseSequence.test.ts
@@ -11,75 +11,75 @@ describe('respondent1Sequence', () => {
     expect(respondentCaseSequence[1].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[1].getNextStep({})).toBe('/tasklistresponse/consent-to-application/summary');
 
-    expect(respondentCaseSequence[2].url).toBe('/tasklistresponse/consent-to-application/summary');  //3
+    expect(respondentCaseSequence[2].url).toBe('/tasklistresponse/consent-to-application/summary');  
     expect(respondentCaseSequence[2].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[2].getNextStep({})).toBe('/tasklistresponse/consent-to-application/save');
 
-    expect(respondentCaseSequence[3].url).toBe('/respondent/keep-details-private/details_known');   //4
+    expect(respondentCaseSequence[3].url).toBe('/respondent/keep-details-private/details_known');   
     expect(respondentCaseSequence[3].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[3].getNextStep({})).toBe('/respondent/keep-details-private/start_alternative');
 
-    expect(respondentCaseSequence[4].url).toBe('/respondent/keep-details-private/start_alternative');   //5
+    expect(respondentCaseSequence[4].url).toBe('/respondent/keep-details-private/start_alternative');   
     expect(respondentCaseSequence[4].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[4].getNextStep({})).toBe('/respondent/keep-details-private/save');
 
-    expect(respondentCaseSequence[5].url).toBe('/respondent/keep-details-private/private_details_confirmed');   //6
+    expect(respondentCaseSequence[5].url).toBe('/respondent/keep-details-private/private_details_confirmed');   
     expect(respondentCaseSequence[5].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[5].getNextStep({})).toBe('/respondent/task-list');
 
-    expect(respondentCaseSequence[6].url).toBe('/respondent/keep-details-private/private_details_not_confirmed');   //7
+    expect(respondentCaseSequence[6].url).toBe('/respondent/keep-details-private/private_details_not_confirmed');   
     expect(respondentCaseSequence[6].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[6].getNextStep({})).toBe('/respondent/task-list');
 
-    expect(respondentCaseSequence[7].url).toBe('/tasklistresponse/miam/miam-start');      //8
+    expect(respondentCaseSequence[7].url).toBe('/tasklistresponse/miam/miam-start');      
     expect(respondentCaseSequence[7].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[7].getNextStep({})).toBe('/tasklistresponse/miam/summary');
 
-    expect(respondentCaseSequence[8].url).toBe('/tasklistresponse/miam/willingness-to-attend-miam');  //9
+    expect(respondentCaseSequence[8].url).toBe('/tasklistresponse/miam/willingness-to-attend-miam');  
     expect(respondentCaseSequence[8].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[8].getNextStep({})).toBe('/tasklistresponse/miam/summary');
 
-    expect(respondentCaseSequence[9].url).toBe('/tasklistresponse/miam/summary');     //10
+    expect(respondentCaseSequence[9].url).toBe('/tasklistresponse/miam/summary');     
     expect(respondentCaseSequence[9].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[9].getNextStep({})).toBe('/tasklistresponse/miam/save');
 
-    expect(respondentCaseSequence[10].url).toBe('/respondent/confirm-contact-details/checkanswers');    //11
+    expect(respondentCaseSequence[10].url).toBe('/respondent/confirm-contact-details/checkanswers');    
     expect(respondentCaseSequence[10].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[10].getNextStep({})).toBe('/respondent/confirm-contact-details/save');
 
-    expect(respondentCaseSequence[11].url).toBe('/respondent/confirm-contact-details/personaldetails');   //12
+    expect(respondentCaseSequence[11].url).toBe('/respondent/confirm-contact-details/personaldetails');   
     expect(respondentCaseSequence[11].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[11].getNextStep({})).toBe('/respondent/confirm-contact-details/checkanswers');
 
-    expect(respondentCaseSequence[12].url).toBe('/respondent/confirm-contact-details/contactdetails');    //13
+    expect(respondentCaseSequence[12].url).toBe('/respondent/confirm-contact-details/contactdetails');    
     expect(respondentCaseSequence[12].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[12].getNextStep({})).toBe('/respondent/confirm-contact-details/checkanswers');
 
-    expect(respondentCaseSequence[13].url).toBe('/respondent/confirm-contact-details/addressdetails');    //14
+    expect(respondentCaseSequence[13].url).toBe('/respondent/confirm-contact-details/addressdetails');    
     expect(respondentCaseSequence[13].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[13].getNextStep({})).toBe('/respondent/confirm-contact-details/address/lookup');
 
-    expect(respondentCaseSequence[14].url).toBe('/respondent/confirm-contact-details/address/lookup');    //15
+    expect(respondentCaseSequence[14].url).toBe('/respondent/confirm-contact-details/address/lookup');    
     expect(respondentCaseSequence[14].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[14].getNextStep({})).toBe('/respondent/confirm-contact-details/address/select');
 
-    expect(respondentCaseSequence[15].url).toBe('/respondent/confirm-contact-details/address/select');    //16
+    expect(respondentCaseSequence[15].url).toBe('/respondent/confirm-contact-details/address/select');    
     expect(respondentCaseSequence[15].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[15].getNextStep({})).toBe('/respondent/confirm-contact-details/addressconfirmation');
 
-    expect(respondentCaseSequence[16].url).toBe('/respondent/confirm-contact-details/address/lookup');    //17
+    expect(respondentCaseSequence[16].url).toBe('/respondent/confirm-contact-details/address/lookup');    
     expect(respondentCaseSequence[16].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[16].getNextStep({})).toBe('/respondent/confirm-contact-details/addressconfirmation');
 
-    expect(respondentCaseSequence[17].url).toBe('/respondent/confirm-contact-details/addressconfirmation');    //18
+    expect(respondentCaseSequence[17].url).toBe('/respondent/confirm-contact-details/addressconfirmation');    
     expect(respondentCaseSequence[17].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[17].getNextStep({})).toBe('/respondent/confirm-contact-details/addresshistory');
 
-    expect(respondentCaseSequence[18].url).toBe('/respondent/confirm-contact-details/address/manual');    //19
+    expect(respondentCaseSequence[18].url).toBe('/respondent/confirm-contact-details/address/manual');    
     expect(respondentCaseSequence[18].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[18].getNextStep({})).toBe('/respondent/confirm-contact-details/addresshistory');
 
-    expect(respondentCaseSequence[19].url).toBe('/respondent/confirm-contact-details/addresshistory');    //20
+    expect(respondentCaseSequence[19].url).toBe('/respondent/confirm-contact-details/addresshistory');    
     expect(respondentCaseSequence[19].showInSection).toBe('aboutRespondentCase');
     expect(respondentCaseSequence[19].getNextStep({})).toBe('/respondent/confirm-contact-details/checkanswers');
 
