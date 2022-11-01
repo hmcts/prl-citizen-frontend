4694dceee1707e387cd441683183f497ee49ceaf
praveenrach <praveen.rachapally@HMCTS.NET>
2022-11-01 17:06:49 +0000

    ordinalnumber hearing changes

--- src/main/steps/respondent/yourhearings/hearings/HearingsGetController.ts
+++ src/main/steps/respondent/yourhearings/hearings/HearingsGetController.ts
@@ -44,12 +44,35 @@ export default class HearingsGetController extends GetController {
     console.log('retrieved caseHEARINGdata for case : ' + JSON.stringify(caseHearingDataFromCos));
     req.session.userCase = caseHearingDataFromCos;
 
+    const ordinalNumberMap = new Map<number, string>([
+      [1, 'First'],
+      [2, 'Second'],
+      [3, 'Third'],
+      [4, 'Fourth'],
+      [5, 'Fifth'],
+      [6, 'Sixth'],
+      [7, 'Seventh'],
+      [8, 'Eighth'],
+      [9, 'Ninth'],
+      [10, 'Tenth'],
+      [11, 'Eleventh'],
+      [12, 'Twelfth'],
+      [13, 'Thirteenth'],
+      [14, 'Fourteenth'],
+      [15, 'Fifteenth'],
+      [16, 'Sixteenth'],
+      [17, 'Seventeenth'],
+      [18, 'Eighteenth'],
+      [19, 'Nineteenth'],
+      [20, 'Twentieth'],
+    ]);
     res.render(this.view, {
       ...content,
       sessionErrors,
       htmlLang: language,
       formaction,
       userIdamId: req.session?.user?.id,
+      ordinalNumberMap,
     });
   }
 }
