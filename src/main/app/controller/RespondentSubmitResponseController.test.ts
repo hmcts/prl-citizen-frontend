161f76608099d93217c7236f75e83bf0b6245c44
sahaSonali <sonali.saha@hmcts.net>
2022-11-07 23:11:20 +0000

    testcase for RespondentSubmitResponseController

---
+++ src/main/app/controller/RespondentSubmitResponseController.test.ts
@@ -0,0 +1,24 @@
+import { mockRequest } from '../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../test/unit/utils/mockResponse';
+import { CA_RESPONDENT_RESPONSE_CONFIRMATION } from '../../steps/urls';
+import { CosApiClient } from '../case/CosApiClient';
+import { RespondentSubmitResponseController } from './RespondentSubmitResponseController';
+
+describe('RespondentSubmitResponseController', () => {
+    const controller = new RespondentSubmitResponseController();
+    const req = mockRequest();
+    const res = mockResponse();
+    const submitRespondentResponseMock = jest.spyOn(CosApiClient.prototype, 'submitRespondentResponse');
+    beforeEach(() => {
+        submitRespondentResponseMock.mockResolvedValue(req.session.userCase);
+    });
+    afterEach(() => {
+        submitRespondentResponseMock.mockClear();
+      });
+    test('Test Save', async () => {
+        req.session.userCase.id = '12234567890';
+        await controller.save(req, res);
+        expect(res.redirect).toHaveBeenCalledWith(CA_RESPONDENT_RESPONSE_CONFIRMATION);
+      });
+});
+   
