8192536a56165e0afb2fa238393d3915d7861c3f
sahaSonali <sonali.saha@hmcts.net>
2022-11-07 18:56:30 +0000

    testcase for DocumentManagerController

--- src/main/steps/applicant/confirm-contact-details/addressdetails/content.test.ts
+++ src/main/steps/applicant/confirm-contact-details/addressdetails/content.test.ts
@@ -4,20 +4,20 @@ import { CommonContent, generatePageContent } from '../../../common/common.conte
 import { generateContent } from './content';
 
 const en = {
-    title: 'Your address',
-    citizenUserAddressText: 'address',
-    continue: 'Save and continue',
-    editAddress: 'Edit Address',
-    errors: {},
-  };
-  
-  describe('address confirmation > content', () => {
-    const commonContent = generatePageContent({
-      language: 'en',
-      userCase: {},
-    }) as CommonContent;
-  
-    test('should return correct english content', () => {
-      languageAssertions('en', en, () => generateContent(commonContent));
-    });
+  title: 'Your address',
+  citizenUserAddressText: 'address',
+  continue: 'Save and continue',
+  editAddress: 'Edit Address',
+  errors: {},
+};
+
+describe('address confirmation > content', () => {
+  const commonContent = generatePageContent({
+    language: 'en',
+    userCase: {},
+  }) as CommonContent;
+
+  test('should return correct english content', () => {
+    languageAssertions('en', en, () => generateContent(commonContent));
   });
+});
