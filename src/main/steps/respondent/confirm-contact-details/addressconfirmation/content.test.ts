11be7dca444441737f3333b008ddd25beac38295
JamesCutts00 <James.Cutts@HMCTS.ORG>
2022-11-07 19:12:08 +0000

    add test suites for respondant

---
+++ src/main/steps/respondent/confirm-contact-details/addressconfirmation/content.test.ts
@@ -0,0 +1,36 @@
+import languageAssertions from '../../../../../test/unit/utils/languageAssertions';
+import { CommonContent, generatePageContent } from '../../../common/common.content';
+
+import { generateContent } from './content';
+
+const en = {
+  title: 'Your Address',
+  citizenUserAddress1: 'Building and street',
+  citizenUserAddressTown: 'Town or city',
+  citizenUserAddressCounty: 'County',
+  citizenUserAddressPostcode: 'Postcode',
+  errors: {
+    citizenUserAddress1: {
+      required: 'Enter the first line of the address',
+    },
+    citizenUserAddressTown: {
+      required: 'Enter the town or city',
+    },
+    citizenUserAddressPostcode: {
+      required: 'Enter a real postcode',
+      invalid: 'Enter a real postcode',
+    },
+  },
+};
+
+/* eslint-disable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any */
+describe('address confirmation > content', () => {
+  const commonContent = generatePageContent({
+    language: 'en',
+    userCase: {},
+  }) as CommonContent;
+
+  test('should return correct english content', () => {
+    languageAssertions('en', en, () => generateContent(commonContent));
+  });
+});
