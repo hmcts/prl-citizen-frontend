5bffa33e0364c6ed0eeb1c1b8b6c2cd63d3d09b7
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2022-11-07 23:54:56 +0000

    made some changes in check-answers-no

--- src/main/steps/respondent/allegations-of-harm-and-violence/check-answers-no/content.ts
+++ src/main/steps/respondent/allegations-of-harm-and-violence/check-answers-no/content.ts
@@ -1,21 +1,64 @@
 import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { CommonContent } from '../../../common/common.content';
+import { summaryList } from '../../../common/summary/utils';
 
-const en = {
+export const enContent = {
   section: 'Check your answers',
-  title:'Safety concerns',
-  lineheading:'Do you have any concerns for your safety or the safety of the children?',
-  
- 
-  continue: 'Save and continue',
+  title: 'Safety concerns',
+  sectionTitles: {
+    applicationDetails: 'Application details',
+  },
+  keys: {
+    yourchildconcernsstart: 'Do you have any concerns for your safety or the safety of the children?',
+  },
+  errors: {},
 };
 
-const cy: typeof en = {
-  section: 'Check your answers - in welsh',
-  title:'Safety concerns - in welsh',
-  lineheading:'Do you have any concerns for your safety or the safety of the children? - in welsh',
-  
- 
-  continue: 'Save and continue - in welsh',
+const en = (content: CommonContent) => {
+  const userCase = content.userCase!;
+  return {
+    ...enContent,
+    language: content.language,
+    sections: [summaryList(enContent, userCase, urls, '', fieldType, content.language)],
+  };
+};
+
+const cyContent: typeof enContent = {
+  section: 'Check your answers',
+  title: 'Safety concerns',
+  sectionTitles: {
+    applicationDetails: 'Application details',
+  },
+  keys: {
+    yourchildconcernsstart: 'Do you have any concerns for your safety or the safety of the children?',
+  },
+  errors: {},
+};
+
+const urls = {
+  yourchildconcernsstart: 'your-or-child-safety-concerns',
+};
+const fieldType = {
+  yourchildconcernsstart: 'String',
+};
+
+const cy: typeof en = (content: CommonContent) => {
+  const userCase = content.userCase!;
+  //userCase.citizenUserDateOfBirthText = getFormattedDate(userCase.citizenUserDateOfBirth);
+  //console.log("userCase.citizenUserDateOfBirthText====>"+userCase.citizenUserDateOfBirthText);
+  return {
+    ...cyContent,
+    language: content.language,
+    sections: [summaryList(cyContent, userCase, urls, '', fieldType, content.language)],
+  };
+};
+
+export const form: FormContent = {
+  fields: {},
+  submit: {
+    text: l => l.continue,
+  },
 };
 
 const languages = {
@@ -24,8 +67,9 @@ const languages = {
 };
 
 export const generateContent: TranslationFn = content => {
-  const translations = languages[content.language];
+  const translations = languages[content.language](content);
   return {
-    ...translations
+    ...translations,
+    form,
   };
 };
