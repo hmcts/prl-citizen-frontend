f2347345fe96f23caf8b53bcc2b146300b7d691a
vivek-sekhar <vivek.sekhar@hmcts.net>
2022-11-08 17:20:49 +0000

    test(PRLC100-26): Fixed test case

--- src/main/steps/c100-rebuild/c100sequence.test.ts
+++ src/main/steps/c100-rebuild/c100sequence.test.ts
@@ -12,7 +12,7 @@ import { C100Sequence } from './c100sequence';
 
 describe('C100Sequence', () => {
   test('should contain 1 entries in c100 screen sequence', () => {
-    expect(C100Sequence).toHaveLength(113);
+    expect(C100Sequence).toHaveLength(114);
     expect(C100Sequence[0].url).toBe('/c100-rebuild/confidentiality/details-know');
     expect(C100Sequence[0].showInSection).toBe('c100');
     expect(C100Sequence[0].getNextStep({ detailsKnown: YesOrNo.YES })).toBe(
