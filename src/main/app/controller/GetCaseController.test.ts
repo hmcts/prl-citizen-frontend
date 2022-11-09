de358bd99f4f4a9897ff1c5f786c162b7e0fb306
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-11-09 18:38:14 +0000

    get case controller test cases added

--- src/main/app/controller/GetCaseController.test.ts
+++ src/main/app/controller/GetCaseController.test.ts
@@ -1,49 +1,43 @@
-import axios, { AxiosInstance, AxiosStatic } from 'axios';
-
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
+import { CosApiClient } from '../case/CosApiClient';
 
-//import { generatePageContent } from '../../steps/common/common.content';
-// import { Case } from '../case/case';
 import { GetCaseController } from './GetCaseController';
 
+jest.mock('../case/CosApiClient');
+
+const mockMyFunction = CosApiClient as jest.Mock;
 jest.mock('axios');
 jest.mock('config');
 jest.mock('../auth/service/get-service-auth-token');
-const mockedAxiosUser = axios as jest.Mocked<AxiosStatic>;
-const token = 'test token';
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 describe('GetCaseController', () => {
-  test('Should render the page', async () => {
-    const controller = new GetCaseController();
+  let controller;
+  let req;
+  let res;
 
-    const mockGet = jest.fn().mockResolvedValueOnce({ data: { mockPayment: 'data', id: '232442' } });
-    mockedAxios.create.mockReturnValueOnce({ get: mockGet } as unknown as AxiosInstance);
-    const req = mockRequest();
-    const res = mockResponse();
-    mockedAxiosUser.post.mockResolvedValue({
-      data: {
-        access_token: token,
-        id_token: token,
+  beforeEach(() => {
+    controller = new GetCaseController();
+    req = mockRequest({
+      session: {
+        userCase: {},
+        userCaseList: [
+          {
+            id: '1234',
+          },
+        ],
       },
+      caseId: '1234',
     });
-    let flag = false;
-    try {
-      await controller.getApplicantCase(req, res);
-    } catch (err) {
-      flag = true;
-    }
-    expect(flag).toBe(true);
+    res = mockResponse();
+    mockMyFunction.mockReturnValue('hello');
+  });
+  test('applicant Case', async () => {
+    await controller.getApplicantCase(req, res);
+    expect(mockMyFunction).toHaveBeenCalled();
+  });
 
-    /*expect(res.render).toBeCalledWith('page', {
-        ...defaultViewArgs,
-        language: 'en',
-        serviceName: 'Apply for a service"',
-        isDraft: true,
-        text: 'english',
-        userCase: req.session.userCase,
-        userEmail,
-      });*/
-    expect(1).toEqual(1);
+  test('respondent case', async () => {
+    await controller.getRespondentCase(req, res);
+    expect(mockMyFunction).toHaveBeenCalled();
   });
 });
