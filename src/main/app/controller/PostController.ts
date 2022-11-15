5c3ec8f2834d0ed9e01917b211137a68f9dfd686
Aloknath Datta <alokdatta.uit@gmail.com>
2022-11-15 23:54:57 +0000

    bug fix. added bearer message

--- src/main/app/controller/PostController.ts
+++ src/main/app/controller/PostController.ts
@@ -34,7 +34,7 @@ export class PostController<T extends AnyObject> {
       await this.saveBeforeSessionTimeout(req, res, formData);
     } else if (req.body.accessCodeCheck) {
       await this.checkCaseAccessCode(req, res, form, formData);
-      await this.getCaseList(req, res, form, formData);
+      // await this.getCaseList(req, res, form, formData);
     } else if (req.body.onlyContinue) {
       await this.onlyContinue(req, res, form, formData);
     } else {
@@ -243,24 +243,6 @@ export class PostController<T extends AnyObject> {
     if (req.session.errors.length) {
       return this.redirect(req, res);
     }
-
-    this.redirect(req, res);
-  }
-
-  private async getCaseList(req: AppRequest<T>, res: Response, form: Form, formData: Partial<Case>): Promise<void> {
-    req.session.errors = form.getErrors(formData);
-
-    this.filterErrorsForSaveAsDraft(req);
-
-    if (req.session.errors.length) {
-      return this.redirect(req, res);
-    }
-
-    const citizenUser = req.session.user;
-    console.log('=====citizenUser====' + citizenUser);
-    const cosApiClient = new CosApiClient(citizenUser.accessToken, 'http://localhost:3001');
-    const caseDataFromCos = await cosApiClient.retrieveCasesByUserId(req.session.user);
-    console.log('retrieved casedata for case : ' + caseDataFromCos);
 
     this.redirect(req, res);
   }
