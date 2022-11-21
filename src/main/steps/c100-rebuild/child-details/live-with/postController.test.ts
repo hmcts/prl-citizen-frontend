2cd52e04e13aa4789e67db7947ba2282f720ece0
Saranya Vinayagam <saranyavinayagam@Saranyas-MacBook-Pro.local>
2022-11-21 13:46:55 +0000

    test case for live with post controller

--- src/main/steps/c100-rebuild/child-details/live-with/postController.test.ts
+++ src/main/steps/c100-rebuild/child-details/live-with/postController.test.ts
@@ -1,5 +1,6 @@
 import { mockRequest } from '../../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../../test/unit/utils/mockResponse';
+import { PartyType } from '../../../../app/case/definition';
 import { FormContent } from '../../../../app/form/Form';
 import { CommonContent } from '../../../common/common.content';
 
@@ -10,6 +11,15 @@ describe('PersonaldetailsPostController Post Controller', () => {
   const commonContent = {
     language: 'en',
     userCase: {
+      people: [
+        {
+          id: '7483640e-0817-4ddc-b709-6723f7925474',
+          firstName: 'Bob',
+          lastName: 'Silly',
+          partyType: PartyType.CHILDREN,
+        },
+      ],
+
       cd_children: [
         {
           id: '7483640e-0817-4ddc-b709-6723f7925474',
@@ -35,7 +45,22 @@ describe('PersonaldetailsPostController Post Controller', () => {
           parentialResponsibility: {
             statement: 'test',
           },
-          liveWith: [],
+          liveWith: [
+            {
+              id: '7483640e-0817-4ddc-b709-6723f7925474',
+              firstName: 'Bob',
+              lastName: 'Silly',
+              partyType: PartyType.CHILDREN,
+            },
+          ],
+        },
+      ],
+      appl_allApplicants: [
+        {
+          applicantFirstName: 'Dummy ',
+          id: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
+          applicantLastName: 'Test1',
+          applicantAddressPostcode: 'AG11NB',
         },
       ],
     },
@@ -77,7 +102,14 @@ describe('PersonaldetailsPostController Post Controller', () => {
 
   test('Should update case when save and come back button is clicked', async () => {
     const mockFormContent = {
-      fields: {},
+      fields: {
+        liveWith: {
+          id: '7483640e-0817-4ddc-b709-6723f7925474',
+          firstName: 'Bob',
+          lastName: 'Silly',
+          partyType: PartyType.CHILDREN,
+        },
+      },
     } as unknown as FormContent;
     const controller = new PersonaldetailsPostController(mockFormContent.fields);
     const language = 'en';
@@ -86,6 +118,14 @@ describe('PersonaldetailsPostController Post Controller', () => {
         childId: '7483640e-0817-4ddc-b709-6723f7925474',
       },
       body: {
+        liveWith: [
+          {
+            id: '7483640e-0817-4ddc-b709-6723f7925474',
+            firstName: 'Bob',
+            lastName: 'Silly',
+            partyType: PartyType.CHILDREN,
+          },
+        ],
         saveAndComeLater: true,
       },
       session: {
@@ -100,5 +140,31 @@ describe('PersonaldetailsPostController Post Controller', () => {
     await controller.post(req, res);
 
     expect(res.redirect).toHaveBeenCalled();
+  });
+
+  test('Should call post and update other person > relationship to child', async () => {
+    const mockFields = {
+      fields: {},
+    } as unknown as FormContent;
+    const req = mockRequest(commonContent);
+    req.body = {
+      liveWith: [
+        {
+          id: '7483640e-0817-4ddc-b709-6723f7925474',
+          firstName: 'Bob',
+          lastName: 'Silly',
+          partyType: PartyType.CHILDREN,
+        },
+      ],
+      onlycontinue: true,
+    };
+    const controller = new PersonaldetailsPostController(mockFields.fields);
+    // req.session.userCase = ;
+    const res = mockResponse();
+    generateContent(commonContent);
+    await controller.post(req, res);
+    expect(res.redirect).toHaveBeenCalled();
+    // expect(req.session.userCase).toEqual({ ...commonContent.userCase, id: '1234' });
+    // expect(res.redirect).toHaveBeenCalledWith('/citizen-home');
   });
 });
