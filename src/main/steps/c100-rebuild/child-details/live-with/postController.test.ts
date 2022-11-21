8f13f99e5e1a231538c9107c55f01d533d193913
Saranya Vinayagam <saranyavinayagam@Saranyas-MacBook-Pro.local>
2022-11-21 16:57:50 +0000

    updated test case for child live with post controller

--- src/main/steps/c100-rebuild/child-details/live-with/postController.test.ts
+++ src/main/steps/c100-rebuild/child-details/live-with/postController.test.ts
@@ -7,63 +7,63 @@ import { CommonContent } from '../../../common/common.content';
 import { generateContent } from './content';
 import PersonaldetailsPostController from './postController';
 
-describe('PersonaldetailsPostController Post Controller', () => {
-  const commonContent = {
-    language: 'en',
-    userCase: {
-      cd_children: [
-        {
-          id: '7483640e-0817-4ddc-b709-6723f7925474',
-          firstName: 'Bob',
-          lastName: 'Silly',
-          personalDetails: {
-            dateOfBirth: {
-              year: '',
-              month: '',
-              day: '',
-            },
-            isDateOfBirthUnknown: 'Yes',
-            approxDateOfBirth: {
-              year: '1987',
-              month: '12',
-              day: '12',
-            },
-            sex: 'Male',
+const commonContent = {
+  language: 'en',
+  userCase: {
+    cd_children: [
+      {
+        id: '7483640e-0817-4ddc-b709-6723f7925474',
+        firstName: 'Bob',
+        lastName: 'Silly',
+        personalDetails: {
+          dateOfBirth: {
+            year: '',
+            month: '',
+            day: '',
           },
-          childMatters: {
-            needsResolution: [],
+          isDateOfBirthUnknown: 'Yes',
+          approxDateOfBirth: {
+            year: '1987',
+            month: '12',
+            day: '12',
           },
-          parentialResponsibility: {
-            statement: 'test',
+          sex: 'Male',
+        },
+        childMatters: {
+          needsResolution: [],
+        },
+        parentialResponsibility: {
+          statement: 'test',
+        },
+        liveWith: [
+          {
+            id: '7483640e-0817-4ddc-b709-6723f7925474',
+            firstName: 'Bob',
+            lastName: 'Silly',
+            partyType: PartyType.APPLICANT,
           },
-          liveWith: [
-            {
-              id: '7483640e-0817-4ddc-b709-6723f7925474',
-              firstName: 'Bob',
-              lastName: 'Silly',
-              partyType: PartyType.APPLICANT,
-            },
-          ],
-        },
-      ],
-      appl_allApplicants: [
-        {
-          applicantFirstName: 'Dummy ',
-          id: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
-          applicantLastName: 'Test1',
-          applicantAddressPostcode: 'AG11NB',
-        },
-      ],
-    },
-    additionalData: {
-      req: {
-        params: {
-          childId: '7483640e-0817-4ddc-b709-6723f7925474',
-        },
+        ],
+      },
+    ],
+    appl_allApplicants: [
+      {
+        applicantFirstName: 'Dummy ',
+        id: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
+        applicantLastName: 'Test1',
+        applicantAddressPostcode: 'AG11NB',
+      },
+    ],
+  },
+  additionalData: {
+    req: {
+      params: {
+        childId: '7483640e-0817-4ddc-b709-6723f7925474',
       },
     },
-  } as unknown as CommonContent;
+  },
+} as unknown as CommonContent;
 
+describe('PersonaldetailsPostController Post Controller', () => {
   test('Should navigagte to the next page when there are no errors when continue button is clicked', async () => {
     const mockFormContent = {
       fields: {},
@@ -143,6 +143,49 @@ describe('PersonaldetailsPostController Post Controller', () => {
             id: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
             applicantLastName: 'Test1',
             applicantAddressPostcode: 'AG11NB',
+          },
+        ],
+        saveAndComeLater: true,
+      },
+      session: {
+        lang: language,
+        userCase: {
+          ...commonContent.userCase,
+        },
+      },
+    });
+    const res = mockResponse();
+    generateContent(commonContent);
+    await controller.post(req, res);
+
+    expect(res.redirect).toHaveBeenCalled();
+  });
+
+  test('Should update case child live with respondent when save and come back button is clicked', async () => {
+    const mockFormContent = {
+      fields: {},
+    } as unknown as FormContent;
+    const controller = new PersonaldetailsPostController(mockFormContent.fields);
+    const language = 'en';
+    const req = mockRequest({
+      params: {
+        childId: '7483640e-0817-4ddc-b709-6723f7925474',
+      },
+      body: {
+        liveWith: [
+          {
+            id: '7483640e-0817-4ddc-b709-6723f7925474',
+            firstName: 'Bob',
+            lastName: 'Silly',
+            partyType: PartyType.RESPONDENT,
+          },
+        ],
+        respondent: [
+          {
+            id: '983640e-0817-4ddc-b709-6723f7925094',
+            respondentFirstName: 'rob',
+            respondentLastName: 'di',
+            partyType: PartyType.RESPONDENT,
           },
         ],
         saveAndComeLater: true,
