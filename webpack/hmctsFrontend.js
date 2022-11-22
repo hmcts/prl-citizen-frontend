45e150060f6b27f9e71e179af67991e7d116a78b
bharadwajsairam <bharadwajsairam001@gmail.com>
2022-11-22 12:30:53 +0000

    Adding tick image to train track

--- webpack/hmctsFrontend.js
+++ webpack/hmctsFrontend.js
@@ -4,13 +4,19 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const packageJson = require.resolve('@hmcts/frontend/package.json');
 const root = path.resolve(packageJson, '..');
+const localImages = path.resolve(root, '../../../src/main/assets/images')
 const assets = path.resolve(root, 'assets');
 const images = path.resolve(assets, 'images');
 
 const copyTemplateAssets = new CopyWebpackPlugin({
-  patterns: [{ from: images, to: 'hmcts-assets/images' }],
+  patterns: [{ from: images, to: 'hmcts-assets/images' },
+  {
+    from: localImages,
+    to: 'assets/images',
+    noErrorOnMissing: true
+  }],
 });
-
+console.log(copyTemplateAssets);
 module.exports = {
   paths: { template: root, assets },
   plugins: [copyTemplateAssets],
