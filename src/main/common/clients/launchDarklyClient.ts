af95a792030217090184ee086724ed6d868ee707
Yogendra Upasani <yogendra.upasani@hmcts.net>
2022-11-23 09:15:05 +0000

    LD flag updates

--- src/main/common/clients/launchDarklyClient.ts
+++ src/main/common/clients/launchDarklyClient.ts
@@ -9,15 +9,11 @@ export class LaunchDarklyClient {
   private static client: LDClient;
 
   constructor() {
-    console.log("config.get('launchDarkly.offline')", config.get('launchDarkly.offline'));
-    if (config.get<string>('launchDarkly.offline').trim() === 'false') {
+    if (!LaunchDarklyClient.client) {
       console.log('initializing LD Client');
-      if (!LaunchDarklyClient.client) {
-        const sdkKey: string = config.get<string>('featureToggles.launchDarklyKey');
-        LaunchDarklyClient.client = init(sdkKey, ldConfig);
-      }
+      const sdkKey: string = config.get<string>('featureToggles.launchDarklyKey');
+      LaunchDarklyClient.client = init(sdkKey, ldConfig);
     }
-    console.log('outside if loop');
   }
 
   async initializeLD(): Promise<void> {
@@ -27,17 +23,15 @@ export class LaunchDarklyClient {
   }
 
   async serviceVariation(featureKey: string, offlineDefault: LDFlagValue): Promise<LDFlagValue> {
-    if (config.get<string>('launchDarkly.offline').trim() === 'false') {
-      const roles: string[] = [];
-      const ldUser: LDUser = {
-        key: 'citizen-frontend',
-        custom: {
-          roles,
-        },
-      };
+    const roles: string[] = [];
+    const ldUser: LDUser = {
+      key: 'citizen-frontend',
+      custom: {
+        roles,
+      },
+    };
 
-      return LaunchDarklyClient.client.variation(featureKey, ldUser, offlineDefault);
-    }
+    return LaunchDarklyClient.client.variation(featureKey, ldUser, offlineDefault);
     return true;
   }
 }
