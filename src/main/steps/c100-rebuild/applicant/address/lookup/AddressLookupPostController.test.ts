f722646f01e533487d948277d85838b3254fdf38
niranjan-301 <113594061+niranjan-301@users.noreply.github.com>
2022-11-24 13:06:14 +0000

    test coverage for Address controller

--- src/main/steps/c100-rebuild/applicant/address/lookup/AddressLookupPostController.test.ts
+++ src/main/steps/c100-rebuild/applicant/address/lookup/AddressLookupPostController.test.ts
@@ -29,7 +29,7 @@ describe('applicant1 > address > lookup > AddressLookupPostController', () => {
     },
   } as unknown as CommonContent;
 
-  test('should call super constructor with correct params', async () => {
+  test('Check saveAndComeLater conditions', async () => {
     const mockFormContent = {
       fields: {},
     } as unknown as FormContent;
@@ -45,6 +45,40 @@ describe('applicant1 > address > lookup > AddressLookupPostController', () => {
         applicantLastName: 'Test1',
         applicantAddressPostcode: 'AG11NB',
         saveAndComeLater: true,
+      },
+      session: {
+        lang: language,
+        userCase: {
+          appl_allApplicants: [
+            {
+              id: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
+              applicantFirstName: 'Dummy',
+              applicantLastName: 'Test1',
+            },
+          ],
+        },
+      },
+    });
+    const res = mockResponse();
+    generateContent(commonContent);
+    await controller.post(req, res);
+
+    expect(res.redirect).toHaveBeenCalled();
+  });
+
+  test('check valid address scenario', async () => {
+    const mockFormContent = {
+      fields: {},
+    } as unknown as FormContent;
+    const controller = new AddressLookupPostController(mockFormContent.fields);
+    const language = 'en';
+    const req = mockRequest({
+      params: {
+        applicantId: '480e8295-4c5b-4b9b-827f-f9be423ec1c5',
+      },
+      body: {
+        addressPostcode: 'B437AJ',
+        onlycontinue: true,
       },
       session: {
         lang: language,
