226a4b13ffd0987594535a04d6204e918d061b45
Khurshed Khan <khurshed.khan@hmcts.net>
2022-11-24 20:49:48 +0000

    added matching of user idam id

--- src/main/steps/tasklistresponse/summary/content.ts
+++ src/main/steps/tasklistresponse/summary/content.ts
@@ -886,7 +886,7 @@ export const enDummyContent = {
 
 const en = (content: CommonContent) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  populateSummaryData(content.userCase);
+  populateSummaryData(content.userCase, content.userIdamId);
   const userCase = content.userCase!;
 
   // updateContent(enContent, userCase, urls);
@@ -1537,147 +1537,152 @@ export const generateContent: TranslationFn = content => {
     form,
   };
 };
-function populateSummaryData(userCase: Partial<import('../../../app/case/case').CaseWithId> | undefined) {
+function populateSummaryData(
+  userCase: Partial<import('../../../app/case/case').CaseWithId> | undefined,
+  userIdamId: string | undefined
+) {
   userCase?.respondents?.forEach((respondent: Respondent) => {
-    /* Keep detais private */
-    userCase.detailsKnown = respondent.value.response.keepDetailsPrivate?.otherPeopleKnowYourContactDetails;
-    userCase.startAlternative = respondent.value.response.keepDetailsPrivate?.confidentiality;
-    userCase.contactDetailsPrivate = respondent.value.response.keepDetailsPrivate?.confidentialityList;
-    /** consent to application */
-    if (respondent?.value?.response?.consent?.consentToTheApplication === YesOrNo.NO) {
-      userCase.doYouConsent = YesOrNo.NO;
-      userCase.reasonForNotConsenting = respondent?.value?.response?.consent.noConsentReason;
-    } else {
-      userCase.doYouConsent = YesOrNo.YES;
-      userCase.reasonForNotConsenting = '';
-    }
-    if (respondent?.value?.response?.consent?.permissionFromCourt === YesOrNo.NO) {
-      userCase.courtPermission = YesOrNo.NO;
-      userCase.courtOrderDetails = '';
-    } else {
-      userCase.courtPermission = YesOrNo.YES;
-      userCase.courtOrderDetails = respondent?.value?.response?.consent?.courtOrderDetails;
-    }
-    userCase.applicationReceivedDate = fromApiDate(respondent?.value?.response?.consent?.applicationReceivedDate);
+    if (userIdamId === respondent.value.user.idamId) {
+      /* Keep detais private */
+      userCase.detailsKnown = respondent.value.response.keepDetailsPrivate?.otherPeopleKnowYourContactDetails;
+      userCase.startAlternative = respondent.value.response.keepDetailsPrivate?.confidentiality;
+      userCase.contactDetailsPrivate = respondent.value.response.keepDetailsPrivate?.confidentialityList;
+      /** consent to application */
+      if (respondent?.value?.response?.consent?.consentToTheApplication === YesOrNo.NO) {
+        userCase.doYouConsent = YesOrNo.NO;
+        userCase.reasonForNotConsenting = respondent?.value?.response?.consent.noConsentReason;
+      } else {
+        userCase.doYouConsent = YesOrNo.YES;
+        userCase.reasonForNotConsenting = '';
+      }
+      if (respondent?.value?.response?.consent?.permissionFromCourt === YesOrNo.NO) {
+        userCase.courtPermission = YesOrNo.NO;
+        userCase.courtOrderDetails = '';
+      } else {
+        userCase.courtPermission = YesOrNo.YES;
+        userCase.courtOrderDetails = respondent?.value?.response?.consent?.courtOrderDetails;
+      }
+      userCase.applicationReceivedDate = fromApiDate(respondent?.value?.response?.consent?.applicationReceivedDate);
 
-    /** Miam */
-    if (respondent?.value?.response?.miam?.attendedMiam === YesOrNo.YES) {
-      userCase.miamStart = YesOrNo.YES;
-      userCase.miamWillingness = YesOrNo.NO;
-      userCase.miamNotWillingExplnation = '';
-    } else if (respondent?.value?.response?.miam?.attendedMiam === YesOrNo.NO) {
-      if (respondent?.value?.response?.miam?.willingToAttendMiam === YesOrNo.YES) {
-        userCase.miamStart = YesOrNo.NO;
-        userCase.miamWillingness = YesOrNo.YES;
-        userCase.miamNotWillingExplnation = '';
-      } else if (respondent?.value?.response?.miam?.willingToAttendMiam === YesOrNo.NO) {
-        userCase.miamStart = YesOrNo.NO;
+      /** Miam */
+      if (respondent?.value?.response?.miam?.attendedMiam === YesOrNo.YES) {
+        userCase.miamStart = YesOrNo.YES;
         userCase.miamWillingness = YesOrNo.NO;
-        userCase.miamNotWillingExplnation = respondent?.value?.response?.miam?.reasonNotAttendingMiam;
+        userCase.miamNotWillingExplnation = '';
+      } else if (respondent?.value?.response?.miam?.attendedMiam === YesOrNo.NO) {
+        if (respondent?.value?.response?.miam?.willingToAttendMiam === YesOrNo.YES) {
+          userCase.miamStart = YesOrNo.NO;
+          userCase.miamWillingness = YesOrNo.YES;
+          userCase.miamNotWillingExplnation = '';
+        } else if (respondent?.value?.response?.miam?.willingToAttendMiam === YesOrNo.NO) {
+          userCase.miamStart = YesOrNo.NO;
+          userCase.miamWillingness = YesOrNo.NO;
+          userCase.miamNotWillingExplnation = respondent?.value?.response?.miam?.reasonNotAttendingMiam;
+        }
       }
-    }
 
-    /** International Elements */
+      /** International Elements */
 
-    if (respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWl === YesOrNo.NO) {
-      userCase.start = YesOrNo.NO;
-      userCase.iFactorsStartProvideDetails = '';
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWl === YesOrNo.YES) {
-      userCase.start = YesOrNo.YES;
-      userCase.iFactorsStartProvideDetails =
-        respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWlDetails;
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWl === YesOrNo.NO) {
-      userCase.parents = YesOrNo.NO;
-      userCase.iFactorsParentsProvideDetails = '';
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWl === YesOrNo.YES) {
-      userCase.parents = YesOrNo.YES;
-      userCase.iFactorsParentsProvideDetails =
-        respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWlDetails;
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWl === YesOrNo.NO) {
-      userCase.jurisdiction = YesOrNo.NO;
-      userCase.iFactorsJurisdictionProvideDetails = '';
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWl === YesOrNo.YES) {
-      userCase.jurisdiction = YesOrNo.YES;
-      userCase.iFactorsJurisdictionProvideDetails =
-        respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWlDetails;
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformation === YesOrNo.NO) {
-      userCase.request = YesOrNo.NO;
-      userCase.iFactorsRequestProvideDetails = '';
-    }
-    if (respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformation === YesOrNo.YES) {
-      userCase.request = YesOrNo.YES;
-      userCase.iFactorsRequestProvideDetails =
-        respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformationDetaails;
-    }
+      if (respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWl === YesOrNo.NO) {
+        userCase.start = YesOrNo.NO;
+        userCase.iFactorsStartProvideDetails = '';
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWl === YesOrNo.YES) {
+        userCase.start = YesOrNo.YES;
+        userCase.iFactorsStartProvideDetails =
+          respondent?.value?.response?.citizenInternationalElements?.childrenLiveOutsideOfEnWlDetails;
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWl === YesOrNo.NO) {
+        userCase.parents = YesOrNo.NO;
+        userCase.iFactorsParentsProvideDetails = '';
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWl === YesOrNo.YES) {
+        userCase.parents = YesOrNo.YES;
+        userCase.iFactorsParentsProvideDetails =
+          respondent?.value?.response?.citizenInternationalElements?.parentsAnyOneLiveOutsideEnWlDetails;
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWl === YesOrNo.NO) {
+        userCase.jurisdiction = YesOrNo.NO;
+        userCase.iFactorsJurisdictionProvideDetails = '';
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWl === YesOrNo.YES) {
+        userCase.jurisdiction = YesOrNo.YES;
+        userCase.iFactorsJurisdictionProvideDetails =
+          respondent?.value?.response?.citizenInternationalElements?.anotherPersonOrderOutsideEnWlDetails;
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformation === YesOrNo.NO) {
+        userCase.request = YesOrNo.NO;
+        userCase.iFactorsRequestProvideDetails = '';
+      }
+      if (respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformation === YesOrNo.YES) {
+        userCase.request = YesOrNo.YES;
+        userCase.iFactorsRequestProvideDetails =
+          respondent?.value?.response?.citizenInternationalElements?.anotherCountryAskedInformationDetaails;
+      }
 
-    /** Confirm your details*/
-    if (respondent?.value?.firstName) {
-      userCase.citizenUserFirstNames = respondent?.value?.firstName;
-    }
-    if (respondent?.value?.lastName) {
-      userCase.citizenUserLastNames = respondent?.value?.lastName;
-    }
-    if (!userCase.citizenUserFirstNames || !userCase.citizenUserLastNames) {
-      userCase.citizenUserFullName = '';
-    } else {
-      userCase.citizenUserFullName = userCase.citizenUserFirstNames + ' ' + userCase.citizenUserLastNames;
-    }
-    if (respondent?.value?.placeOfBirth) {
-      userCase.citizenUserPlaceOfBirthText = respondent?.value?.placeOfBirth;
-    }
-    if (respondent?.value?.dateOfBirth) {
-      userCase.citizenUserDateOfBirthText = respondent?.value?.dateOfBirth;
-    }
-    if (respondent?.value?.phoneNumber) {
-      userCase.citizenUserPhoneNumberText = respondent?.value?.phoneNumber;
-    }
-    if (respondent?.value?.email) {
-      userCase.citizenUserEmailAddressText = respondent?.value?.email;
-    }
+      /** Confirm your details*/
+      if (respondent?.value?.firstName) {
+        userCase.citizenUserFirstNames = respondent?.value?.firstName;
+      }
+      if (respondent?.value?.lastName) {
+        userCase.citizenUserLastNames = respondent?.value?.lastName;
+      }
+      if (!userCase.citizenUserFirstNames || !userCase.citizenUserLastNames) {
+        userCase.citizenUserFullName = '';
+      } else {
+        userCase.citizenUserFullName = userCase.citizenUserFirstNames + ' ' + userCase.citizenUserLastNames;
+      }
+      if (respondent?.value?.placeOfBirth) {
+        userCase.citizenUserPlaceOfBirthText = respondent?.value?.placeOfBirth;
+      }
+      if (respondent?.value?.dateOfBirth) {
+        userCase.citizenUserDateOfBirthText = respondent?.value?.dateOfBirth;
+      }
+      if (respondent?.value?.phoneNumber) {
+        userCase.citizenUserPhoneNumberText = respondent?.value?.phoneNumber;
+      }
+      if (respondent?.value?.email) {
+        userCase.citizenUserEmailAddressText = respondent?.value?.email;
+      }
 
-    if (respondent?.value.address) {
-      if (respondent?.value.address.AddressLine1) {
-        userCase.citizenUserAddress1 = respondent?.value.address.AddressLine1;
+      if (respondent?.value.address) {
+        if (respondent?.value.address.AddressLine1) {
+          userCase.citizenUserAddress1 = respondent?.value.address.AddressLine1;
+        }
+        if (respondent?.value.address.AddressLine2) {
+          userCase.citizenUserAddress2 = respondent?.value.address.AddressLine2;
+        }
+        if (respondent?.value.address.PostTown) {
+          userCase.citizenUserAddressTown = respondent?.value.address.PostTown;
+        }
+        if (respondent?.value.address.County) {
+          userCase.citizenUserAddressCounty = respondent?.value.address.County;
+        }
+        if (respondent?.value.address.PostCode) {
+          userCase.citizenUserAddressPostcode = respondent?.value.address.PostCode;
+        }
       }
-      if (respondent?.value.address.AddressLine2) {
-        userCase.citizenUserAddress2 = respondent?.value.address.AddressLine2;
+      if (respondent?.value.addressLivedLessThan5YearsDetails) {
+        userCase.citizenUserAddressHistory = respondent?.value.addressLivedLessThan5YearsDetails;
       }
-      if (respondent?.value.address.PostTown) {
-        userCase.citizenUserAddressTown = respondent?.value.address.PostTown;
-      }
-      if (respondent?.value.address.County) {
-        userCase.citizenUserAddressCounty = respondent?.value.address.County;
-      }
-      if (respondent?.value.address.PostCode) {
-        userCase.citizenUserAddressPostcode = respondent?.value.address.PostCode;
-      }
-    }
-    if (respondent?.value.addressLivedLessThan5YearsDetails) {
-      userCase.citizenUserAddressHistory = respondent?.value.addressLivedLessThan5YearsDetails;
-    }
 
-    if (!userCase.citizenUserAddress1 && !userCase.citizenUserAddressTown && !userCase.citizenUserAddressPostcode) {
-      userCase.citizenUserAddressText = '';
-    } else {
-      userCase.citizenUserAddressText = userCase.citizenUserAddress1 + ' ';
-      if (userCase.citizenUserAddress2) {
-        userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddress2 + ' ';
+      if (!userCase.citizenUserAddress1 && !userCase.citizenUserAddressTown && !userCase.citizenUserAddressPostcode) {
+        userCase.citizenUserAddressText = '';
+      } else {
+        userCase.citizenUserAddressText = userCase.citizenUserAddress1 + ' ';
+        if (userCase.citizenUserAddress2) {
+          userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddress2 + ' ';
+        }
+        if (userCase.citizenUserAddressTown) {
+          userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddressTown + ' ';
+        }
+        if (userCase.citizenUserAddressPostcode) {
+          userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddressPostcode;
+        }
       }
-      if (userCase.citizenUserAddressTown) {
-        userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddressTown + ' ';
+      if (YesOrNo.YES === userCase.isAtAddressLessThan5Years) {
+        userCase.citizenUserAddressHistory = '';
       }
-      if (userCase.citizenUserAddressPostcode) {
-        userCase.citizenUserAddressText = userCase.citizenUserAddressText + userCase.citizenUserAddressPostcode;
-      }
-    }
-    if (YesOrNo.YES === userCase.isAtAddressLessThan5Years) {
-      userCase.citizenUserAddressHistory = '';
     }
   });
 }
