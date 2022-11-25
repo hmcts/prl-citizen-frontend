d569939c084f880df0c478d727c10686c3ac6e44
sealDebolina <debolina.seal@hmcts.net>
2022-11-25 12:51:09 +0000

    Welsh for Initial Guidance+Confidentiality+International Element

--- src/main/steps/c100-rebuild/confidentiality/feedback/content.ts
+++ src/main/steps/c100-rebuild/confidentiality/feedback/content.ts
@@ -11,11 +11,11 @@ const en = () => ({
 });
 
 const cy = () => ({
-  caption: 'Keeping your contact details private  - welsh',
-  headingTitle: 'The court will keep your contact details private- welsh ',
-  p1: 'You have told us you want to keep these contact details private - welsh',
-  heading3: 'What the court will do - welsh',
-  p2: 'The court will hold this information securely and will not share it with anyone except Cafcass (Children and Family Court Advisory and Support Service) or Cafcass CYMRU unless it is by order of the court. - welsh',
+  caption: 'Cadw eich manylion cyswllt yn breifat',
+  headingTitle: 'Bydd y llys yn cadw eich manylion cyswllt yn breifat.',
+  p1: "Rydych wedi dweud wrthym eich bod eisiau cadw'r manylion cyswllt yma yn breifat:",
+  heading3: 'Beth fydd y llys yn ei wneud',
+  p2: "Bydd y llys yn cadw'r wybodaeth hon yn ddiogel ac ni fydd yn ei rhannu ag unrhyw un ac eithrio Cafcass (Children and Family Court Advisory and Support Service) neu Cafcass CYMRU oni bai ei fod trwy orchymyn y llys.",
 });
 
 const languages = {
