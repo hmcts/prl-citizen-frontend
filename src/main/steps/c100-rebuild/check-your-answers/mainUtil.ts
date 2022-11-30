fc82bddcdccd7113d1eb21855d70526f1104d70e
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2022-11-30 13:47:19 +0000

    added check your answer cosmetic issue regarding statment of truth

--- src/main/steps/c100-rebuild/check-your-answers/mainUtil.ts
+++ src/main/steps/c100-rebuild/check-your-answers/mainUtil.ts
@@ -86,12 +86,17 @@ export const LegalRepresentativeDetails = (
       value: userCase['sq_legalRepresentation'],
       changeUrl: Urls['C100_SCREENING_QUESTIONS_LEGAL_RESPRESENTATION'],
     },
-    {
-      key: keys['doyouWantLegalRespresentatorToCompleteApplication'],
-      value: userCase['sq_legalRepresentationApplication'],
-      changeUrl: Urls['C100_SCREENING_QUESTIONS_LEGAL_REPRESENTATION_APPLICATION'],
-    },
   ];
+  if(userCase.hasOwnProperty('sq_legalRepresentation') && userCase['sq_legalRepresentation'] === YesOrNo.YES){
+    SummaryData.push(
+      {
+        key: keys['doyouWantLegalRespresentatorToCompleteApplication'],
+        value: userCase['sq_legalRepresentationApplication'],
+        changeUrl: Urls['C100_SCREENING_QUESTIONS_LEGAL_REPRESENTATION_APPLICATION'],
+      },
+    );
+  }
+
   return {
     title: sectionTitles['legalRepresentativeDetails'],
     rows: getSectionSummaryList(SummaryData, content),
@@ -102,12 +107,8 @@ export const PermissionForApplication = (
   { sectionTitles, keys, ...content }: SummaryListContent,
   userCase: Partial<CaseWithId>
 ): SummaryList | undefined => {
+  const valForPermissionWhy = userCase.hasOwnProperty('sq_permissionsWhy') ? (HTML.UNORDER_LIST + userCase['sq_permissionsWhy']?.map(props => HTML.LIST_ITEM + keys[props] + ': ' + userCase[`sq_${props}_subfield`] + HTML.LIST_ITEM_END) + HTML.UNORDER_LIST_END).split(',').join('') : '';
   const SummaryData = [
-    {
-      key: keys['whyCourtGrantSubmittingPermission'],
-      value: userCase['sq_permissionsRequest'],
-      changeUrl: Urls['C100_SCREENING_QUESTIONS_PERMISSIONS_REQUEST'], 
-    },
     {
       key: keys['reasonPermissionRequired'],
       value: userCase['sq_courtPermissionRequired'],
@@ -115,9 +116,13 @@ export const PermissionForApplication = (
     },
     {
       key: keys['whyPermissionRequiredFromCourt'],
-      valueHtml: userCase.hasOwnProperty('sq_permissionsWhy') ? (HTML.UNORDER_LIST + userCase['sq_permissionsWhy']?.map(props => HTML.LIST_ITEM + keys[props] + HTML.LIST_ITEM_END) + HTML.UNORDER_LIST_END).split(',').join('')
-      : '',
+      valueHtml: valForPermissionWhy,
       changeUrl: Urls['C100_SCREENING_QUESTIONS_PERMISSIONS_WHY'],
+    },
+    {
+      key: keys['whyCourtGrantSubmittingPermission'],
+      value: userCase['sq_permissionsRequest'],
+      changeUrl: Urls['C100_SCREENING_QUESTIONS_PERMISSIONS_REQUEST'], 
     },
   ];
   return {
