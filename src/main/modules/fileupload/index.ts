b445f79aa1f94909edd4fae046b0b8850bb41fb8
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2022-12-02 14:55:55 +0000

    fixed document upload for respondent journey and adusted file upload limit setting it to infinite

--- src/main/modules/fileupload/index.ts
+++ src/main/modules/fileupload/index.ts
@@ -7,13 +7,6 @@ import fileUpload from 'express-fileupload';
  */
 export class FileUpload {
   public enableFor(app: Application): void {
-    // const DefaultFileUploadSize = config.get<number>('documentManagement.uploadSizeinMB');
-
-    const DefaultFileUploadSize = 30;
-    app.use(
-      fileUpload({
-        limits: { fileSize: 1024 * 1024 * DefaultFileUploadSize },
-      })
-    );
+    app.use(fileUpload());
   }
 }
