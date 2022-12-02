96a87a70f463bd5e5138ba83bf0d7bf63c8e2a3b
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2022-12-02 12:15:35 +0000

    fixed issues with csurf

--- src/main/modules/csrf/index.ts
+++ src/main/modules/csrf/index.ts
@@ -1,36 +1,21 @@
 import csurf from 'csurf';
 import type { Application } from 'express';
 
-import {
-  C100_CONSENT_ORDER_UPLOAD,
-  C100_MIAM_UPLOAD,
-  C100_OTHER_PROCEEDINGS_DOCUMENT_UPLOAD,
-  CSRF_TOKEN_ERROR_URL,
-} from '../../steps/urls';
+import { CSRF_TOKEN_ERROR_URL } from '../../steps/urls';
 
 export class CSRFToken {
-  private C100CSRFRoutes = [
-    C100_CONSENT_ORDER_UPLOAD,
-    C100_MIAM_UPLOAD,
-    C100_OTHER_PROCEEDINGS_DOCUMENT_UPLOAD,
-    CSRF_TOKEN_ERROR_URL,
-  ];
-
   public enableFor(app: Application): void {
-    app.use(csurf(), (error, req, res, next) => {
-      if (
-        req.method === 'POST' &&
-        this.C100CSRFRoutes.includes(req.originalUrl as `/${string}`) &&
-        req.headers.referer.includes(req.headers.host)
-      ) {
-        if (error) {
-          return next();
-        }
-        return next();
-      } else {
-        res.locals.csrfToken = req.csrfToken();
-        next();
+    app.use(csurf(), (req, res, next) => {
+      res.locals.csrfToken = req.csrfToken();
+      next();
+    });
+
+    app.use((error, req, res, next) => {
+      if (error.code === 'EBADCSRFTOKEN') {
+        console.error(`${error.stack || error}`);
+        return res.redirect(CSRF_TOKEN_ERROR_URL);
       }
+      next();
     });
   }
 }
