988dead58d12ced47f7fe3a8fbe1f69473263295
channa-patil <channabasappagouda.patil@hmcts.net>
2022-12-08 14:55:48 +0000

    c100 rebuild - Fixed to update case in case submit case failures

--- src/main/modules/payments/paymentController.ts
+++ src/main/modules/payments/paymentController.ts
@@ -12,37 +12,52 @@ const SUCCESS = 'Success';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const PaymentHandler = async (req: AppRequest, res: Response) => {
-  const paymentHelperTranspiler = await new PaymentHelper().SystemCredentailsToApiData(req);
-  const { Authorization, ServiceAuthorization, returnUrL, caseId, applicantCaseName, hwfRefNumber } =
-    paymentHelperTranspiler;
-  const paymentApiEndpoint = config.get('payments.url');
-  const createPaymentEndpoint = '/fees-and-payment-apis/create-payment';
-  const baseURL = paymentApiEndpoint + createPaymentEndpoint;
+  try {
+    const paymentHelperTranspiler = await new PaymentHelper().SystemCredentailsToApiData(req);
+    const { Authorization, ServiceAuthorization, returnUrL, caseId, applicantCaseName, hwfRefNumber } =
+      paymentHelperTranspiler;
+    const paymentApiEndpoint = config.get('payments.url');
+    const createPaymentEndpoint = '/fees-and-payment-apis/create-payment';
+    const baseURL = paymentApiEndpoint + createPaymentEndpoint;
 
-  const paymentCreator = new PaymentTaskResolver(
-    baseURL,
-    Authorization,
-    ServiceAuthorization,
-    caseId,
-    returnUrL,
-    applicantCaseName as string,
-    hwfRefNumber as string
-  );
-  const response = await paymentCreator.getPaymentCredentails();
-  req.session.userCase.paymentDetails = response;
-  //if previous payment is success then invoke submit case else redirect gov.uk
-  //if help with fees opted then submit case & redirect to confirmation page
-  if (hwfRefNumber || SUCCESS === response?.status) {
-    await submitCase(
-      req,
-      res,
-      req.session.userCase!.caseId!,
-      req.session.userCase,
-      req.originalUrl,
-      C100_CASE_EVENT.CASE_SUBMIT
+    const paymentCreator = new PaymentTaskResolver(
+      baseURL,
+      Authorization,
+      ServiceAuthorization,
+      caseId,
+      returnUrL,
+      applicantCaseName as string,
+      hwfRefNumber as string
     );
-  } else {
-    res.redirect(response['next_url']);
+    const response = await paymentCreator.getPaymentCredentails();
+    if (response === undefined) {
+      req.locals.logger.error('Error in creating service/payment request');
+      req.session.paymentError = true;
+      req.session.save(() => {
+        res.redirect(C100_CHECK_YOUR_ANSWER);
+      });
+    }
+    req.session.userCase.paymentDetails = response;
+    //if previous payment is success then invoke submit case else redirect gov.uk
+    //if help with fees opted then submit case & redirect to confirmation page
+    if (hwfRefNumber || SUCCESS === response?.status) {
+      submitCase(
+        req,
+        res,
+        req.session.userCase!.caseId!,
+        req.session.userCase,
+        req.originalUrl,
+        C100_CASE_EVENT.CASE_SUBMIT
+      );
+    } else {
+      res.redirect(response['next_url']);
+    }
+  } catch (e) {
+    req.locals.logger.error('Error in payment/submit case ', e);
+    req.session.paymentError = true;
+    req.session.save(() => {
+      res.redirect(C100_CHECK_YOUR_ANSWER);
+    });
   }
 };
 
@@ -66,7 +81,7 @@ export const PaymentValidationHandler = async (req: AppRequest, res: Response) =
         case 'Success':
           req.session.userCase.paymentSuccessDetails = checkPayment['data'];
           //Invoke update case with 'citizen-case-submit' event & reidrect confirmation page
-          await submitCase(
+          submitCase(
             req,
             res,
             req.session.userCase!.caseId!,
@@ -84,6 +99,9 @@ export const PaymentValidationHandler = async (req: AppRequest, res: Response) =
     } catch (error) {
       req.locals.logger.error(error);
       res.status(500);
+      req.session.save(() => {
+        res.redirect(C100_CHECK_YOUR_ANSWER);
+      });
     }
   }
 };
