30b0e28e74fabd2fb9559585a63210e943f665d5
sealDebolina <debolina.seal@hmcts.net>
2022-12-14 18:02:07 +0000

    welsh hyperlink

--- src/main/steps/c100-rebuild/miam/valid-reason/content.ts
+++ src/main/steps/c100-rebuild/miam/valid-reason/content.ts
@@ -27,7 +27,7 @@ export const cy = () => ({
   title: 'A oes gennych chi resymau dilys dros beidio â mynychu MIAM?',
   paragraph1: 'Rhaid i chi fynychu MIAM cyn gwneud cais oni bai bod gennych resymau dilys dros beidio â mynychu.',
   applyForVrLink:
-    'If you\'re unsure, you can check the <a href="https://www.justice.gov.uk/courts/procedure-rules/family/practice_directions/pd_part_03a" class="govuk-link" target="_blank" aria-label="list of valid reasons">list of valid reasons</a>. - Welsh',
+    'Os ydych yn ansicr, gallwch <a href="https://www.justice.gov.uk/courts/procedure-rules/family/practice_directions/pd_part_03a" class="govuk-link" target="_blank" aria-label="list of valid reasons">wirio’r rhestr o resymau dilys</a>.',
   paragraph2:
     'Os ydych chi’n honni bod gennych resymau dilys dros beidio â mynychu MIAM, bydd y llys angen mwy o wybodaeth gennych',
   one: 'Oes',
