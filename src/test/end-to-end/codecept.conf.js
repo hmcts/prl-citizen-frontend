a8ecedc6df6bf2e34d740e25091c655281665711
Elize Reyes <elize.reyes@hmcts.net>
2022-12-22 18:07:56 +0000

    revert codecept.conf

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -1,7 +1,5 @@
-const CitizenLoginPage = require("./pages/C100-Rebuild/CitizenLoginPage");
-
 exports.config = {
-  tests: './tests/C100Rebuild-BaseFlow.js',
+  tests: './tests/*.js',
   output: './output',
   helpers: {
     Puppeteer: {
@@ -27,9 +25,7 @@ exports.config = {
       minTimeout: 2000
     }
   },
-  include: { I: './steps_file.js',
-  CitizenLoginPage: './src/test/end-to-end/pages/C100-Rebuild/CitizenLoginPage.js' },
-  
+  include: { I: './steps_file.js' },
   bootstrap: null,
   mocha: {},
   name: 'prl-ccd-definitions'
