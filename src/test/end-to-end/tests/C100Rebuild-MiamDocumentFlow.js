085674bd946b29db8b194c4813501be2691166d5
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-01-10 18:07:57 +0000

    updated format to codeceptjs 3

--- src/test/end-to-end/tests/C100Rebuild-MiamDocumentFlow.js
+++ src/test/end-to-end/tests/C100Rebuild-MiamDocumentFlow.js
@@ -1,6 +1,6 @@
 Feature('C100 Rebuild - Basic Miam Document flow');
 
-Scenario('C100 Rebuild - basic miam document journey @cross-browser',  async I => {
+Scenario('C100 Rebuild - basic miam document journey @cross-browser',  async ({ I }) => {
   await I.loginAsCitizenUserNamePassWord();
   await I.createC100Application();
   await I.addCaseNameAndPostCode();
