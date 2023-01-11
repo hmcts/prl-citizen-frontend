455e6f4dfbfd200a4ef76dc9fa118861c67d5fbd
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-01-11 19:03:32 +0000

    test baseflow new codeceptjs format

---
+++ codecept.conf.js
@@ -0,0 +1,31 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'prl-citizen-frontend',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
