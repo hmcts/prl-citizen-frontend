26d7f2d992e8b11a35c09d8699e550983046512c
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-01-11 13:44:27 +0000

    Update README.md (#709)
    * code smell
    
    * code review suggestion
    
    * welsh hyperlink
    
    * welsh translation received on 14/12
    
    * C100 rebuild - corrected warning message iconFallbackText
    
    * fixing yarn audit vulnerabilities
    
    * removed curly braces for one of the attribute as its as per typscript the document summary
    
    * 2/12 translation+QA finding
    
    * address found translation
    
    * bug fix+6/1 translation (#731)
    
    * fortify fix
    
    * dependency fix
    
    * Bump axios from 0.27.2 to 1.2.2
    
    Bumps [axios](https://github.com/axios/axios) from 0.27.2 to 1.2.2.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/v1.x/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.27.2...1.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: axios
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * bug fix+6/1 translation
    
    * contact type bug fix
    
    * review comment addressed
    
    * removing strict check
    
    * check you answer screen fix
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Aloknath Datta <alokdatta.uit@gmail.com>
    Co-authored-by: Aloknath Datta <Aloknath.datta@hmcts.net>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: sealDebolina <debolina.seal@hmcts.net>
    Co-authored-by: channa-patil <channabasappagouda.patil@hmcts.net>
    Co-authored-by: giorgi-hmcts <giorgi.gutsaev@hmcts.net>
    Co-authored-by: sealDebolina <116593651+sealDebolina@users.noreply.github.com>
    Co-authored-by: Aloknath Datta <alokdatta.uit@gmail.com>
    Co-authored-by: Aloknath Datta <Aloknath.datta@hmcts.net>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main/steps/c100-rebuild/screening-questions/contact-representative/content.test.ts
+++ src/main/steps/c100-rebuild/screening-questions/contact-representative/content.test.ts
@@ -24,17 +24,17 @@ const en = {
 
 const cy = {
   serviceName: 'Trefniadau plant',
-  title: 'Contact your legal representative - welsh',
-  paragraph: 'If you have a legal representative and want them to complete the application for you: - welsh',
-  closeApplication: 'Close the application - welsh',
+  title: "Cysylltwch â'ch cynrychiolydd cyfreithiol",
+  paragraph: " Os oes gennych chi gynrychiolydd cyfreithiol a bod arnoch eisiau iddynt gwblhau'r cais ar eich rhan:",
+  closeApplication: "Cau'r cais",
   bulletPoints: [
-    'get in touch with your legal representative - welsh',
-    'ask them what information they need from you to complete the application - welsh',
-    'ask them to explain the next steps - welsh',
+    "Cysylltu â'ch cynrychiolydd cyfreithiol",
+    "Gofynnwch iddynt pa wybodaeth sydd ei hangen arnynt i gwblhau'r cais",
+    "Gofynnwch iddynt egluro'r camau nesaf",
   ],
   warningText: {
-    text: 'Do not complete the application yourself if you plan to have a legal representative fill it in. - welsh',
-    iconFallbackText: 'Warning - welsh',
+    text: "Cau'r cais",
+    iconFallbackText: 'Rhybudd',
   },
 };
 
