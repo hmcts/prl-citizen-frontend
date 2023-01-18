2c694527af9dcbef2e01c8389831a7636aff2239
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-01-18 14:44:29 +0000

    Modified smoke tests for health checks

--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -1,6 +1,7 @@
 export type PageLink = `/${string}`;
 
 export const HOME_URL: PageLink = '/';
+export const HEALTH_URL: PageLink = '/health';
 export const CALLBACK_URL: PageLink = '/receiver';
 export const SIGN_IN_URL: PageLink = '/login';
 export const SIGN_OUT_URL: PageLink = '/logout';
