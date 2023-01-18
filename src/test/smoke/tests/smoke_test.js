413b42d0ee0444d1dae85512f49e51afd4af76d3
niranjan-301 <113594061+niranjan-301@users.noreply.github.com>
2023-01-18 15:24:08 +0000

    smoke test failure issue changes

--- src/test/smoke/tests/smoke_test.js
+++
@@ -1,5 +0,0 @@
-Feature('Smoke tests @smoke-tests');
-Scenario('sign in as citizen', async ({ CitizenLoginPage, CreateApplication }) => {
-    await CitizenLoginPage.loginAsCitizenUserNamePassWord();
-    await CreateApplication.createNewC100Application();
-});
