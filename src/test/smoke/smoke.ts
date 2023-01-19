5ce1de475fa303ababe47a2a2f53017c05d50b91
niranjan-301 <113594061+niranjan-301@users.noreply.github.com>
2023-01-19 17:04:03 +0000

    added smoke test for COS And IDAM

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -7,6 +7,12 @@ jest.setTimeout(15000);
 
 const servicesToCheck = [
   { name: 'Private Law', url: process.env.TEST_URL },
+  { name: 'IDAM Web', url: config.get('services.idam.authorizationURL') },
+  { name: 'IDAM API', url: config.get('services.idam.tokenURL') },
+  { name: 'Auth Provider', url: config.get('services.authProvider.url') },
+  { name: 'CCD Data Store', url: config.get('services.case.url') },
+  { name: 'COS', url: config.get('services.cos.url') },
+  { name: 'Document Management', url: config.get('services.documentManagement.url') },
   {
     name: 'Postcode Lookup',
     url: config.get('services.postcodeLookup.url'),
