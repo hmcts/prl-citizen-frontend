928ecb78112b38cfb959a7dee4c33c66f2076f3b
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-01-19 14:55:59 +0000

    smoke text failure fix

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,32 +1,113 @@
+require('./src/test/end-to-end/helpers/event_listener');
+const lodash = require('lodash');
+
+const testConfig = require('./src/test/config.js').config;
 exports.config = {
-  tests: './tests/*.js',
   output: './output',
+  multiple: {
+    parallel: {
+      chunks: files => {
+        const splitFiles = (list, size) => {
+          const sets = [];
+          const chunks = list.length / size;
+          let i = 0;
+
+          while (i < chunks) {
+            sets[i] = list.splice(0, size);
+            i++;
+          }
+          return sets;
+        };
+
+        const buckets = parseInt(process.env.PARALLEL_CHUNKS || '1');
+        const slowTests = lodash.filter(files, file => file.includes('@slow'));
+        const otherTests = lodash.difference(files, slowTests);
+
+        let chunks = [];
+        if (buckets > slowTests.length + 1) {
+          const slowTestChunkSize = 1;
+          const regularChunkSize = Math.ceil((files.length - slowTests.length) / (buckets - slowTests.length));
+          chunks = lodash.union(splitFiles(slowTests, slowTestChunkSize), splitFiles(otherTests, regularChunkSize));
+        } else {
+          chunks = splitFiles(files, Math.ceil(files.length / buckets));
+        }
+
+        console.log(chunks);
+
+        return chunks;
+      },
+    },
+  },
   helpers: {
     Puppeteer: {
-      // headless mode
       show: process.env.SHOW_BROWSER_WINDOW || false,
-      //show: true,
-      url: 'http://localhost:3000',
-      waitForNavigation: ['load', 'domcontentloaded', 'networkidle0'],
-      waitForTimeout: 180000,
-      ignoreHTTPSErrors: true,
+      waitForTimeout: parseInt(process.env.WAIT_FOR_TIMEOUT || '20000'),
       chrome: {
         ignoreHTTPSErrors: true,
-        args: ['--no-sandbox']
+        args: process.env.DISABLE_WEB_SECURITY === 'true' ? ['--disable-web-security'] : [],
+        devtools: process.env.SHOW_BROWSER_WINDOW || false,
       },
-      windowSize: '1280x960'
+      windowSize: '1280x960',
     },
-    PuppeteerHelpers: { require: './src/test/end-to-end/helpers/puppeterHelper.js' }
+    HooksHelper: {
+      require: './src/test/end-to-end/helpers/hooks_helper.js',
+    },
+    BrowserHelpers: {
+      require: './src/test/end-to-end/helpers/browser_helper.js',
+    },
+    DumpBrowserLogsHelper: {
+      require: './src/test/end-to-end/helpers/dump_browser_logs_helper.js',
+    },
+    StepListener: {
+      require: './src/test/end-to-end/helpers/stepListener.js',
+    },
+    Mochawesome: {
+      uniqueScreenshotNames: true,
+    },
+  },
+
+  include: {
+    config: './src/test/end-to-end/config.js',
+    I: './src/test/end-to-end/actors/main.js',
   },
   plugins: {
+    autoLogin: testConfig.AutoLogin,
     retryFailedStep: {
       enabled: true,
-      retries: 2,
-      minTimeout: 2000
-    }
+    },
+    screenshotOnFail: {
+      enabled: true,
+      fullPageScreenshots: true,
+    },
   },
-  include: { I: './steps_file.js' },
-  bootstrap: null,
-  mocha: {},
-  name: 'prl-ccd-definitions'
+  tests: './src/test/end-to-end/tests/*_test.js',
+  teardownAll: require('./src/test/end-to-end/hooks/aggregate-metrics'),
+  teardown: testConfig.teardown,
+  mocha: {
+    reporterOptions: {
+      'codeceptjs-cli-reporter': {
+        stdout: '-',
+        options: {
+          steps: false,
+        },
+      },
+      'mocha-junit-reporter': {
+        stdout: '-',
+        options: {
+          mochaFile: 'test-results/result.xml',
+        },
+      },
+      mochawesome: {
+        stdout: '-',
+        options: {
+          reportDir: './output',
+          inlineAssets: true,
+          json: false,
+        },
+      },
+      '../../src/test/end-to-end/reporters/json-file-reporter/reporter': {
+        stdout: '-',
+      },
+    },
+  },
 };
