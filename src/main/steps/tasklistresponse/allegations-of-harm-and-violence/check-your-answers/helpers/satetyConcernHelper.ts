d57c22415805f926bf8ae5cc3388a27a6bc2e479
Khurshed Khan <khurshed.khan@hmcts.net>
2023-01-27 14:28:52 +0000

    Refactored safety concern code

--- src/main/steps/tasklistresponse/allegations-of-harm-and-violence/check-your-answers/helpers/satetyConcernHelper.ts
+++ src/main/steps/tasklistresponse/allegations-of-harm-and-violence/check-your-answers/helpers/satetyConcernHelper.ts
@@ -18,30 +18,7 @@ export const childNameFormatter = (childId, userCase) => {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const HTMLParser = (keys, FoundElement: ANYTYPE, bodyHtml, userCase, typeOfUser) => {
-  if (typeOfUser === 'child') {
-    bodyHtml += HTML.H4 + keys['childrenConcernedAboutLabel'] + HTML.H4_CLOSE;
-    if (FoundElement.hasOwnProperty('childrenConcernedAbout')) {
-      bodyHtml += HTML.UNORDER_LIST;
-      if (
-        Array.isArray(FoundElement['childrenConcernedAbout']) &&
-        FoundElement['childrenConcernedAbout'][0] === 'All the children in application'
-      ) {
-        bodyHtml += HTML.LIST_ITEM + FoundElement['childrenConcernedAbout'][0] + HTML.LIST_ITEM_END;
-      } else {
-        if (Array.isArray(FoundElement['childrenConcernedAbout'])) {
-          bodyHtml += FoundElement['childrenConcernedAbout']
-            ?.map(childId => childNameFormatter(childId, userCase))
-            .toString()
-            .split(',')
-            .join('');
-        } else {
-          bodyHtml += childNameFormatter(FoundElement['childrenConcernedAbout'], userCase);
-        }
-      }
-      bodyHtml += HTML.UNORDER_LIST_END;
-    }
-    bodyHtml += HTML.RULER;
-  }
+  bodyHtml = getChildBodyHtml(typeOfUser, bodyHtml, keys, FoundElement, userCase);
   bodyHtml += HTML.H4 + keys['behaviourDetailsLabel'] + HTML.H4_CLOSE;
   bodyHtml += HTML.P + FoundElement.hasOwnProperty('behaviourDetails') ? FoundElement['behaviourDetails'] : '';
   bodyHtml += HTML.RULER;
@@ -78,3 +55,32 @@ export const SafetyConcernsHelper = (userCase, keys, sessionKey, childField, typ
   }
   return '';
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getChildBodyHtml(typeOfUser: any, bodyHtml: any, keys: any, FoundElement: any, userCase: any) {
+  if (typeOfUser === 'child') {
+    bodyHtml += HTML.H4 + keys['childrenConcernedAboutLabel'] + HTML.H4_CLOSE;
+    if (FoundElement.hasOwnProperty('childrenConcernedAbout')) {
+      bodyHtml += HTML.UNORDER_LIST;
+      if (
+        Array.isArray(FoundElement['childrenConcernedAbout']) &&
+        FoundElement['childrenConcernedAbout'][0] === 'All the children in application'
+      ) {
+        bodyHtml += HTML.LIST_ITEM + FoundElement['childrenConcernedAbout'][0] + HTML.LIST_ITEM_END;
+      } else {
+        if (Array.isArray(FoundElement['childrenConcernedAbout'])) {
+          bodyHtml += FoundElement['childrenConcernedAbout']
+            ?.map(childId => childNameFormatter(childId, userCase))
+            .toString()
+            .split(',')
+            .join('');
+        } else {
+          bodyHtml += childNameFormatter(FoundElement['childrenConcernedAbout'], userCase);
+        }
+      }
+      bodyHtml += HTML.UNORDER_LIST_END;
+    }
+    bodyHtml += HTML.RULER;
+  }
+  return bodyHtml;
+}
