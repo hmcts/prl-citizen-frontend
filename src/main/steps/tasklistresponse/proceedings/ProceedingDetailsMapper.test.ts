c0f526ddf53775df88172518db60941a5be7cb88
Khurshed Khan <khurshed.khan@hmcts.net>
2023-02-01 19:23:47 +0000

    improved coverage

--- src/main/steps/tasklistresponse/proceedings/ProceedingDetailsMapper.test.ts
+++ src/main/steps/tasklistresponse/proceedings/ProceedingDetailsMapper.test.ts
@@ -30,33 +30,110 @@ describe('ProceedingDetailsMapper', () => {
       currentOrPreviousProceedings: {
         haveChildrenBeenInvolvedInCourtCase: 'No',
         courtOrderMadeForProtection: 'Yes',
-        proceedingsList: [
-          {
-            orderType: 'emergencyProtectionOrder',
-            proceedingDetails: [
+        proceedingsList: {
+          order: {
+            childArrangementOrders: [
               {
-                caseNo: 'test',
-                orderDate: '2021-02-02',
-                currentOrder: YesOrNo.YES,
-                orderEndDate: '2021-02-02',
-                orderCopy: YesOrNo.YES,
-                orderDocument: 'test doc',
+                id: '1',
+                orderDetail: '',
+                caseNo: '',
+                orderDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                currentOrder: '',
+                orderEndDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                orderCopy: '',
+              },
+            ],
+            emergencyProtectionOrders: [
+              {
+                id: '1',
+                orderDetail: '',
+                caseNo: '',
+                orderDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                currentOrder: '',
+                orderEndDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                orderCopy: '',
+                orderDocument: {
+                  url: '',
+                  fileName: '',
+                  binaryUrl: '',
+                },
+              },
+            ],
+            supervisionOrders: [
+              {
+                id: '1',
+                orderDetail: '',
+                caseNo: '',
+                orderDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                currentOrder: '',
+                orderEndDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                orderCopy: '',
+              },
+            ],
+            careOrders: [
+              {
+                id: '1',
+                orderDetail: '',
+                caseNo: '',
+                orderDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                currentOrder: '',
+                orderEndDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                orderCopy: '',
+              },
+            ],
+            childAbductionOrders: [
+              {
+                id: '1',
+                orderDetail: '',
+                caseNo: '',
+                orderDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                currentOrder: '',
+                orderEndDate: {
+                  year: '',
+                  month: '',
+                  day: '',
+                },
+                orderCopy: '',
               },
             ],
           },
-          {
-            orderType: 'careOrder',
-            proceedingDetails: [
-              {
-                caseNo: 'test',
-                orderDate: '2021-02-02',
-                currentOrder: YesOrNo.YES,
-                orderEndDate: '2021-02-02',
-                orderCopy: YesOrNo.NO,
-              },
-            ],
-          },
-        ],
+        },
       },
     };
     respondents[0].value.response = response;
@@ -69,6 +146,7 @@ describe('ProceedingDetailsMapper', () => {
     courtProceedingsOrders.push(orderType1);
     courtProceedingsOrders.push(orderType2);
     req.session.userCase.courtProceedingsOrders = courtProceedingsOrders;
+    req.session.userCase.otherProceedings = respondents[0].value.response.currentOrPreviousProceedings.proceedingsList;
     await setProceedingDetails(req.session.userCase, respondents[0], req);
     expect(respondents[0].value.response.currentOrPreviousProceedings.haveChildrenBeenInvolvedInCourtCase).toEqual(
       'No'
@@ -180,7 +258,46 @@ describe('ProceedingDetailsMapper', () => {
       currentOrPreviousProceedings: {
         haveChildrenBeenInvolvedInCourtCase: 'No',
         courtOrderMadeForProtection: 'Yes',
-        proceedingsList: [],
+        proceedingsList: [
+          {
+            value: {
+              orderType: 'value183',
+              proceedingDetails: [
+                {
+                  value: {
+                    orderDetail: 'value184',
+                    caseNo: 'value185',
+                    orderDate: 'value186',
+                    currentOrder: 'value187',
+                    orderEndDate: 'value188',
+                    orderCopy: 'value189',
+                    orderDocument: 'document',
+                  },
+                },
+                {
+                  value: {
+                    orderDetail: 'value190',
+                    caseNo: 'value191',
+                    orderDate: 'value192',
+                    currentOrder: 'value193',
+                    orderEndDate: 'value194',
+                    orderCopy: 'value195',
+                  },
+                },
+                {
+                  value: {
+                    orderDetail: 'value196',
+                    caseNo: 'value197',
+                    orderDate: 'value198',
+                    currentOrder: 'value199',
+                    orderEndDate: 'value200',
+                    orderCopy: 'value201',
+                  },
+                },
+              ],
+            },
+          },
+        ],
       },
     };
     respondents[0].value.response = response;
