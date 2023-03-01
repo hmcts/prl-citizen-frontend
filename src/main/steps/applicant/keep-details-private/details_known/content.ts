fe57e8faee4ee14b657ec452a9a403a2e55063bc
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2023-03-01 12:24:38 +0000

    added common for details-known

--- src/main/steps/applicant/keep-details-private/details_known/content.ts
+++ src/main/steps/applicant/keep-details-private/details_known/content.ts
@@ -1,76 +1,23 @@
-import { YesNoDontKnow } from '../../../../app/case/definition';
 import { TranslationFn } from '../../../../app/controller/GetController';
-import { FormContent } from '../../../../app/form/Form';
-import { isFieldFilledIn } from '../../../../app/form/validation';
+import { en as english, form, cy as welsh } from '../../../common/keep-details-private/details_known/content';
 
 const en = {
-  section: 'Keeping your contact details private',
+  ...english,
   title: 'Does the other person named in your application (the respondent) know any of your contact details?',
   line2:
     'Your application will be shared with the other person in the case (the respondent). This includes your contact details, unless you ask the court not to share these details.',
-  one: 'Yes',
-  two: 'No',
-  three: "I don't know",
-  threeHint: 'This is a 8 character code',
-  summaryText: 'Contacts for help',
-  continue: 'Save and continue',
-  errors: {
-    detailsKnown: {
-      required: 'Enter your details known',
-    },
-  },
 };
 
 const cy: typeof en = {
-  section: 'Keeping your contact details private',
+  ...welsh,
   title: 'Does the other person named in your application (the respondent) know any of your contact details?',
   line2:
     'Your application will be shared with the other person in the case (the respondent). This includes your contact details, unless you ask the court not to share these details.',
-  one: 'Yes',
-  two: 'No',
-  three: "I don't know",
-  threeHint: 'This is a 8 character code',
-  summaryText: 'Contacts for help',
-  continue: 'Save and continue',
-  errors: {
-    detailsKnown: {
-      required: 'Enter your details known',
-    },
-  },
 };
 
 const languages = {
   en,
   cy,
-};
-
-export const form: FormContent = {
-  fields: {
-    detailsKnown: {
-      type: 'radios',
-      classes: 'govuk-radios',
-      label: l => l.label,
-      section: l => l.section,
-      values: [
-        {
-          label: l => l.one,
-          value: YesNoDontKnow.yes,
-        },
-        {
-          label: l => l.two,
-          value: YesNoDontKnow.no,
-        },
-        {
-          label: l => l.three,
-          value: YesNoDontKnow.dontKnow,
-        },
-      ],
-      validator: isFieldFilledIn,
-    },
-  },
-  submit: {
-    text: l => l.continue,
-  },
 };
 
 export const generateContent: TranslationFn = content => {
