7d7975599689e10933f616fd8e84b971c3db3c18
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-03-01 11:25:27 +0000

    Functional test changes for account dashboard

--- src/test/end-to-end/pages/C100-Rebuild/CreateApplication.js
+++ src/test/end-to-end/pages/C100-Rebuild/CreateApplication.js
@@ -4,9 +4,11 @@ const retryCount = 3;
 module.exports = {
 
   async clickCreateNewCApplication() {
+    await I.retry(retryCount).waitForText('Child arrangements and family injunction cases');
     await I.retry(retryCount).waitForText('New child arrangements application (C100)');
     await I.retry(retryCount).click('New child arrangements application (C100)');
     I.wait('2');
+    await I.retry(retryCount).waitForText('You have not started your application');
   },
 
   async createNewC100Application() {
