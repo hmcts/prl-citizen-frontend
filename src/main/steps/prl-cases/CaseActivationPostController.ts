7a3911727faef1cece8cd3952701e45342b707b2
channa-patil <channabasappagouda.patil@hmcts.net>
2023-03-23 10:47:44 +0000

    Fixed sonar bug to resolve build issue

--- src/main/steps/prl-cases/CaseActivationPostController.ts
+++ src/main/steps/prl-cases/CaseActivationPostController.ts
@@ -1,4 +1,3 @@
-// import { CosApiClient } from '../../app/case/CosApiClient';
 import autobind from 'autobind-decorator';
 import type { Response } from 'express';
 
@@ -6,7 +5,7 @@ import { CaseType, PartyType } from '../../app/case/definition';
 import { AppRequest } from '../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../app/controller/PostController';
 import { FormFields, FormFieldsFn } from '../../app/form/Form';
-import { C100_APPLICANT_TASKLIST, RESPONDENT_TASK_LIST_URL } from '../../steps/urls';
+import { APPLICANT_TASK_LIST_URL, C100_APPLICANT_TASKLIST, RESPONDENT_TASK_LIST_URL } from '../../steps/urls';
 
 import { getCasePartyType } from './dashboard/utils';
 
@@ -21,18 +20,14 @@ export class CaseActivationPostController extends PostController<AnyObject> {
       let redirectUrl;
       const partyType = getCasePartyType(req.session.userCase, req.session.user.id);
 
-      if (req.session.userCase.caseTypeOfApplication === CaseType.C100) {
-        if (partyType === PartyType.APPLICANT) {
+      if (partyType === PartyType.APPLICANT) {
+        if (req.session.userCase.caseTypeOfApplication === CaseType.C100) {
           redirectUrl = C100_APPLICANT_TASKLIST;
         } else {
-          redirectUrl = RESPONDENT_TASK_LIST_URL;
+          redirectUrl = APPLICANT_TASK_LIST_URL;
         }
       } else {
-        if (partyType === PartyType.APPLICANT) {
-          redirectUrl = C100_APPLICANT_TASKLIST;
-        } else {
-          redirectUrl = RESPONDENT_TASK_LIST_URL;
-        }
+        redirectUrl = RESPONDENT_TASK_LIST_URL;
       }
       req.session.save(() => res.redirect(redirectUrl));
     } catch (err) {
