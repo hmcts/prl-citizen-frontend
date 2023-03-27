66e3ae67f106d2de6d70f36cba8d05ee07d71b8e
vivek-sekhar <vivek.sekhar@hmcts.net>
2023-03-27 12:44:05 +0100

    Fix application submitted complete status in progress bar

--- src/main/steps/common/task-list/components/progress-bar/utils.ts
+++ src/main/steps/common/task-list/components/progress-bar/utils.ts
@@ -80,7 +80,13 @@ const progressBarConfig = {
         ...progressBarStage.applicationSubmitted,
         isComplete: (caseData: Partial<CaseWithId>) =>
           caseData &&
-          ![State.CASE_DRAFT, State.CASE_SUBMITTED_NOT_PAID, State.CASE_SUBMITTED_PAID].includes(caseData.state!),
+          ![
+            State.CASE_DRAFT,
+            State.CASE_SUBMITTED_NOT_PAID,
+            State.CASE_SUBMITTED_PAID,
+            State.CASE_ISSUED_TO_LOCAL_COURT,
+            State.CASE_GATE_KEEPING,
+          ].includes(caseData.state!),
         isInProgress: (caseData: Partial<CaseWithId>) => caseData && caseData.state !== State.CASE_DRAFT,
       },
       progressBarStage.cafcassSafetyChecks,
