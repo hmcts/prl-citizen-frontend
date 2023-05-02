eb59389ca7c7838f0f86c93236c22b55504bfc2e
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2023-05-02 12:46:57 +0100

    added ccd and cos prs

--- src/main/steps/tasklistresponse/proceedings/ProceedingDetailsMapper.test.ts
+++ src/main/steps/tasklistresponse/proceedings/ProceedingDetailsMapper.test.ts
@@ -457,7 +457,7 @@ describe('ProceedingDetailsMapper', () => {
   });
 
   test('Should return value for providing a date for getDisplayDate', () => {
-    const retunrn_date = { day: undefined, month: undefined, year: 'April' };
+    const retunrn_date = { day: undefined, month: undefined, year: 'May' };
     expect(getDisplayDate(new Date())).toEqual(retunrn_date);
   });
 
