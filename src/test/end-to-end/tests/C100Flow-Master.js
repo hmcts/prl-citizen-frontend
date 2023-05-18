96a197589f26b1b36b56a44af2e15bd39a6c1d4c
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-05-18 17:56:36 +0100

    cleanup test for master

--- src/test/end-to-end/tests/C100Flow-Master.js
+++ src/test/end-to-end/tests/C100Flow-Master.js
@@ -5,18 +5,4 @@ Scenario('C100 Citizen Basic Flow - Master @cross-browser @master', async ({ I }
     await I.createC100Application();
     await I.startTheApplication();
     await I.addCaseNameAndPostCode();
-    // await I.screeningQuestions();
-    // await I.miamSignedDocument();
-    // await I.typeOfOrder();
-    // await I.urgencyWithoutNotice();
-    // await I.childrenDetails();
-    // await I.applicantDetails();
-    // await I.respondentDetails();
-    // await I.otherPersonDetails();
-    // await I.otherProceedings();
-    // await I.safetyConcerns();
-    // await I.internationElements();
-    // await I.reasonableAdjustments();
-    // await I.helpWithFeeEvent();
-    // await I.checkYourAnswersEvent();
   }).retry({ retries: 3, minTimeout: 30000 });
