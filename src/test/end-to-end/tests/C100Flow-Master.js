c5d7e576d30a8b65b546b1c5ca6a6a66853820e9
Jahnavi <jahnavi1satya@gmail.com>
2023-05-29 14:30:30 +0100

    Delete src/test/end-to-end directory

--- src/test/end-to-end/tests/C100Flow-Master.js
+++
@@ -1,8 +0,0 @@
-Feature('C100 Citizen Basic Flow - Master');
-
-Scenario('C100 Citizen Basic Flow - Master @cross-browser', async ({ I }) => {
-    await I.loginAsCitizen();
-    await I.createC100Application();
-    await I.startTheApplication();
-    await I.addCaseNameAndPostCode();
-  }).retry({ retries: 3, minTimeout: 30000 });
