5c88c499e70fe89a64bcfe16299437e4a63043f4
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2023-06-19 12:03:29 +0100

    added continue

--- src/main/steps/tasklistresponse/support-you-need-during-case/summary/content.test.ts
+++ src/main/steps/tasklistresponse/support-you-need-during-case/summary/content.test.ts
@@ -51,7 +51,7 @@ const cy: typeof en = {
   sectionTitles: {
     aboutYou: 'Amdanoch chi',
   },
-  edit: 'Edit -welsh',
+  edit: 'Golygu',
   keys: {
     attendingToCourt: 'A fyddech chi’n gallu cymryd rhan mewn gwrandawiadau drwy fideo a dros y ffôn?',
     hearingDetails: 'Rhowch fanylion',
