a8ef65909d9775a8400d2592e88d91a90396616e
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2023-06-20 15:03:03 +0100

    added loggers for auth token and service token

--- src/main/app/case/CosApiClient.ts
+++ src/main/app/case/CosApiClient.ts
@@ -1,6 +1,7 @@
 import Axios, { AxiosInstance, AxiosResponse } from 'axios';
 import config from 'config';
 import FormData from 'form-data';
+import type { LoggerInstance } from 'winston';
 
 import { DeleteDocumentRequest } from '../../app/document/DeleteDocumentRequest';
 import { DocumentDetail } from '../../app/document/DocumentDetail';
@@ -20,6 +21,9 @@ import {
   YesOrNo,
 } from './definition';
 import { fromApiFormat } from './from-api-format';
+
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger: LoggerInstance = Logger.getLogger('server');
 
 export class CosApiClient {
   client: AxiosInstance;
@@ -139,6 +143,11 @@ export class CosApiClient {
           accessCode: 'Dummy accessCode',
         },
       });
+      const serviceauth = getServiceAuthToken();
+      logger.info(`Auth Token ****** ${user.accessToken} *******`);
+      logger.info(`service Token ****** ${serviceauth} *******`);
+      console.log(`Auth Token ****** ${user.accessToken} *******`);
+      console.log(`Auth Token ****** ${serviceauth} *******`);
 
       return { id: response.data.id, state: response.data.state, ...fromApiFormat(response.data) };
     } catch (err) {
