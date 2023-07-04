da1c18e09151385bb2ade9a1a22fb946e1bbce76
Jahnavi <jahnavi1satya@gmail.com>
2023-07-04 22:46:56 +0100

    Update C100RespondentAboutYou-Flow.js

--- src/test/end-to-end/tests/C100RespondentAboutYou-Flow.js
+++ src/test/end-to-end/tests/C100RespondentAboutYou-Flow.js
@@ -1,7 +1,6 @@
 Feature('C100 Respondent About You - flow');
 
 Scenario('C100 Respondent About You journey @cross-browser', async ({ I }) => {
-    await I.loginAsCitizen();
-    
+    await I.loginAsPRLCitizen();
     await I.respondentAboutYou();
   }).retry({ retries: 3, minTimeout: 30000 });
