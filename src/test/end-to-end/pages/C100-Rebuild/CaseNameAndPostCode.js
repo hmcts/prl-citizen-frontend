3b0eb3504879f837b26a88584a20106f479f9e0a
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-07-07 16:44:39 +0100

    FT test issues

--- src/test/end-to-end/pages/C100-Rebuild/CaseNameAndPostCode.js
+++ src/test/end-to-end/pages/C100-Rebuild/CaseNameAndPostCode.js
@@ -19,6 +19,7 @@ module.exports = {
     await I.wait('4');
   },
   async enterCaseName() {
+    await I.wait('4');
     await I.retry(retryCount).waitForText('Enter Case Name' , 60);
     await I.wait('4');
     await I.retry(retryCount).fillField(this.fields.caseName, this.fields.caseNameDetails);
