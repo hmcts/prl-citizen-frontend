8b10fdd0c48f76821eccde1924f9bb2177934df8
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-07-11 12:39:52 +0100

    fix ft failure

--- src/test/end-to-end/pages/C100-Rebuild/MiniDashboard.js
+++ src/test/end-to-end/pages/C100-Rebuild/MiniDashboard.js
@@ -4,13 +4,12 @@ const retryCount = 3;
 module.exports = {
 
   async clickStartTheApplication() {
-    await I.wait('6');
     await I.retry(retryCount).waitForText('Start the application' , 60);
     await I.wait('2');
     await I.retry(retryCount).click('Start the application');
     await I.wait('8');
   },
-  
+
   async createStartTheApplication() {
     await this.clickStartTheApplication();
   }
