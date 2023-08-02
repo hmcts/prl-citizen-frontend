12d9ff1ac211cd2aea581cc68ef49095a204fbf0
SasankAchyuta9 <VenkataSaiMadhavaSasank.Achyuta@HMCTS.NET>
2023-08-02 10:29:56 +0100

    added test cases for PRL-4134

--- src/main/steps/common/upload-document/util.ts
+++ src/main/steps/common/upload-document/util.ts
@@ -10,116 +10,126 @@ interface DocumentMeta {
   type: string;
 }
 
-
-const getPositionStatementsDocInfo = (type: DocType, documentCategory: Record<string, string>, documentType: Record<string, string>): DocumentMeta => {
+const getPositionStatementsDocInfo = (
+  type: DocType,
+  documentCategory: Record<string, string>,
+  documentType: Record<string, string>
+): DocumentMeta => {
   const docInfo = {
     category: '',
-    type: ''
+    type: '',
   };
 
   switch (type) {
     case DocType.POSITION_STATEMENTS: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.your_position_statements
-      })
+        type: documentType.your_position_statements,
+      });
       break;
     }
     case DocType.YOUR_WITNESS_STATEMENTS: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.your_witness_statements
-      })
+        type: documentType.your_witness_statements,
+      });
       break;
     }
     case DocType.OTHER_PEOPLE_WITNESS_STATEMENTS: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.other_witness_statements
-      })
+        type: documentType.other_witness_statements,
+      });
       break;
     }
     case DocType.MEDIA_FILES: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.mail_screenshots_media_files
-      })
+        type: documentType.mail_screenshots_media_files,
+      });
       break;
-    } case DocType.MEDICAL_RECORDS: {
+    }
+    case DocType.MEDICAL_RECORDS: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.medical_records
-      })
+        type: documentType.medical_records,
+      });
       break;
-    } case DocType.LETTERS_FROM_SCHOOL: {
+    }
+    case DocType.LETTERS_FROM_SCHOOL: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.letters_from_school
-      })
+        type: documentType.letters_from_school,
+      });
       break;
-    } case DocType.TENANCY_AND_MORTGAGE_AVAILABILITY: {
+    }
+    case DocType.TENANCY_AND_MORTGAGE_AVAILABILITY: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-        type: documentType.tenancy_mortgage_agreements
-      })
+        type: documentType.tenancy_mortgage_agreements,
+      });
       break;
     }
     default: {
       Object.assign(docInfo, {
         category: documentCategory.witness_statements_and_evidence,
-      })
+      });
       break;
     }
   }
 
   return docInfo;
-}
+};
 
-const getExpertReportsDocInfo = (type: DocType, documentCategory: Record<string, string>, documentType: Record<string, string>): DocumentMeta => {
+const getExpertReportsDocInfo = (
+  type: DocType,
+  documentCategory: Record<string, string>,
+  documentType: Record<string, string>
+): DocumentMeta => {
   const docInfo = {
     category: '',
-    type: ''
+    type: '',
   };
 
   switch (type) {
     case DocType.MEDICAL_REPORTS: {
       Object.assign(docInfo, {
         category: documentCategory.expert_reports,
-        type: documentType.medical_reports
-      })
+        type: documentType.medical_reports,
+      });
       break;
     }
     case DocType.PATERNITY_TEST_REPORTS: {
       Object.assign(docInfo, {
         category: documentCategory.expert_reports,
-        type: documentType.paternity_test_reports
-      })
+        type: documentType.paternity_test_reports,
+      });
       break;
     }
     case DocType.DRUG_ALCOHOL_TESTS: {
       Object.assign(docInfo, {
         category: documentCategory.expert_reports,
-        type: documentType.drug_and_alcohol_tests
-      })
+        type: documentType.drug_and_alcohol_tests,
+      });
       break;
     }
     case DocType.POLICE_REPORTS: {
       Object.assign(docInfo, {
         category: documentCategory.expert_reports,
-        type: documentType.police_reports
-      })
+        type: documentType.police_reports,
+      });
       break;
     }
     default: {
       Object.assign(docInfo, {
         category: documentCategory.expert_reports,
-      })
+      });
       break;
     }
   }
 
   return docInfo;
-}
+};
 
 export const getDocumentMeta = (category: DocCategory, type: DocType, language: string): DocumentMeta => {
   const meta = {
@@ -130,11 +140,10 @@ export const getDocumentMeta = (category: DocCategory, type: DocType, language: 
   const documentType = language === 'en' ? documents_list_items_en : documents_list_items_cy;
 
   switch (category) {
-    case DocCategory.WITNESS_STATEMENT:
-      {
-        Object.assign(meta, { ...getPositionStatementsDocInfo(type, documentCategory, documentType) });
-        break;
-      }
+    case DocCategory.WITNESS_STATEMENT: {
+      Object.assign(meta, { ...getPositionStatementsDocInfo(type, documentCategory, documentType) });
+      break;
+    }
     case DocCategory.APPLICATIONS: {
       if (type === DocType.PREVIOUS_ORDERS) {
         Object.assign(meta, {
