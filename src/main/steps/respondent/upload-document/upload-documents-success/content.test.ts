054ddaac150b1fef16970a255852da58f687e731
sealDebolina <debolina.seal@hmcts.net>
2023-08-04 12:10:49 +0100

    addressing review comment

--- src/main/steps/respondent/upload-document/upload-documents-success/content.test.ts
+++ src/main/steps/respondent/upload-document/upload-documents-success/content.test.ts
@@ -6,32 +6,32 @@ import { generateContent } from './content';
 //const docsEmail = 'test';
 
 const en = {
-  section: 'Document submitted',
-  title: 'Important',
-  text: 'Serve the documents',
+  pageTitle: 'Document submitted',
+  bannerHeading: 'Important',
+  bannerTitle: 'Serve the documents',
   status: 'Your documents have been uploaded',
-  para1:
+  paragraphs: [
     "If you haven't requested this document is restricted, it is your responsibility to share it with the other people in the case.",
-  para2:
     "You can do this by sending to the party's legal representative if they have one or otherwise the party themselves by post or email.",
-  restrictedHeading: 'If you have requested this document to be restricted',
-  restrictedBody:
+  ],
+  pageCaption: 'If you have requested this document to be restricted',
+  pageContent:
     'If you have requested this document is restricted, the court will review your request and let you know what happens next.',
   continue: 'Close and return to case overview',
   uploadAgain: 'Upload another document',
 };
 
 const cy: typeof en = {
-  section: 'Document submitted -welsh',
-  title: 'Important -welsh',
-  text: 'Serve the documents -welsh',
+  pageTitle: 'Document submitted -welsh',
+  bannerHeading: 'Important -welsh',
+  bannerTitle: 'Serve the documents -welsh',
   status: 'Your documents have been uploaded -welsh',
-  para1:
+  paragraphs: [
     "If you haven't requested this document is restricted, it is your responsibility to share it with the other people in the case. -welsh",
-  para2:
     "You can do this by sending to the party's legal representative if they have one or otherwise the party themselves by post or email. -welsh",
-  restrictedHeading: 'If you have requested this document to be restricted -welsh',
-  restrictedBody:
+  ],
+  pageCaption: 'If you have requested this document to be restricted -welsh',
+  pageContent:
     'If you have requested this document is restricted, the court will review your request and let you know what happens next. -welsh',
   continue: 'Close and return to case overview -welsh',
   uploadAgain: 'Llwytho dogfen arall',
