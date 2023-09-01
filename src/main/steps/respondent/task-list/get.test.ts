3db0018e9e13c8fae5b51bf92db6f244c30200fc
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2023-09-01 10:47:55 +0100

    Add test cases

--- src/main/steps/respondent/task-list/get.test.ts
+++ src/main/steps/respondent/task-list/get.test.ts
@@ -1,15 +1,24 @@
+import axios from 'axios';
+
 import { mockRequest } from '../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../test/unit/utils/mockResponse';
 import mockUserCase from '../../../../test/unit/utils/mockUserCase';
+import { CaseWithId } from '../../../app/case/case';
 import { Applicant, PartyDetails, Respondent } from '../../../app/case/definition';
 
 import { RespondentTaskListGetController } from './get';
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+mockedAxios.create = jest.fn(() => mockedAxios);
+
 describe('RespondentTaskListGetController', () => {
   const controller = new RespondentTaskListGetController();
+  let caseHearingData;
+  let req;
 
-  test('Should render the RespondentTaskList page for private law service', async () => {
-    const req = mockRequest({
+  beforeEach(() => {
+    req = mockRequest({
       session: {
         user: {
           id: '123',
@@ -39,8 +48,36 @@ describe('RespondentTaskListGetController', () => {
         },
       },
     });
+    caseHearingData = {
+      hmctsServiceCode: 'MOCK_CODE',
+      caseRef: 'MOCK_REF',
+      caseHearings: [
+        {
+          hearingID: 1,
+        },
+      ],
+    };
+  });
+
+  test('Should render the RespondentTaskList page for private law service', async () => {
+    mockedAxios.post.mockReturnValueOnce({ data: caseHearingData } as unknown as Promise<CaseWithId>);
     const res = mockResponse();
-    await controller.get(req, res);
-    expect(res.render).toBeCalled;
+    await controller.load(req, res);
+    expect(req.session.userCase.hearingCollection).toStrictEqual(caseHearingData.caseHearings);
+    expect(req.session.applicationSettings.navfromRespondToApplication).toBe(false);
+    expect(req.session.save).toHaveBeenCalled();
+  });
+
+  test('Errors when retrieving hearings should be caught and thrown', async () => {
+    mockedAxios.post.mockRejectedValueOnce;
+    const res = mockResponse();
+    let flag;
+
+    try {
+      await controller.load(req, res);
+    } catch {
+      flag = false;
+    }
+    expect(flag).toEqual(false);
   });
 });
