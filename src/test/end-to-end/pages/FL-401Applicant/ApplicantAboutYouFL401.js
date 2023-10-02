5f6db3b8b1b60af12627f9f0b702165e12a569c6
Jahnavi <jahnavi1satya@gmail.com>
2023-10-02 18:36:25 +0100

    Update ApplicantAboutYouFL401.js

--- src/test/end-to-end/pages/FL-401Applicant/ApplicantAboutYouFL401.js
+++ src/test/end-to-end/pages/FL-401Applicant/ApplicantAboutYouFL401.js
@@ -8,7 +8,20 @@ module.exports = {
         dateofBirthDay: '11',
         dateofBirthMonth: '12',
         dateofBirthYear: '2019',
-        permissionFromCourt: 'test'
+        permissionFromCourt: 'test',
+        keepYourDetailsPrivate: '#keep-your-details-private',
+        yes: '#detailsKnown',
+        saveAndContinue : '#main-form-submit',
+        startAlternative: '#startAlternative',
+        contactDetailsPrivate:  '#contactDetailsPrivate',
+        contactDetailsPrivate-2:  '#contactDetailsPrivate-2',
+        contactDetailsPrivate-3: '#contactDetailsPrivate-3',
+        attendingToCourt: '#attendingToCourt',
+       languageRequirements: '#languageRequirements',
+       reasonableAdjustments-2: '#reasonableAdjustments-2',
+       helpCommunication-2: '#helpCommunication-2',
+       safetyArrangements-2: '#safetyArrangements-2'
+      
     },
   async clickFL401Link() {
     try {
@@ -46,176 +59,72 @@ module.exports = {
    await I.wait('2');
   },
 
-  async clickRespondToApplication() {
-   await I.wait('2');
-    await I.retry(retryCount).click('#respond_to_application');
-   await I.wait('2');
-   },
-
-   async clickRespondConsentToApplication() {
-   await I.wait('2');
-    await I.retry(retryCount).click('#consent-to-the-application');
-   await I.wait('2');
-    await I.retry(retryCount).click('#doYouConsent');
-    await I.retry(retryCount).fillField("#applicationReceivedDate-day", this.fields.dateofBirthDay);
-    await I.retry(retryCount).fillField("#applicationReceivedDate-month", this.fields.dateofBirthMonth);
-    await I.retry(retryCount).fillField("#applicationReceivedDate-year", this.fields.dateofBirthYear);
-    await I.retry(retryCount).click('#courtPermission');
-    await I.retry(retryCount).fillField("#courtOrderDetails", this.fields.permissionFromCourt);
-    await I.retry(retryCount).click('#main-form > div:nth-child(5) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
-   },
+  
 
    async clickYourDetailsPrivate(){
     await I.wait('2');
-    await I.retry(retryCount).click('#keep-your-details-private');
+    await I.retry(retryCount).click(this.fields.keepYourDetailsPrivate);
     await I.wait('2');
-    await I.retry(retryCount).click('#detailsKnown');
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
+    await I.retry(retryCount).waitForText('Does the other person named in your application (the respondent) know any of your contact details?'); 
+    await I.retry(retryCount).click(this.fields.yes);
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
     await I.wait('2');
-    await I.retry(retryCount).click('#startAlternative');
-    await I.seeCheckboxIsChecked('#contactDetailsPrivate') || await I.checkOption('#contactDetailsPrivate');
-    await I.seeCheckboxIsChecked ('#contactDetailsPrivate-2') || await I.checkOption('#contactDetailsPrivate-2');
-    await I.seeCheckboxIsChecked ('#contactDetailsPrivate-3') || await I.checkOption('#contactDetailsPrivate-3');
+    await I.retry(retryCount).waitForText('Do you want to keep your contact details private from the other person named in the application (the'); 
+    await I.retry(retryCount).click(this.fields.startAlternative);
+    await I.seeCheckboxIsChecked(this.fields.contactDetailsPrivate) || await I.checkOption(this.fields.contactDetailsPrivate);
+    await I.seeCheckboxIsChecked (this.fields.contactDetailsPrivate-2) || await I.checkOption(this.fields.contactDetailsPrivate-2);
+    await I.seeCheckboxIsChecked (this.fields.contactDetailsPrivate-3) || await I.checkOption(this.fields.contactDetailsPrivate-3);
     await I.wait('5');
-    await I.retry(retryCount).click('#main-form-submit');
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
     await I.wait('5');
-    await I.retry(retryCount).click('#main-form-submit');
+     await I.retry(retryCount).waitForText('The court will keep your contact details private');
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
    },
 
    async clickEditContactDetails(){
     await I.wait('5');
     await I.retry(retryCount).click('#confirm-or-edit-your-contact-details');
     await I.wait('5');
+    await I.retry(retryCount).waitForText('Place of birth');
     await I.retry(retryCount).click('#main-form > dl > div:nth-child(3) > dd.govuk-summary-list__actions > a');
     await I.wait('2');
     await I.retry(retryCount).fillField(this.fields.placeOfBirth, this.fields.placeofBirthValue);
     await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
     await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
    },
 
    async  clickAboutYouSupportYourNeed(){
     await I.wait('2');
     await I.retry(retryCount).click('#support_you_need_during_your_case');
     await I.wait('2');
-    await I.seeCheckboxIsChecked ('#attendingToCourt') || await I.checkOption('#attendingToCourt');
-    await I.retry(retryCount).click('#main-form > div:nth-child(7) > div > button');
+    await I.retry(retryCount).waitForText('Would you be able to take part in hearings by video and phone?');
+    await I.seeCheckboxIsChecked (this.fields.attendingToCourt) || await I.checkOption(this.fields.attendingToCourt);
+    await I.retry(retryCount).click('Continue');
     await I.wait("2");
-    await I.seeCheckboxIsChecked ('#languageRequirements') || await I.checkOption('#languageRequirements');
-    await I.retry(retryCount).click('#main-form > div:nth-child(8) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#safetyArrangements-2') || await I.checkOption('#safetyArrangements-2');
-   // await I.retry(retryCount).click('#main-form > div:nth-child(12) > div > button');
-   await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#reasonableAdjustments-2') || await I.checkOption('#reasonableAdjustments-2');
-    await I.retry(retryCount).click('#main-form > div:nth-child(10) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#docsSupport-3') || await I.checkOption('#docsSupport-3');
+    await I.retry(retryCount).waitForText('Do you have any language requirements?');
+    await I.seeCheckboxIsChecked (this.fields.languageRequirements) || await I.checkOption(this.fields.languageRequirements);
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
-    await I.seeCheckboxIsChecked ('#helpCommunication-3') || await I.checkOption('#helpCommunication-3');
+    await I.retry(retryCount).waitForText('Do you have a physical, mental or learning disability or health condition that means you need suppor');  
+    await I.seeCheckboxIsChecked (this.fields.reasonableAdjustments-2) || await I.checkOption(this.fields.reasonableAdjustments-2);
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
-    await I.seeCheckboxIsChecked ('Assistance / guide dog') || await I.checkOption('Assistance / guide dog');
+    await I.retry(retryCount).waitForText('I need help communicating and understanding');    
+    await I.seeCheckboxIsChecked (this.fields.helpCommunication-2) || await I.checkOption(this.fields.helpCommunication-2);
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
-    await I.seeCheckboxIsChecked ('#courtComfort-2') || await I.checkOption('#courtComfort-2');
-    await I.retry(retryCount).click('Continue');
-   await I.wait('2');
-    await I.seeCheckboxIsChecked ('#travellingToCourt-3') || await I.checkOption('#travellingToCourt-3');
+    await I.retry(retryCount).waitForText('Do you or the children need special safety arrangements at court?');     
+    await I.seeCheckboxIsChecked (this.fields.safetyArrangements-2) || await I.checkOption(this.fields.safetyArrangements-2);
     await I.retry(retryCount).click('Continue');
     await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
-   },
-//About you section ---- three for C100
 
-   async clickRespondYourDetailsPrivate(){
-    await I.wait('2');
-    await I.retry(retryCount).click('#keep-your-details-private');
-   await I.wait('2');
-    await I.retry(retryCount).click('#detailsKnown');
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#startAlternative');
-    await I.seeCheckboxIsChecked ('#contactDetailsPrivate') || await I.checkOption('#contactDetailsPrivate');
-    await I.seeCheckboxIsChecked ('#contactDetailsPrivate-2') || await I.checkOption('#contactDetailsPrivate-2');
-    await I.seeCheckboxIsChecked ('#contactDetailsPrivate-3') || await I.checkOption('#contactDetailsPrivate-3');
-    await I.retry(retryCount).click('#main-form-submit');
-   await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
+    await I.retry(retryCount).click(this.fields.saveAndContinue);
    },
 
-   async clickRespondYourDetailsSupportYourNeed(){
-   await I.wait('2');
-    await I.retry(retryCount).click('#support_you_need_during_your_case');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#attendingToCourt') || await I.checkOption('#attendingToCourt');
-    await I.retry(retryCount).click('#main-form > div:nth-child(7) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#languageRequirements') || await I.checkOption('#languageRequirements');
-    await I.retry(retryCount).click('#main-form > div:nth-child(8) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#safetyArrangements-2') || await I.checkOption('#safetyArrangements-2');
-    await I.retry(retryCount).click('#main-form > div:nth-child(12) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#reasonableAdjustments-2') || await I.checkOption('#reasonableAdjustments-2');
-    await I.retry(retryCount).click('#main-form > div:nth-child(10) > div > button');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#docsSupport-3') || await I.checkOption('#docsSupport-3');
-    await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#helpCommunication-3') || await I.checkOption('#helpCommunication-3');
-    await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('Assistance / guide dog') || await I.checkOption('Assistance / guide dog');
-    await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#courtComfort-2') || await I.checkOption('#courtComfort-2');
-    await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.seeCheckboxIsChecked ('#travellingToCourt-3') || await I.checkOption('#travellingToCourt-3');
-    await I.retry(retryCount).click('Continue');
-    await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
-   },
 
-   async clickOnMIAM(){
-    await I.wait('2');
-    await I.retry(retryCount).click('#medation-miam');
-    await I.wait('2');
-    await I.retry(retryCount).click('#miamStart');
-    await I.retry(retryCount).click('#main-form > div:nth-child(4) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
-    await I.wait('2');
-   },
-
-   async clickOnInternationalElement(){
-    await I.wait('2');
-    await I.retry(retryCount).click('#international-factors');
-    await I.wait('2');
-    await I.retry(retryCount).click('#start');
-    await I.retry(retryCount).fillField("#iFactorsStartProvideDetails", this.fields.permissionFromCourt);
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#parents');
-    await I.retry(retryCount).fillField("#iFactorsParentsProvideDetails", this.fields.permissionFromCourt);
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#jurisdiction');
-    await I.retry(retryCount).fillField("#iFactorsJurisdictionProvideDetails", this.fields.permissionFromCourt);
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#request');
-    await I.retry(retryCount).fillField("#iFactorsRequestProvideDetails", this.fields.permissionFromCourt);
-    await I.retry(retryCount).click('#main-form > div:nth-child(3) > div > button');
-    await I.wait('2');
-    await I.retry(retryCount).click('#main-form-submit');
-   },
+   
 
   async aboutYouFL401(){
     await this.clickFL401Link();
