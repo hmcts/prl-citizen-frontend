78d639b1d2514b2fe98bbd5d252a1399b75e0a96
sealDebolina <debolina.seal@hmcts.net>
2024-02-10 12:28:04 +0000

    removing Address history complete section and address history for person staying same address for 5 year also fix address details screen bug

---
+++ src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Response } from 'express';
+
+import { AppRequest } from '../../../../app/controller/AppRequest';
+import { getFeesForC100ApplicationSubmission } from '../../../../app/fees/fees-lookup-api';
+
+export const routeGuard = {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  get: async (req: AppRequest, res: Response, next: NextFunction) => {
+    if (req.session.userCase?.c100ApplicationFees === undefined) {
+      const c100ApplicationFees = (await getFeesForC100ApplicationSubmission(req.session.user, req.locals.logger))
+        .feeAmount;
+      req.session.userCase = {
+        ...(req.session.userCase ?? {}),
+        c100ApplicationFees,
+      };
+    next();
+  }
+}
+};
