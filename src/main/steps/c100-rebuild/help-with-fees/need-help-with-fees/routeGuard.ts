dd780cdaf458bc12373090dc4666a79e2ef1d09c
sealDebolina <debolina.seal@hmcts.net>
2024-02-12 13:09:54 +0000

    PRL-5116

--- src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
+++ src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
@@ -6,14 +6,12 @@ import { getFeesForC100ApplicationSubmission } from '../../../../app/fees/fees-l
 export const routeGuard = {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   get: async (req: AppRequest, res: Response, next: NextFunction) => {
-    if (req.session.userCase?.c100ApplicationFees === undefined) {
-      const c100ApplicationFees = (await getFeesForC100ApplicationSubmission(req.session.user, req.locals.logger))
-        .feeAmount;
-      req.session.userCase = {
-        ...(req.session.userCase ?? {}),
-        c100ApplicationFees,
-      };
-      next();
-    }
+    const c100ApplicationFees = (await getFeesForC100ApplicationSubmission(req.session.user, req.locals.logger))
+      .feeAmount;
+    req.session.userCase = {
+      ...(req.session.userCase ?? {}),
+      c100ApplicationFees,
+    };
+    req.session.save(next);
   },
 };
