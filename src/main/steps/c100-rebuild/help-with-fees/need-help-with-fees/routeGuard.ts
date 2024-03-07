796f5fb9924991804098687531109175f023308a
sealDebolina <debolina.seal@hmcts.net>
2024-03-07 13:46:01 +0000

    PRL-5117-Database call before HWF

--- src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
+++ src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
@@ -2,10 +2,18 @@ import { NextFunction, Response } from 'express';
 
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { getFeesForC100ApplicationSubmission } from '../../../../app/fees/fees-lookup-api';
+import { C100_CASE_EVENT } from 'app/case/definition';
 
 export const routeGuard = {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   get: async (req: AppRequest, res: Response, next: NextFunction) => {
+      await req.locals.C100Api.updateCase(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        req.session.userCase?.caseId as string,
+        req.session.userCase,
+        req.originalUrl,
+        C100_CASE_EVENT.CASE_UPDATE
+      );
     const c100ApplicationFees = (await getFeesForC100ApplicationSubmission(req.session.user, req.locals.logger))
       .feeAmount;
     req.session.userCase = {
