aadf96cd342a6125cad9fcab512114c9049cc375
sealDebolina <debolina.seal@hmcts.net>
2024-03-10 16:19:35 +0000

    review comment

--- src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
+++ src/main/steps/c100-rebuild/help-with-fees/need-help-with-fees/routeGuard.ts
@@ -2,11 +2,13 @@ import { NextFunction, Response } from 'express';
 
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { getFeesForC100ApplicationSubmission } from '../../../../app/fees/fees-lookup-api';
-import { C100_CASE_EVENT } from 'app/case/definition';
+import { C100_CASE_EVENT } from '../../../../app/case/definition';
+import { HOME_URL } from '../../../../steps/urls';
 
 export const routeGuard = {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   get: async (req: AppRequest, res: Response, next: NextFunction) => {
+    try{
       await req.locals.C100Api.updateCase(
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         req.session.userCase?.caseId as string,
@@ -21,5 +23,10 @@ export const routeGuard = {
       c100ApplicationFees,
     };
     req.session.save(next);
-  },
+  }
+catch(error){
+  req.locals.logger.error('error in update case', error);
+  req.session.save(()=>res.redirect(HOME_URL));
+}
+}  
 };
