c453c31d2061ef35e0300f0b431810d8fbb52bf8
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2024-04-16 17:40:05 +0100

    PRL-5517: Content changes for reaching agreement without going to court

--- src/main/steps/c100-rebuild/screening-questions/alternative-routes/content.ts
+++ src/main/steps/c100-rebuild/screening-questions/alternative-routes/content.ts
@@ -1,170 +1,57 @@
-import { YesOrNo } from '../../../../app/case/definition';
 import { TranslationFn } from '../../../../app/controller/GetController';
 import { FormContent } from '../../../../app/form/Form';
-import { isTextAreaValid } from '../../../../app/form/validation';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const en = () => ({
-  title: 'Other ways to reach an agreement',
-  secondaryTitle: 'Options to consider',
-  thirdTitle: 'Negotiation tools and services',
-  fourthTitle: 'Mediation',
-  fifthTitle: 'Lawyer negotiation',
-  sixthTitle: 'Collaborative law',
-  paragraph: 'It is usually better for the children if you reach an agreement outside of court.',
-  secondaryLabel: 'This could:',
-  firstList: [
+  title: 'Reaching an agreement without going to court',
+  ncdrDescription:
+    'It’s often a better idea to try to reach an agreement with the other person without going to court. This is known as ‘non-court dispute resolution’ (NCDR).',
+  ncdrCould: 'NCDR could:',
+  ncdrCouldList: [
     'make the situation less stressful for the children',
     'help the children to maintain contact with family members',
     'save time and money',
   ],
-  paragraph1: `If you still communicate with the other people in the case,
-    and there are no safety concerns, you could try to negotiate with each other.
-    <br/><br/>
-    This can be faster and less expensive than going to court.
-    <br/><br/>
-    There are free tools and services such as
-    <a href="https://www.cafcass.gov.uk/grown-ups/parents-and-carers/divorce-and-separation/parenting-together/parenting-plan/"
-    class="govuk-link" rel="external" target="_blank">parenting plans</a>
-    that can help you reach an agreement.
-    `,
-
-  paragraph2: `Mediation sessions are run by professionals who help you try
-    to reach an agreement without going to court.
-    <br/><br/>
-    Mediation isn’t relationship counselling and you don’t
-    have to be in the same room the other people in the case.
-    <br/><br/>
-    Mediation is suitable for people who want to reach an agreement
-    but need help from someone who is independent.
-    <br/><br/>
-    <a href="https://helpwithchildarrangements.service.justice.gov.uk/professional-mediation"
-    class="govuk-link" rel="external" target="_blank">
-    Find out more about professional mediation</a>
-    `,
-
-  paragraph3: `With lawyer negotiation you don't deal directly with the other people in the case.
-  You hire a lawyer to negotiate arrangements for you.
-  <br/><br/>
-  You can still hire a lawyer to negotiate on your behalf,
-  even if the other people choose not to use one.
-  <br/><br/>
-  Lawyer negotiation is suitable for people who prefer not to meet because their
-  relationship is still difficult, or because there’s a lack of trust.
-  <br/><br/>
-  <a href="https://helpwithchildarrangements.service.justice.gov.uk/lawyer-negotiation"
-  class="govuk-link" rel="external" target="_blank">Find out more about lawyer negotiation</a>
-  `,
-  paragraph4: `Collaborative lawyers work with you and the other people in the case to resolve your
-  issues out of court. You each hire a lawyer, then all meet to negotiate in person.
-  <br/><br/>
-  The process takes time but is often quicker and cheaper than going to court.
-  <br/><br/>
-  This option is suitable for people who can still communicate with each other,
-  but have complex legal issues to resolve.
-  <br/><br/>
-  <a href="https://helpwithchildarrangements.service.justice.gov.uk/collaborative-law"
-  class="govuk-link" rel="external" target="_blank">Find out more about collaborative law</a>
-  `,
-  insetText: {
-    html: `<p class="govuk-body govuk-!-font-weight-bold">You could get up to £500 towards family mediation</p>
-  <p class="govuk-body">
-    This is a new scheme available for a short time only. Find out more about the
-    <a href="https://www.gov.uk/guidance/family-mediation-voucher-scheme?utm_source=C100&amp;utm_campaign=mediation_vouchers"
-    class="govuk-link" rel="external" target="_blank">family
-      mediation voucher scheme</a>.
-  </p>`,
-  },
-  formTitle: 'Have you considered any alternative options to reach an agreement?',
-  one: 'Yes',
-  two: 'No',
-  otherDetails: 'Provide details (optional)',
-  errors: {
-    sq_agreementReason: {
-      invalidCharacters: 'You have entered an invalid character. Special characters <,>,{,} are not allowed.',
-      invalid:
-        'You have exceeded the character limit accepted by the free text field. Please enter 5,000 characters or less.',
-    },
-  },
+  mediatorHelp: 'A mediator will help you consider what kinds of NCDR are suitable in your case.',
+  findMoreInformation:
+    'Find more information on getting <a href="https://helpwithchildarrangements.service.justice.gov.uk/" class="govuk-link" rel="external" target="_blank">help with child arrangements on GOV.UK (opens in a new tab)</a>.',
+  ncdrTypes: 'Types of NCDR',
+  youCouldTry: 'You could try:',
+  youCouldTryList: [
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/negotiating-between-parents" class="govuk-link" rel="external" target="_blank">negotiation tools and services (opens in a new tab)</a>, if you’re still communicating with the other person in the case',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/professional-mediation" class="govuk-link" rel="external" target="_blank">mediation (opens in a new tab)</a>, for example if you want an independent professional to help you reach an agreement',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/lawyer-negotiation" class="govuk-link" rel="external" target="_blank">lawyer negotiation (opens in a new tab)</a>, if you prefer a lawyer to communicate with the other person on your behalf',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/collaborative-law" class="govuk-link" rel="external" target="_blank">collaborative law (opens in a new tab)</a>, if you’re still communicating with the other person but have complex legal issues to resolve',
+  ],
+  helpTowardsMediationSubTitle: 'Getting help towards family mediation',
+  helpTowardsMediation:
+    'You could get up to £500 towards the cost of family mediation for a limited time only. Find out more about the <a href="https://www.gov.uk/guidance/family-mediation-voucher-scheme?utm_source=C100&utm_campaign=mediation_vouchers" class="govuk-link" rel="external" target="_blank">Family Mediation Voucher Scheme on GOV.UK (opens in a new tab)</a>.',
 });
 
 const cy = () => ({
-  title: 'Ffyrdd eraill o ddod i gytundeb',
-  secondaryTitle: "Opsiynau i'w hystyried",
-  thirdTitle: 'Dulliau a gwasanaethau negodi',
-  fourthTitle: 'Cyfryngu',
-  fifthTitle: 'Trafod drwy gyfreithiwr',
-  sixthTitle: 'Cyfraith gydweithredol',
-  paragraph: "Fel arfer mae'n well i'r plant os byddwch yn dod i gytundeb y tu allan i'r llys.",
-  secondaryLabel: 'Gallai hyn:',
-  firstList: [
-    'olygu y bydd y sefyllfa yn rhoi llai o bwysau ar y plant',
+  title: 'Reaching an agreement without going to court (welsh)',
+  ncdrDescription:
+    'Yn aml iawn mae’n opsiwn gwell i geisio dod i gytundeb gyda’r person arall heb fynd i’r llys. Gelwir hyn yn ‘datrys anghydfod y tu allan i’r llys’ (NCDR).',
+  ncdrCould: 'Gallai NCDR:',
+  ncdrCouldList: [
+    'wneud y sefyllfa yn llai poenus i’r plant',
     "helpu'r plant i gadw mewn cysylltiad ag aelodau o'r teulu",
     'arbed amser ac arian',
   ],
-  paragraph1: `Os ydych chi'n dal i gyfathrebu gyda'r bobl eraill yn yr achos,
-  ac nad oes pryderon diogelwch, gallech geisio trafod gyda'ch gilydd.
-    <br/><br/>
-    Gall hyn fod yn gyflymach ac yn llai costus na mynd i'r llys.
-    <br/><br/>
-    Mae yna ddulliau a gwasanaethau am ddim fel
-    <a href="https://www.cafcass.gov.uk/grown-ups/parents-and-carers/divorce-and-separation/parenting-together/parenting-plan/"
-    class="govuk-link" rel="external" target="_blank">Cynlluniau rhianta</a>
-    a allai eich helpu i ddod i gytundeb.`,
-
-  paragraph2: `Mae sesiynau cyfryngu yn cael eu cynnal gan weithwyr proffesiynol sy'n eich helpu
-  i geisio dod i gytundeb heb fynd i'r llys.
-    <br/><br/>
-    Nid yw cyfryngu yn gwnsela perthynas ac nid ydych
-    yn gorfod bod yn yr un ystafell â’r bobl eraill yn yr achos.
-    <br/><br/>
-    Mae cyfryngu yn addas ar gyfer pobl sydd am ddod i gytundeb
-    ond bod angen help gan rywun sy'n annibynnol.
-    <br/><br/>
-    <a href="https://helpwithchildarrangements.service.justice.gov.uk/professional-mediation"
-    class="govuk-link" rel="external" target="_blank">
-    Mwy o wybodaeth am gyfryngu proffesiynol`,
-  paragraph3: `Wrth drafod drwy gyfreithiwr nid ydych yn delio'n uniongyrchol gyda'r bobl eraill yn yr achos.
-  Rydych yn talu i gyfreithiwr i drafod trefniadau ar eich rhan.
-  <br/><br/>
-  Gallwch dal dalu i gyfreithiwr drafod ar eich rhan,
-  hyd yn oed os yw'r bobl eraill yn dewis peidio defnyddio cyfreithiwr.
-  <br/><br/>
-  Mae trafod drwy gyfreithiwr yn addas i bobl y mae'n well ganddyn nhw beidio â chyfarfod oherwydd bod eu
-  perthynas yn dal i fod yn anodd, neu oherwydd bod diffyg ymddiriedaeth.
-  <br/><br/>
-  <a href="https://helpwithchildarrangements.service.justice.gov.uk/lawyer-negotiation"
-  class="govuk-link" rel="external" target="_blank">Rhagor o wybodaeth am drafod drwy gyfreithiwr</a>`,
-  paragraph4: `Mae cyfreithwyr cydweithredol yn gweithio gyda chi a'r bobl eraill yn yr achos i ddatrys eich
-  materion y tu allan i'r llys. Byddwch chi a’r bobl eraill yn yr achos yn hurio cyfreithiwr, yna bydd pob un yn cwrdd i drafod wyneb yn wyneb.
-  <br/><br/>
-  Mae'r broses yn cymryd amser ond mae'n aml yn gyflymach ac yn rhatach na mynd i'r llys.
-  <br/><br/>
-  Mae'r opsiwn hwn yn addas i bobl sy'n gallu cyfathrebu â'i gilydd o hyd,
-  ond bod ganddynt faterion cyfreithiol cymhleth i'w datrys.
-  <br/><br/>
-  <a href="https://helpwithchildarrangements.service.justice.gov.uk/collaborative-law"
-  class="govuk-link" rel="external" target="_blank">Mwy o wybodaeth am gyfraith gydweithredol</a>`,
-  insetText: {
-    html: `<p class="govuk-body govuk-!-font-weight-bold">Mwy o wybodaeth am gyfraith gydweithredol</p>
-  <p class="govuk-body">
-  Dyma gynllun newydd sydd ar gael am gyfnod byr yn unig. Mwy o wybodaeth am y
-    <a href="https://www.gov.uk/guidance/family-mediation-voucher-scheme?utm_source=C100&amp;utm_campaign=mediation_vouchers"
-    class="govuk-link" rel="external" target="_blank">cynllun
-    talebau cyfryngu teuluol</a>.
-  </p>`,
-  },
-  formTitle: 'Ydych chi wedi ystyried unrhyw opsiynau amgen i ddod i gytundeb?',
-  one: 'Do',
-  two: 'Naddo',
-  otherDetails: 'Rhowch fanylion (dewisol)',
-  errors: {
-    sq_agreementReason: {
-      invalidCharacters: 'Rydych wedi defnyddio nod annilys. Ni chaniateir y nodau arbennig hyn <,>,{,}',
-      invalid:
-        'Rydych wedi defnyddio mwy o nodau na’r hyn a ganiateir yn y blwch testun rhydd. Defnyddiwch 5,000 neu lai o nodau.',
-    },
-  },
+  mediatorHelp: 'Bydd cyfryngwr yn eich helpu i ystyried pa fath o NCDR sy’n addas yn eich achos chi.',
+  findMoreInformation:
+    'Mae yna fwy o wybodaeth am gael <a href="https://helpwithchildarrangements.service.justice.gov.uk/" class="govuk-link" rel="external" target="_blank">help gyda threfniadau plant ar GOV.UK (yn agor mewn tab newydd)</a>.',
+  ncdrTypes: 'Mathau o NCDR',
+  youCouldTry: 'Gallwch geisio:',
+  youCouldTryList: [
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/negotiating-between-parents" class="govuk-link" rel="external" target="_blank">defnyddio dulliau a gwasanaethau negodi (yn agor mewn tab newydd)</a>, os ydych chi dal yn cyfathrebu gyda’r unigolyn arall yn yr achos',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/professional-mediation" class="govuk-link" rel="external" target="_blank">cyfryngu (yn agor mewn tab newydd)</a>, er enghraifft os ydych chi eisiau i weithiwr proffesiynol annibynnol eich helpu i ddod i gytundeb',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/lawyer-negotiation" class="govuk-link" rel="external" target="_blank">trafodaethau rhwng cyfreithwyr (yn agor mewn tab newydd)</a>, os byddai’n well gennych i gyfreithiwr gyfathrebu â’r unigolyn arall ar eich rhan',
+    '<a href="https://helpwithchildarrangements.service.justice.gov.uk/collaborative-law" class="govuk-link" rel="external" target="_blank">cyfraith gydweithredol (yn agor mewn tab newydd)</a>, os ydych chi dal yn cyfathrebu â’r unigolyn arall ond mae gennych faterion cyfreithiol cymhleth i\'w datrys',
+  ],
+  helpTowardsMediationSubTitle: 'Cymorth ar gyfer cyfryngu teuluol',
+  helpTowardsMediation:
+    'Gallwch gael hyd at £500 tuag at gost cyfryngu teuluol am gyfnod cyfyngedig yn unig. Mae yna fwy o wybodaeth am y <a href="https://www.gov.uk/guidance/family-mediation-voucher-scheme?utm_source=C100&utm_campaign=mediation_vouchers" class="govuk-link" rel="external" target="_blank">Cynllun Talebau Cyfryngu Teuluol ar GOV.UK (yn agor mewn tab newydd)</a>.',
 });
 
 const languages = {
@@ -173,30 +60,7 @@ const languages = {
 };
 
 export const form: FormContent = {
-  fields: {
-    sq_alternativeRoutes: {
-      type: 'radios',
-      classes: 'govuk-radios',
-      values: [
-        {
-          label: l => l.one,
-          value: YesOrNo.YES,
-          subFields: {
-            sq_agreementReason: {
-              type: 'textarea',
-              label: l => l.otherDetails,
-              labelSize: null,
-              validator: value => isTextAreaValid(value),
-            },
-          },
-        },
-        {
-          label: l => l.two,
-          value: YesOrNo.NO,
-        },
-      ],
-    },
-  },
+  fields: {},
   submit: {
     text: l => l.onlycontinue,
   },
