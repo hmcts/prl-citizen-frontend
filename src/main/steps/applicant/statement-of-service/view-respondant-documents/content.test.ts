e233fc902299f1e823e820ab81f35dc1c435dc40
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-04-24 09:27:34 +0100

    respondent docs added to be viewed

---
+++ src/main/steps/applicant/statement-of-service/view-respondant-documents/content.test.ts
@@ -0,0 +1,65 @@
+import languageAssertions from '../../../../../test/unit/utils/languageAssertions';
+import { FormContent, FormFields } from '../../../../app/form/Form';
+import { atLeastOneFieldIsChecked } from '../../../../app/form/validation';
+import { CommonContent } from '../../../common/common.content';
+
+import { cy, en, generateContent } from './content';
+
+jest.mock('../../../../app/form/validation');
+/* eslint-disable @typescript-eslint/ban-types */
+describe('citizen-home content', () => {
+  const commonContent = { language: 'en' } as CommonContent;
+  commonContent.additionalData = {
+    req: {
+      query: {
+        parentDocType: 'parent',
+        docType: 'doc',
+      },
+      params: {
+        context: 'order',
+      },
+    },
+  };
+  let generatedContent;
+  let form;
+  let fields;
+  beforeEach(() => {
+    generatedContent = generateContent(commonContent);
+    form = generatedContent.form as FormContent;
+    fields = form.fields as FormFields;
+  });
+
+  test('should return correct english content', () => {
+    expect(generatedContent.caption).toEqual('Case number ');
+    expect(generatedContent.title).toEqual('Add a statement of service');
+    expect(generatedContent.whowasserved).toEqual('Who was served?');
+    expect(generatedContent.continue).toEqual('Continue');
+    expect(generatedContent.add).toEqual('Submit');
+    expect(generatedContent.uploadFiles).toEqual('Your documents');
+    expect(generatedContent.remove).toEqual('Remove');
+    expect(generatedContent.uplodFileText1).toEqual(
+      'when uploading documents, name the files clearly. For example, position-statement.doc. Files must end with JPG,BMP,PNG,TIF,PDF,DOC,or DOCX.'
+    );
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  test('should return correct english content Data', () => {
+    languageAssertions('en', en, () => generateContent(commonContent));
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  test('should return correct welsh content', () => {
+    languageAssertions('cy', cy, () => generateContent({ ...commonContent, language: 'cy' }));
+  });
+
+  test('should contain continue checkboxes', () => {
+    const sos_partiesServed = fields.sos_partiesServed;
+    expect(sos_partiesServed.type).toBe('checkboxes');
+    expect(sos_partiesServed.validator).toBe(atLeastOneFieldIsChecked);
+
+    const sos_partiesServedDate = fields.sos_partiesServedDate;
+    expect(sos_partiesServedDate.type).toBe('date');
+    expect((sos_partiesServedDate.label as Function)(generatedContent)).toBe(en.servedDate);
+  });
+});
+/* eslint-enable @typescript-eslint/ban-types */
---
+++ src/main/steps/applicant/statement-of-service/view-respondant-documents/content.ts
@@ -0,0 +1,50 @@
+import {
+  ApplicationPackDocumentMeta,
+  CitizenRespondentPack,
+} from '../../../../../main/steps/common/documents/definitions';
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { applyParms } from '../../../../steps/common/url-parser';
+import { DOWNLOAD_DOCUMENT } from '../../../../steps/urls';
+
+export const en = {};
+
+export const cy = {};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {},
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  const data: ApplicationPackDocumentMeta[] = [];
+  const respondentPacks: CitizenRespondentPack =
+    content.additionalData?.req.session.userCase.personalServiceUnServedRespondentPack;
+  respondentPacks.packDocument?.forEach(document => {
+    const documentId = document.value?.document_url.substring(document.value.document_url.lastIndexOf('/') + 1);
+    data.push({
+      documentId: documentId || '',
+      documentName: document.value?.document_filename || '',
+      servedDate: document.value?.category_id || '',
+      documentDownloadUrl: applyParms(DOWNLOAD_DOCUMENT, {
+        documentId,
+        documentName: document.value?.document_filename,
+      }),
+    });
+  });
+  // respondentPacks.packDocument?.forEach(document =>
+  //   data.push({
+  //     date: document.value?.document_creation_date,
+  //     name: document.value?.document_filename,
+  //   })
+  // );
+  return {
+    ...translations,
+    data,
+  };
+};
