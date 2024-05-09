e8fd998a997fe91ba38d20637141789f07a39960
alokDatta <aloknath.datta@hmcts.net>
2024-05-09 02:36:18 +0100

    content added

---
+++ src/main/steps/tasklistresponse/respond-to-allegations-of-harm/wish-to-respond/content.ts
@@ -0,0 +1,77 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn } from '../../../../app/form/validation';
+
+export const en = {
+  section: 'Respond to allegations of harm and violence',
+  title: 'The allegations',
+  reviewBeforeResponding: 'Before responding, review the allegations made by the applicant:',
+  linkToAoH: 'Allegations of harm and violence (C1A) (Opens in new tab)',
+  disclaimer: 'If you do not agree with the allegations made by the applicant, you can respond and give your point of view. All the people in this application will be able to see your comments.',
+  disclaimer_2: 'If you choose not to respond to the allegations now, you\'ll still be able to respond in court.',
+  wishToRespondLabel: 'Do you wish to respond to the applicant\'s allegations of harm?',
+  one: 'Yes',
+  two: 'No',
+  onlyContinue: 'Continue',
+  errors: {
+    detailsKnown: {
+      required: 'Select yes if you want to respond now',
+    },
+  },
+};
+
+export const cy: typeof en = {
+  section: 'Ymateb i honiadau o niwed a thrais',
+  title: 'Yr honiadau',
+  reviewBeforeResponding: 'Cyn ymateb, adolygwch yr honiadau a wnaed gan y ceisydd:',
+  linkToAoH: 'Allegations of harm and violence (C1A) (Opens in new tab) - welsh',
+  disclaimer: 'Os nad ydych yn cytuno â\’r honiadau a wnaed gan y ceisydd, gallwch ymateb a rhoi ei barn chi. Bydd pawb sy\’n rhan o\’r cais hwn yn gallu gweld eich sylwadau.',
+  disclaimer_2: 'Os byddwch yn dewis peidio ag ymateb i\’r honiadau nawr, byddwch dal yn gallu ymateb yn y llys.',
+  wishToRespondLabel: 'Ydych chi eisiau ymateb i honiadau o niwed y ceisydd?',
+  one: 'Ydw',
+  two: 'Nac ydyw',
+  onlyContinue: 'Continue - welsh',
+  errors: {
+    detailsKnown: {
+      required: 'Dewiswch \‘Ydw\’ os ydych eisiau ymateb nawr',
+    },
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    wishToRespond: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.label,
+      section: l => l.section,
+      values: [
+        {
+          label: l => l.one,
+          value: 'yes',
+        },
+        {
+          label: l => l.two,
+          value: 'no',
+        }
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
+  onlyContinue: {
+    text: l => l.onlyContinue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
---
+++ src/main/steps/tasklistresponse/respond-to-allegations-of-harm/your-response/content.ts
@@ -0,0 +1,68 @@
+import { TranslationFn } from '../../../../app/controller/GetController';
+import { FormContent } from '../../../../app/form/Form';
+import { isFieldFilledIn, isTextAreaValid } from '../../../../app/form/validation';
+
+export const en = {
+  section: 'Respond to allegations of harm and violence',
+  title: 'Respond to the allegations',
+  suggestionLabel_1: 'Give your response to each of the points made by the applicant. Try to explain what happened as best you can.',
+  suggestionLabel_2: 'You will have the chance to give a more detailed statement later in court.',
+  suggestionLabel_3: 'Include any evidence that is relevant to the allegations.',
+  respondToAohTitle: 'Your response',
+  respondToAohHint: 'Include times, dates and details of what happened.',
+  continue: 'Save and continue',
+  errors: {
+    detailsKnown: {
+      required: 'You must give details if you want to respond to the allegations',
+      invalidCharacters: 'You have entered an invalid character. Special characters <,>,{,} are not allowed.',
+      invalid:
+        'You have exceeded the character limit accepted by the free text field. Please enter 5,000 characters or less.',
+    },
+  }
+};
+
+export const cy: typeof en = {
+  section: 'Ymateb i honiadau o niwed a thrais',
+  title: 'Ymateb i\’r honiadau',
+  suggestionLabel_1: 'Rhowch eich ymateb i bob un o\’r pwyntiau a wnaed gan y ceisydd. Ceisiwch egluro beth ddigwyddodd y gorau y gallwch chi.',
+  suggestionLabel_2: 'Cewch gyfle i roi datganiad manylach yn nes ymlaen yn y llys.',
+  suggestionLabel_3: 'Dylech gynnwys unrhyw dystiolaeth sy\’n berthnasol i\’r honiadau.',
+  respondToAohTitle: 'Eich ymateb',
+  respondToAohHint: 'Dylech gynnwys amseroedd, dyddiadau a manylion beth ddigwyddodd.',
+  continue: 'Cadw a pharhau',
+  errors: {
+    detailsKnown: {
+      required: 'Mae’n rhaid i chi roi manylion os ydych eisiau ymateb i’r honiadau',
+      invalidCharacters: 'Rydych wedi defnyddio nod annilys. Ni chaniateir y nodau arbennig hyn <,>,{,}',
+      invalid:
+        'Rydych wedi defnyddio mwy o nodau na’r hyn a ganiateir yn y blwch testun rhydd. Defnyddiwch 5,000 neu lai o nodau.',
+    },
+  }
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const form: FormContent = {
+  fields: {
+    hwn_reasonsForApplicationWithoutNotice: {
+      type: 'textarea',
+      hint: l => l.hint,
+      validator: value => isFieldFilledIn(value) || isTextAreaValid(value),
+    }
+    
+  },
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language];
+  return {
+    ...translations,
+    form,
+  };
+};
