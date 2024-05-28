91716a700a55249c05da0c5fd48cd4827f61aa8b
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-05-28 11:12:20 +0100

    C7 response post controller review commments addressed

--- src/main/steps/tasklistresponse/summary/postController.ts
+++ src/main/steps/tasklistresponse/summary/postController.ts
@@ -1,15 +1,19 @@
 import autobind from 'autobind-decorator';
-import config from 'config';
+import { config } from 'codeceptjs';
 import { Response } from 'express';
 
+import { CosApiClient } from '../../../app/case/CosApiClient';
+import { CaseType } from '../../../app/case/definition';
 import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
 import { Form, FormFields, FormFieldsFn } from '../../../app/form/Form';
-import PCQGetController from '../../common/equality/PcqNavigationController';
-import { RESPONDENT_TO_APPLICATION_SUMMARY_REDIRECT } from '../../urls';
+import PCQGetController from '../../../steps/common/equality/PcqNavigationController';
+import { getCasePartyType } from '../../../steps/prl-cases/dashboard/utils';
+import { CA_RESPONDENT_RESPONSE_CONFIRMATION, RESPONDENT_TO_APPLICATION_SUMMARY_REDIRECT } from '../../urls';
+import { getPartyDetails, mapDataInSession } from '../utils';
 
 @autobind
-export default class ResponseSummaryConfirmationPcqPostController extends PostController<AnyObject> {
+export default class ResponseSummaryConfirmationPostController extends PostController<AnyObject> {
   constructor(protected readonly fields: FormFields | FormFieldsFn) {
     super(fields);
   }
@@ -28,5 +32,35 @@ export default class ResponseSummaryConfirmationPcqPostController extends PostCo
     const port = req.app.locals.developmentMode ? `:${config.get('port')}` : '';
     const returnUrl = `${protocol}${res.locals.host}${port}${RESPONDENT_TO_APPLICATION_SUMMARY_REDIRECT}`;
     new PCQGetController().get(req, res, returnUrl);
+  }
+
+  public async submitC7(req: AppRequest, res: Response): Promise<void> {
+    //TODO update when merged with response submission fixes
+    const { user, userCase } = req.session;
+    const partyType = getCasePartyType(userCase, user.id);
+    const partyDetails = getPartyDetails(userCase, user.id);
+    const client = new CosApiClient(user.accessToken, req.locals.logger);
+    if (partyDetails) {
+      try {
+        if (!partyDetails.user.pcqId) {
+          partyDetails.user.pcqId = userCase.respondentPcqId;
+        }
+
+        req.session.userCase = await client.submitC7Response(
+          userCase.id,
+          partyDetails,
+          partyType,
+          userCase.caseTypeOfApplication as CaseType
+        );
+        mapDataInSession(req.session.userCase, user.id);
+        req.session.save(() => {
+          res.redirect(CA_RESPONDENT_RESPONSE_CONFIRMATION);
+        });
+      } catch (error) {
+        throw new Error('Error occured, could not sumbit C7 response. - ResponseSummaryConfirmationPostController');
+      }
+    } else {
+      this.redirect(req, res);
+    }
   }
 }
