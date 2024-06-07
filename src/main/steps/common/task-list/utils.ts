d2bc7f7f7da5369f8b725139e27bb5bdb11e4459
sealDebolina <debolina.seal@hmcts.net>
2024-06-07 09:28:00 +0100

    PRL 5429

--- src/main/steps/common/task-list/utils.ts
+++ src/main/steps/common/task-list/utils.ts
@@ -52,17 +52,7 @@ export const isCaseWithdrawn = (caseData: Partial<CaseWithId>): boolean => {
   if (!caseData) {
     return false;
   }
-
-  if (caseData?.orderCollection) {
-    return !!caseData.orderCollection.find(
-      order =>
-        order.value?.orderTypeId === 'blankOrderOrDirectionsWithdraw' &&
-        order.value?.withdrawnRequestType === 'Withdrawn application' &&
-        order.value.isWithdrawnRequestApproved === YesOrNo.YES
-    );
-  } else {
     return [State.CASE_WITHDRAWN].includes(caseData.state!);
-  }
 };
 
 export const isCaseLinked = (caseData: Partial<CaseWithId>, userDetails: UserDetails): boolean => {
