05a6f9d0d8a8283eb5a8f43ec88b239a7f961cf8
bala <bala.ganesan1@hmcts.net>
2024-06-24 12:16:50 +0100

    enabled other test

--- src/test/end-to-end/tests/C100-PCQ-Test.js
+++ src/test/end-to-end/tests/C100-PCQ-Test.js
@@ -2,11 +2,11 @@ const testConfig = require('../config');
 
 Feature('C100 - PCQ Flow');
 
-// Scenario('C100 - PCQ submit - @master @nightly', async ({ I }) => {
-//   await I.loginAsCitizen();
-//   await I.createC100DraftTS();
-//   await I.submitPCQ();
-//  }).retry(testConfig.TestRetryScenarios);
+Scenario('C100 - PCQ submit - @master @nightly', async ({ I }) => {
+  await I.loginAsCitizen();
+  await I.createC100DraftTS();
+  await I.submitPCQ();
+ }).retry(testConfig.TestRetryScenarios);
 
 Scenario('C100 - DO NOT want to submit PCQ - @master @nightly', async ({ I }) => {
   await I.loginAsCitizen();
