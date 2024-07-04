47b3f0e832b97ea2c6338c20019c12795fd2d994
alokDatta <aloknath.datta@hmcts.net>
2024-07-04 01:02:22 +0100

    unit test fix

--- src/main/steps/common/task-list/components/tasklist/config/ca_respondent.ts
+++ src/main/steps/common/task-list/components/tasklist/config/ca_respondent.ts
@@ -35,7 +35,6 @@ import {
   getConfirmOrEditYourContactDetailsStatus,
   getContents,
   getFinalApplicationStatus,
-  getInternationalFactorsStatus,
   getKeepYourDetailsPrivateStatus,
   hasAnyHearing,
 } from '../utils';
