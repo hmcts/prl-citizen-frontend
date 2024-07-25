6b1047a5e36aad5d2641d7e462019b4ad451cfde
JamesCutts00 <James.Cutts@HMCTS.ORG>
2024-07-25 17:49:46 +0100

    Refactored code to reduce duplicate code

--- src/main/steps/respondent/confirm-contact-details/contactdetails/content.ts
+++ src/main/steps/respondent/confirm-contact-details/contactdetails/content.ts
@@ -1,46 +1,7 @@
 import { TranslationFn } from '../../../../app/controller/GetController';
 import { FormContent } from '../../../../app/form/Form';
 import { isEmailValid, isFieldFilledIn, isPhoneNoValid } from '../../../../app/form/validation';
-
-const en = {
-  title: 'Your contact details',
-  citizenUserPhoneNumber: 'UK telephone number',
-  citizenUserEmailAddress: 'Email address',
-  continue: 'Continue',
-  errors: {
-    citizenUserPhoneNumber: {
-      required: 'Enter a valid UK telephone number',
-      invalid: 'Enter a valid UK telephone number',
-    },
-    citizenUserEmailAddress: {
-      required: 'Enter a valid email address',
-      invalid: 'Enter a valid email address, like name@example.com',
-    },
-    citizenUserSafeToCall: {
-      invalid: 'You have entered an invalid character. Enter using letters and numbers only.',
-    },
-  },
-};
-
-const cy: typeof en = {
-  title: 'Eich manylion cyswllt',
-  citizenUserPhoneNumber: 'Rhif ffôn yn y DU',
-  citizenUserEmailAddress: 'Cyfeiriad e-bost',
-  continue: 'Parhau',
-  errors: {
-    citizenUserPhoneNumber: {
-      required: 'Rhowch rif ffôn y DU',
-      invalid: 'Rhowch rif ffôn dilys yn y DU',
-    },
-    citizenUserEmailAddress: {
-      required: 'Rhowch gyfeiriad e-bost',
-      invalid: 'Rhowch gyfeiriad e-bost yn y fformat cywir, fel name@example.com',
-    },
-    citizenUserSafeToCall: {
-      invalid: 'Rydych wedi defnyddio nod annilys. Defnyddiwch lythrennau a rhifau yn unig.',
-    },
-  },
-};
+import { cy, en } from '../../../../steps/common/confirm-contact-details/contactdetails/content';
 
 const languages = {
   en,
