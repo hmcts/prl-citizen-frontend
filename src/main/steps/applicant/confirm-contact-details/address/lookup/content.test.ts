17abcb428d319b4991e58f335c04bd958370dceb
sealDebolina <debolina.seal@hmcts.net>
2024-07-29 09:47:44 +0100

    lint fix

--- src/main/steps/applicant/confirm-contact-details/address/lookup/content.test.ts
+++ src/main/steps/applicant/confirm-contact-details/address/lookup/content.test.ts
@@ -1,4 +1,3 @@
-import { ResourceReader } from '../.././../../../modules/resourcereader/ResourceReader';
 import { FormFields } from '../../../../../app/form/Form';
 import { CommonContent } from '../../../../common/common.content';
 import {
@@ -6,7 +5,7 @@ import {
   generateContent as generateAddressLookupContent,
 } from '../../../../common/components/address-lookup';
 import { APPLICANT_MANUAL_ADDRESS } from '../../../../urls';
-
+import { ResourceReader } from '../.././../../../modules/resourcereader/ResourceReader';
 import { generateContent } from './content';
 
 const resourceLoader = new ResourceReader();
