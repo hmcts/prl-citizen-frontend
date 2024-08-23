2011314ec60208ad094ec07ca11a16274ff91d0f
sealDebolina <116593651+sealDebolina@users.noreply.github.com>
2024-08-23 14:36:33 +0100

    PRL-6197 (#1633)

--- src/main/steps/application-within-proceedings/content.ts
+++ src/main/steps/application-within-proceedings/content.ts
@@ -78,43 +78,43 @@ const en = {
 
 const cy: typeof en = {
   [AWPApplicationReason.DELAY_CANCEL_HEARING_DATE]: {
-    reasonText: 'Gohirio neu ganslo dyddiad gwrandawiad',
+    reasonText: 'Gofyn i ohirio neu ganslo dyddiad gwrandawiad',
   },
   [AWPApplicationReason.REQUEST_MORE_TIME]: {
-    reasonText: 'Request more time to do what is required by a court order - welsh',
+    reasonText: 'Gofyn am ragor o amser i wneud yr hyn y mae gorchymyn llys yn eich cyfarwyddo i wneud',
   },
   [AWPApplicationReason.CHILD_ARRANGEMENTS_ORDER_TO_LIVE_SPEND_TIME]: {
-    reasonText: 'Child arrangements live with, or spend time with, order - welsh',
+    reasonText: 'Gorchymyn Trefniadau Plant Byw Gyda neu Treulio Amser Gyda',
   },
   [AWPApplicationReason.PROHIBITED_STEPS_ORDER]: {
-    reasonText: 'Prohibited steps order - welsh',
+    reasonText: 'Gorchymyn Camau Gwaharddedig',
   },
   [AWPApplicationReason.SPECIFIC_ISSUE_ORCDER]: {
-    reasonText: 'Specific issue order - welsh',
+    reasonText: 'Gorchymyn Mater Penodol',
   },
   [AWPApplicationReason.ENFORCE_CHILD_ARRANGEMENTS_ORDER]: {
     reasonText: 'Gorfodi Gorchymyn Trefniadau Plant',
   },
   [AWPApplicationReason.CHANGE_EXTEND_CANCEL_NON_MOLESTATION_OR_OCCUPATION_ORDER]: {
-    reasonText: 'Apply to change, extend or cancel a non-molestation order or occupation order - welsh',
+    reasonText: 'Gwneud cais i newid, ymestyn neu ganslo gorchymyn rhag molestu neu orchymyn anheddu',
   },
   [AWPApplicationReason.SUBMIT_EVIDENCE_COURT_NOT_REQUESTED]: {
-    reasonText: 'Ask to submit evidence the court has not requested - welsh',
+    reasonText: 'Gofyn i gael cyflwyno tystiolaeth nad yw’r llys wedi gofyn amdani',
   },
   [AWPApplicationReason.SHARE_DOCUMENTS_WITH_SOMEONE_ELSE]: {
-    reasonText: 'Ask to share documents with someone else - welsh',
+    reasonText: 'Gofyn i gael rhannu dogfennau gyda rhywun arall',
   },
   [AWPApplicationReason.JOIN_OR_LEAVE_CASE]: {
-    reasonText: 'Ask to join or leave a case - welsh',
+    reasonText: 'Gofyn i gael ymuno ag achos neu adael achos',
   },
   [AWPApplicationReason.REQUEST_TO_WITHDRAW_APPLICATION]: {
-    reasonText: 'Request to withdraw an application - welsh',
+    reasonText: 'Gwneud cais i dynnu cais yn ôl',
   },
   [AWPApplicationReason.ASK_COURT_FOR_APPOINTING_EXPERT]: {
-    reasonText: 'Ask the court to appoint an expert (such as a medical professional or a child psychologist) - welsh',
+    reasonText: 'Gofyn i’r llys benodi arbenigwr (megis gweithiwr iechyd proffesiynol neu seicolegydd plant)',
   },
   [AWPApplicationReason.PERMISSION_FOR_APPLICATION]: {
-    reasonText: 'Get permission for an application if the court previously stopped you - welsh',
+    reasonText: 'Cael caniatâd i wneud cais os yw’r llys wedi’ch atal rhag gwneud hynny yn y gorffennol',
   },
   [AWPApplicationReason.REQUEST_PARENTAL_RESPONSIBILITY]: {
     reasonText: 'Gofyn am orchymyn sy’n ymwneud â phlentyn',
@@ -132,10 +132,12 @@ const cy: typeof en = {
     reasonText: 'Apelio yn erbyn gorchymyn llys neu ofyn am ganiatâd i apelio',
   },
   [AWPApplicationReason.YOU_ACCUSED_SOMEONE]: {
-    reasonText: 'Ask the court to prevent questioning in person when accusations of abuse have been made - welsh',
+    reasonText:
+      'Gofyn i’r llys atal caniatáu cwestiynu unigolyn yn bersonol pan fydd honiadau o gam-drin wedi’u gwneud',
   },
   [AWPApplicationReason.ACCUSED_BY_SOMEONE]: {
-    reasonText: 'Ask the court to prevent questioning in person when accusations of abuse have been made - welsh',
+    reasonText:
+      'Gofyn i’r llys atal caniatáu cwestiynu unigolyn yn bersonol pan fydd honiadau o gam-drin wedi’u gwneud',
   },
   [AWPApplicationReason.ORDER_AUTHORISING_SEARCH]: {
     reasonText:
@@ -148,7 +150,7 @@ const cy: typeof en = {
     reasonText: 'Gofyn i’r llys orchymyn bod rhywun yn darparu gwybodaeth am leoliad plentyn',
   },
   [AWPApplicationReason.REQUEST_FOR_ARREST_WARRENT]: {
-    reasonText: 'Request the court issues an arrest warrant - welsh',
+    reasonText: 'Gwneud cais i’r llys godi gwarant i arestio',
   },
 };
 
