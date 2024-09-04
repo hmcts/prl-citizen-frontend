cdc3bbb1f1499cd4e96fe1ef07ebb92d4f9903da
sahaSonali <110189370+sahaSonali@users.noreply.github.com>
2024-09-04 16:18:43 +0100

    Citizen AwP Fee Change (#1640)
    * Changes for FC600
    
    * Changes for FC600
    
    * feat(PRL-6179): Enable C79 for CA Respondent
    
    ---------
    
    Co-authored-by: Vivek Sekhar <vivek.sekhar@hmcts.net>
    Co-authored-by: vivek-sekhar <111058132+vivek-sekhar@users.noreply.github.com>

--- src/main/steps/application-within-proceedings/utils.ts
+++ src/main/steps/application-within-proceedings/utils.ts
@@ -192,7 +192,7 @@ const applicationTypesConfiguration: AWPApplicationTypesConfig[] = [
         reason: AWPApplicationReason.ENFORCE_CHILD_ARRANGEMENTS_ORDER,
         applicableCaseTypes: [CaseType.C100],
         applicablePartyType: {
-          C100: [PartyType.APPLICANT],
+          C100: [PartyType.APPLICANT, PartyType.RESPONDENT],
         },
       },
     ],
@@ -265,8 +265,8 @@ const applicationTypesConfiguration: AWPApplicationTypesConfig[] = [
         reason: AWPApplicationReason.REQUEST_COURT_TO_ACT_DURING_DISOBEY,
         applicableCaseTypes: [CaseType.C100, CaseType.FL401],
         applicablePartyType: {
-          C100: [PartyType.APPLICANT],
-          FL401: [PartyType.APPLICANT],
+          C100: [PartyType.APPLICANT, PartyType.RESPONDENT],
+          FL401: [PartyType.APPLICANT, PartyType.RESPONDENT],
         },
       },
     ],
