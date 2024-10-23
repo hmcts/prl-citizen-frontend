202502d743d47b88b658c0c6cfe7041e74dc6d76
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2024-10-23 14:50:03 +0100

    PRL-6433: Fix error messages for staying in refuge screen

--- src/main/steps/common/refuge/staying-in-refuge/postController.ts
+++ src/main/steps/common/refuge/staying-in-refuge/postController.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import autobind from 'autobind-decorator';
 import { Response } from 'express';
+import _ from 'lodash';
 
 import { C100Applicant, C100RebuildPartyDetails, PartyType, YesOrNo } from '../../../../app/case/definition';
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../app/controller/PostController';
-import { FormFields, FormFieldsFn } from '../../../../app/form/Form';
+import { Form, FormFields, FormFieldsFn } from '../../../../app/form/Form';
 import { getPeople } from '../../../../steps/c100-rebuild/child-details/live-with/utils';
 import { getPartyDetails, updatePartyDetails } from '../../../../steps/c100-rebuild/people/util';
 import { C100_URL } from '../../../../steps/urls';
-import { deleteC100RefugeDoc, handleError } from '../utils';
+import { deleteC100RefugeDoc, getC8DocumentForC100, handleError } from '../utils';
 
+import { form as refugeForm } from './content';
 @autobind
 export default class StayingInRefugeController extends PostController<AnyObject> {
   constructor(protected readonly fields: FormFields | FormFieldsFn) {
@@ -23,34 +25,40 @@ export default class StayingInRefugeController extends PostController<AnyObject>
     const id = req.params.id;
     const { citizenUserLivingInRefuge } = formFields;
 
-    if (req?.originalUrl?.startsWith(C100_URL)) {
-      const c100Person = getPeople(userCase).find(person => person.id === id)!;
-      if (c100Person.partyType === PartyType.APPLICANT) {
-        const applicantDetails = getPartyDetails(id, userCase.appl_allApplicants) as C100Applicant;
-        Object.assign(applicantDetails, { liveInRefuge: citizenUserLivingInRefuge });
-        req.session.userCase.appl_allApplicants = updatePartyDetails(
-          applicantDetails,
-          req.session.userCase.appl_allApplicants
-        ) as C100Applicant[];
-      } else {
-        const otherPersonDetails = getPartyDetails(id, userCase.oprs_otherPersons) as C100RebuildPartyDetails;
-        Object.assign(otherPersonDetails, { liveInRefuge: citizenUserLivingInRefuge });
-        req.session.userCase.oprs_otherPersons = updatePartyDetails(
-          otherPersonDetails,
-          req.session.userCase.oprs_otherPersons
-        ) as C100RebuildPartyDetails[];
-      }
-
-      if (citizenUserLivingInRefuge === YesOrNo.NO) {
-        try {
-          deleteC100RefugeDoc(req, userCase, id);
-        } catch (e) {
-          req.session.errors = handleError(req.session.errors, 'deleteError', true);
+    if (!_.isEmpty(citizenUserLivingInRefuge)) {
+      if (req?.originalUrl?.startsWith(C100_URL)) {
+        const c100Person = getPeople(userCase).find(person => person.id === id)!;
+        if (c100Person.partyType === PartyType.APPLICANT) {
+          const applicantDetails = getPartyDetails(id, userCase.appl_allApplicants) as C100Applicant;
+          Object.assign(applicantDetails, { liveInRefuge: citizenUserLivingInRefuge });
+          req.session.userCase.appl_allApplicants = updatePartyDetails(
+            applicantDetails,
+            req.session.userCase.appl_allApplicants
+          ) as C100Applicant[];
+        } else {
+          const otherPersonDetails = getPartyDetails(id, userCase.oprs_otherPersons) as C100RebuildPartyDetails;
+          Object.assign(otherPersonDetails, { liveInRefuge: citizenUserLivingInRefuge });
+          req.session.userCase.oprs_otherPersons = updatePartyDetails(
+            otherPersonDetails,
+            req.session.userCase.oprs_otherPersons
+          ) as C100RebuildPartyDetails[];
         }
+
+        if (citizenUserLivingInRefuge === YesOrNo.NO && !_.isEmpty(getC8DocumentForC100(id, userCase, c100Person))) {
+          try {
+            deleteC100RefugeDoc(req, userCase, id);
+          } catch (e) {
+            req.session.errors = handleError(req.session.errors, 'deleteError', true);
+          }
+        }
+      } else {
+        userCase.citizenUserLivingInRefuge = citizenUserLivingInRefuge as YesOrNo;
       }
-    } else {
-      userCase.citizenUserLivingInRefuge = citizenUserLivingInRefuge as YesOrNo;
     }
+
+    const form = new Form((refugeForm.fields as FormFieldsFn)(req.session.userCase, req));
+    const { _csrf, ...formData } = form.getParsedBody(formFields);
+    req.session.errors = form.getErrors(formData);
 
     if (onlyContinue) {
       return this.redirect(req, res);
