b773a686dea6d1df731826a5e6ed8b6e15cae72f
Vivek Sekhar <vivek.sekhar@hmcts.net>
2024-10-24 19:05:57 +0100

    feat(PRL-6319): Integrate CAN6 Service of docs notification

--- src/main/steps/common/task-list/components/notification-banner/config/ca_respondent.test.ts
+++ src/main/steps/common/task-list/components/notification-banner/config/ca_respondent.test.ts
@@ -3,9 +3,10 @@ import { CA_RESPONDENT_CONFIG } from './ca_respondent';
 describe('ca_respondent', () => {
   test('should have correct notification ids', () => {
     const ca_respondentNotifications = CA_RESPONDENT_CONFIG();
-    expect(ca_respondentNotifications).toHaveLength(3);
+    expect(ca_respondentNotifications).toHaveLength(4);
     expect(ca_respondentNotifications[0].id).toBe('applicationServedByCourtToRespondent');
     expect(ca_respondentNotifications[1].id).toBe('submitFM5');
     expect(ca_respondentNotifications[2].id).toBe('orderNonPersonalService');
+    expect(ca_respondentNotifications[3].id).toBe('serveDocuments');
   });
 });
