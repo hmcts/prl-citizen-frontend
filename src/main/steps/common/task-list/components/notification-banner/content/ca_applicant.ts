afbfcb2206d5cd453e749b8d1c1a67adfdbfb2f2
Vivek Sekhar <vivek.sekhar@hmcts.net>
2024-11-14 17:27:37 +0000

    feat(PRL-6561): Update SOD notfication

--- src/main/steps/common/task-list/components/notification-banner/content/ca_applicant.ts
+++ src/main/steps/common/task-list/components/notification-banner/content/ca_applicant.ts
@@ -506,11 +506,21 @@ const en: NotificationBannerContentConfig = {
         ],
         links: [
           {
-            text: 'Link to the new document(s)',
+            text: 'View your documents',
             href: applyParms(VIEW_TYPE_DOCUMENT, {
               partyType: PartyType.APPLICANT,
               type: DocumentPartyType.OTHER,
             }),
+          },
+        ],
+      },
+      {
+        contents: [
+          {
+            text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
+          },
+          {
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
           },
         ],
       },
@@ -997,11 +1007,21 @@ const cy: typeof en = {
         ],
         links: [
           {
-            text: 'Dolen i’r ddogfen(nau) newydd',
+            text: 'Gweld eich dogfen(nau)',
             href: applyParms(VIEW_TYPE_DOCUMENT, {
               partyType: PartyType.APPLICANT,
               type: DocumentPartyType.OTHER,
             }),
+          },
+        ],
+      },
+      {
+        contents: [
+          {
+            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+          },
+          {
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
           },
         ],
       },
