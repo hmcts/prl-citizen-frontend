1b89384363a7e68a9a403b65f3964051cd46b8b3
Vivek Sekhar <vivek.sekhar@hmcts.net>
2024-11-20 18:11:33 +0000

    feat(PRL-6606): Update SOA DA dashboard notification

--- src/main/steps/common/task-list/components/notification-banner/content/da_applicant.ts
+++ src/main/steps/common/task-list/components/notification-banner/content/da_applicant.ts
@@ -54,6 +54,16 @@ const en: NotificationBannerContentConfig = {
           },
         ],
       },
+      {
+        contents: [
+          {
+            text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
+          },
+          {
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+          },
+        ],
+      },
     ],
   },
   applicationServedByCourtAdminBailiffToDARespondent: {
@@ -224,6 +234,16 @@ const en: NotificationBannerContentConfig = {
           },
         ],
       },
+      {
+        contents: [
+          {
+            text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
+          },
+          {
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+          },
+        ],
+      },
     ],
   },
   orderSOSPersonalServiceByCourtAdminBailiffToDARespondent: {
@@ -312,6 +332,16 @@ const cy: typeof en = {
               partyType: PartyType.APPLICANT,
               context: 'personal-service',
             }),
+          },
+        ],
+      },
+      {
+        contents: [
+          {
+            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+          },
+          {
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
           },
         ],
       },
@@ -482,6 +512,16 @@ const cy: typeof en = {
             show: (caseData: Partial<CaseWithId>): boolean => {
               return isApplicationPackAvailable(caseData, PartyType.APPLICANT);
             },
+          },
+        ],
+      },
+      {
+        contents: [
+          {
+            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+          },
+          {
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
           },
         ],
       },
