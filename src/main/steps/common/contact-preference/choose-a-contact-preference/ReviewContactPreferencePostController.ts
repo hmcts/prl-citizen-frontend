cc327f65aca3faaff368141bddaf50f6d5939f22
sealDebolina <debolina.seal@hmcts.net>
2024-12-17 18:11:59 +0000

    6778

--- src/main/steps/common/contact-preference/choose-a-contact-preference/ReviewContactPreferencePostController.ts
+++ src/main/steps/common/contact-preference/choose-a-contact-preference/ReviewContactPreferencePostController.ts
@@ -4,6 +4,10 @@ import type { Response } from 'express';
 import { AppRequest } from '../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../app/controller/PostController';
 import { Form, FormFields, FormFieldsFn } from '../../../../app/form/Form';
+import { applyParms } from '../../../../steps/common/url-parser';
+import { getCasePartyType } from '../../../../steps/prl-cases/dashboard/utils';
+import { getPartyDetails } from '../../../../steps/tasklistresponse/utils';
+import { PageLink, REVIEW_CONTACT_PREFERENCE } from '../../../../steps/urls';
 import { saveAndRedirectContactDetailsAndPreference } from '../../confirm-contact-details/checkanswers/ConfirmContactDetailsPostController';
 
 @autobind
@@ -16,12 +20,23 @@ export default class ReviewContactPreferencePostController extends PostControlle
     const fields = typeof this.fields === 'function' ? this.fields(req.session.userCase, req) : this.fields;
     const form = new Form(fields);
     const { _csrf, ...formData } = form.getParsedBody(req.body);
-    Object.assign(req.session.userCase, formData);
+    const { user, userCase } = req.session;
+    Object.assign(userCase, formData);
     req.session.applicationSettings = { ...req.session.applicationSettings, navFromContactPreferences: true };
-    try {
-      await saveAndRedirectContactDetailsAndPreference(req, res);
-    } catch (error) {
-      throw new Error('ReviewContactPreferencePostController - error when saving contact preferences and redirecting');
+    const partyType = getCasePartyType(userCase, user.id);
+    const partyDetailsContactPreference = getPartyDetails(userCase, user.id)?.contactPreferences;
+    if (userCase.partyContactPreference !== partyDetailsContactPreference) {
+      try {
+        await saveAndRedirectContactDetailsAndPreference(req, res);
+      } catch (error) {
+        throw new Error(
+          'ReviewContactPreferencePostController - error when saving contact preferences and redirecting'
+        );
+      }
+    } else {
+      req.session.save(() => {
+        res.redirect(applyParms(REVIEW_CONTACT_PREFERENCE, { partyType }) as PageLink);
+      });
     }
   }
 }
