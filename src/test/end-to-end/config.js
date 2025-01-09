003063222596f71319a117a05c384fdd3bf41f5f
Pavi <pva08@Pavithrans-MacBook-Pro.local>
2025-01-09 14:12:21 +0000

    Update values in preview template yml to point to AAT

--- src/test/end-to-end/config.js
+++ src/test/end-to-end/config.js
@@ -66,6 +66,6 @@ module.exports = {
 
   bxuiBseUrl: process.env.URL || 'https://manage-case.aat.platform.hmcts.net/cases',
   runningEnv: process.env.ENVIRONMENT,
-  pcqEnabled: process.env.ENABLE_PCQ_COMPONENT || true,
+  pcqEnabled: process.env.ENABLE_PCQ_COMPONENT !== undefined ? process.env.ENABLE_PCQ_COMPONENT === "true" : true,
   // eslint-disable-next-line no-magic-numbers
 };
