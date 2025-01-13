dd8ee518f73b733d8965b75fbd911127bc0bd922
Vivek Sekhar <vivek.sekhar@hmcts.net>
2025-01-13 11:16:05 +0000

    Fortify fixes

--- src/main/steps/common/utils.ts
+++ src/main/steps/common/utils.ts
@@ -1,6 +1,11 @@
 import { Document } from '../../app/case/definition';
+import { AppRequest } from '../../app/controller/AppRequest';
 import { FormError } from '../../app/form/Form';
 import { isFileSizeGreaterThanMaxAllowed, isValidFileFormat } from '../../app/form/validation';
+import { parseUrl } from '../../steps/common/url-parser';
+import { SIGN_IN_URL, SIGN_IN_URL_WITH_CALLBACK } from '../../steps/urls';
+
+import { interpolate } from './string-parser';
 
 export const removeErrors = (errors: FormError[] | undefined): FormError[] => {
   return errors?.length
@@ -47,4 +52,21 @@ export const getUploadedDocumentErrorType = (
   }
 
   return errorType;
+};
+
+//eslint-disable-next-line @typescript-eslint/ban-types
+export const validateRedirectUrl = (Urls: Object, req: AppRequest): string | undefined => {
+  const currentUrlChunks = req.url.split('/');
+  return Object.values(Urls).find(url => {
+    return parseUrl(url)
+      .url.split('/')
+      .every(chunk => currentUrlChunks.includes(chunk));
+  });
+};
+
+//eslint-disable-next-line @typescript-eslint/ban-types
+export const getLoginUrl = (Urls: Object, req: AppRequest): string => {
+  return validateRedirectUrl(Urls, req)
+    ? interpolate(SIGN_IN_URL_WITH_CALLBACK, { url: encodeURIComponent(req.url) })
+    : SIGN_IN_URL;
 };
