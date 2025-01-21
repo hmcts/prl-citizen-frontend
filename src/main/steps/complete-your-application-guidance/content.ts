cd6c652283cee8b94d5fd46d9d857fa362699d98
hcarslaw <119344120+hcarslaw@users.noreply.github.com>
2025-01-21 17:38:31 +0000

    PRL-6807: Integrate new fee api for complete application page (#1745)
    * PRL-6807: Integrate new fee api for complete application screening question page
    
    * PRL-6807: Handle error scenario where fee cant be fetched
    
    * PRL-6807: Fix review comment
    
    * PRL-6807: Remove reference to post
    
    ---------
    
    Co-authored-by: Channabasappagouda Patil <109162337+channa-patil@users.noreply.github.com>

--- src/main/steps/complete-your-application-guidance/content.ts
+++ src/main/steps/complete-your-application-guidance/content.ts
@@ -1,5 +1,8 @@
+import _ from 'lodash';
+
 import { TranslationFn } from '../../app/controller/GetController';
 import { FormContent } from '../../app/form/Form';
+import { interpolate } from '../../steps/common/string-parser';
 import { getMOJForkingScreenUrl } from '../../steps/urls';
 export * from './routeGuard';
 
@@ -7,32 +10,38 @@ export * from './routeGuard';
 const en = () => ({
   title: 'Completing your application',
   paragraphs: [
-    'You can complete this application online or by post.',
-    'At the end of the application process you will need to pay a court fee of £232.',
+    'You can complete this application online.',
+    'At the end of the application process you will need to pay a court fee{feeText}',
     'The payment will be taken at the end of the process, when you submit your application.',
   ],
   helpPayingCourtFeesSubHeading: 'Get help paying court fees',
   courtFeesParagraphs: [
     'You may be able to get help with some or all of your court fees depending on your savings, income and benefits.',
     'If you apply for help with fees a payment will not be taken when you submit the application.',
-    "<p class='govuk-body'> Check the <a href='https://www.gov.uk/get-help-with-court-fees' class='govuk-link' rel='external' target='_blank'>help with fees guidance on GOV.UK</a> to find out if you meet the criteria and apply for support.</p>",
+    "<p class='govuk-body'> Check the <a href='https://www.gov.uk/get-help-with-court-fees' class='govuk-link' rel='external' target='_blank'>help with fees guidance on GOV.UK (opens in a new tab)</a> to find out if you meet the criteria and apply for support.</p>",
   ],
+  feeRetrievedText: ' of £{feeAmount}.',
+  errorRetrievingFee:
+    ". The leaftlet EX50 tells you the fee amount - <a href='https://www.gov.uk/government/publications/fees-in-the-civil-and-family-courts-main-fees-ex50/family-court-fees-ex50'class='govuk-link' rel='external' target='_blank'>Family court fees (EX50) (opens in a new tab)</a>",
   cancel: 'Cancel',
 });
 
 const cy = () => ({
   title: 'Cwblhau eich cais',
   paragraphs: [
-    'Gallwch gwblhau’r cais hwn ar-lein neu ei anfon drwy’r post.',
-    'Ar ddiwedd y broses gwneud cais bydd angen ichi dalu ffi’r llys o £232.',
+    'Gallwch gwblhau’r cais hwn ar-lein.',
+    'Ar ddiwedd y broses gwneud cais bydd angen ichi dalu ffi’r llys{feeText}',
     'Fe gymerir y taliad ar ddiwedd y broses, pan fyddwch yn cyflwyno eich cais.',
   ],
   helpPayingCourtFeesSubHeading: 'Help i dalu ffioedd llys',
   courtFeesParagraphs: [
     'Efallai y gallwch gael help gyda rhywfaint o’ch ffioedd llys neu’r holl ffioedd yn dibynnu ar eich cynilion, eich incwm a’ch budd-daliadau.',
     'Os byddwch yn gwneud cais am help i dalu ffioedd ni chymerir y taliad pan fyddwch yn cyflwyno’r cais.',
-    "<p class='govuk-body'> Cyfeiriwch at y cyfarwyddyd ar <a href='https://www.gov.uk/get-help-with-court-fees' class='govuk-link' rel='external' target='_blank'>help i dalu ffioedd ar GOV.UK</a> i weld os ydych yn bodloni’r meini prawf ac i wneud cais am gymorth. </p>",
+    "<p class='govuk-body'> Cyfeiriwch at y cyfarwyddyd ar <a href='https://www.gov.uk/get-help-with-court-fees' class='govuk-link' rel='external' target='_blank'>help i dalu ffioedd ar GOV.UK (yn agor mewn tab newydd)</a> i weld os ydych yn bodloni’r meini prawf ac i wneud cais am gymorth. </p>",
   ],
+  feeRetrievedText: ' o £{feeAmount}.',
+  errorRetrievingFee:
+    ". The leaftlet EX50 tells you the fee amount - <a href='https://www.gov.uk/government/publications/fees-in-the-civil-and-family-courts-main-fees-ex50/family-court-fees-ex50'class='govuk-link' rel='external' target='_blank'>Family court fees (EX50) (opens in a new tab)</a> (welsh)",
   cancel: 'Canslo',
 });
 
@@ -56,6 +65,12 @@ export const form: FormContent = {
 export const generateContent: TranslationFn = content => {
   const translations = languages[content.language]();
   const testingSupport = content.additionalData?.req?.session?.testingSupport;
+  const feeAmount = _.get(content, 'userCase.c100ApplicationFees', '');
+  const feeText =
+    feeAmount !== '' ? interpolate(translations.feeRetrievedText, { feeAmount }) : translations.errorRetrievingFee;
+  translations.paragraphs[1] = interpolate(translations.paragraphs[1], {
+    feeText,
+  });
 
   return {
     ...translations,
