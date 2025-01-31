5a6382c3a16ef6c7a7edef0b159b61f3710bdcb5
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2025-01-31 13:25:08 +0000

    s2s pact fix

--- src/test/pact/rpe-service-auth-provider.test.ts
+++ src/test/pact/rpe-service-auth-provider.test.ts
@@ -3,11 +3,13 @@ import { when } from 'jest-when';
 
 import { getTokenFromApi } from '../../main/app/auth/service/get-service-auth-token';
 
+const { Matchers } = require('@pact-foundation/pact');
 const { pactWith } = require('jest-pact');
+const { string } = Matchers;
 
 jest.mock('otplib', () => ({
   authenticator: {
-    generate: jest.fn(() => '123456'),
+    generate: jest.fn(() => '784467'),
   },
 }));
 
@@ -21,28 +23,27 @@ pactWith(
   },
   provider => {
     describe('rpe-service-auth-provider API', () => {
-      const EXPECTED_RESPONSE = 'MOCK_TOKEN';
+      const EXPECTED_RESPONSE = 'someMicroServiceToken';
 
       const successResponse = {
         status: 200,
         headers: {
-          'content-type': 'application/json',
+          'Content-Type': 'text/plain;charset=ISO-8859-1',
         },
-        body: EXPECTED_RESPONSE,
+        body: string('someMicroServiceToken'),
       };
 
       const serviceAuthTokenRequest = {
-        uponReceiving: 'a request for service auth token',
+        uponReceiving: 'a request for a token for a microservice',
         withRequest: {
           method: 'POST',
-          path: '/testing-support/lease',
+          path: '/lease',
           headers: {
-            Accept: 'application/json, text/plain, */*',
             'content-type': 'application/json',
           },
           body: {
-            microservice: 'prl-citizen-frontend',
-            oneTimePassword: '123456',
+            microservice: 'microserviceName',
+            oneTimePassword: '784467',
           },
         },
       };
@@ -52,12 +53,12 @@ pactWith(
           .calledWith('services.authProvider.url')
           .mockReturnValue(provider.mockService.baseUrl)
           .calledWith('services.authProvider.microservice')
-          .mockReturnValue('prl-citizen-frontend')
+          .mockReturnValue('microserviceName')
           .calledWith('services.authProvider.secret')
           .mockReturnValue('mock-secret');
 
         const interaction = {
-          state: 'i request a service auth token',
+          state: 'microservice with valid credentials',
           ...serviceAuthTokenRequest,
           willRespondWith: successResponse,
         };
