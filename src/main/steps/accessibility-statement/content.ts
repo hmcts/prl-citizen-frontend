5896577a9c9aad396638b4f4c364f537a21fb8d7
Caroline-Amanquah-HMCTS <Caroline.Amanquah@HMCTS.NET>
2025-02-04 15:33:44 +0000

    updated nunjucks file

--- src/main/steps/accessibility-statement/content.ts
+++ src/main/steps/accessibility-statement/content.ts
@@ -1,49 +1,51 @@
 import { TranslationFn } from '../../app/controller/GetController';
 
-const en = {
+export const en = {
   title: 'Accessibility statement for Family Private Law',
-  websiteAvailable:
+  paragraphs: [
     'This accessibility statement applies to <a href="/" class="govuk-link" target="_blank">this link</a>, that enables users to make child arrangements or submit domestic abuse applications.',
-  websiteRanBy: 'The website is run by HM Courts & Tribunals Service (HMCTS).',
-  asManyAsPossible:
+    'The website is run by HM Courts & Tribunals Service (HMCTS).',
     'We want as many people as possible to be able to use this website. For example, that means you should be able to:',
-  asManyAsPossibleColours: 'change colours, contrast levels and fonts using browser or device settings',
-  asManyAsPossibleZoom: 'zoom in up to 400% without the text spilling off the screen',
-  asManyAsPossibleKeyboard: 'navigate most of the website using just a keyboard',
-  asManyAsPossibleSpeech: 'navigate most of the website using speech recognition software',
-  asManyAsPossibleListen:
-    'listen to most of the website using a screen reader (including the most recent versions of JAWS, NVDA and VoiceOver',
-  simpleAsPossible: 'We’ve also made the website text as simple as possible to understand.',
-  abilityNet:
+  ],
+  accessibilityOptions: [
+    'change colours, contrast levels and fonts using browser or device settings',
+    'zoom in up to 400% without the text spilling off the screen',
+    'navigate most of the website using just a keyboard',
+    'navigate most of the website using speech recognition software',
+    'listen to most of the website using a screen reader (including the most recent versions of JAWS, NVDA and VoiceOver)',
+  ],
+  additionalParagraphs: [
+    'We’ve also made the website text as simple as possible to understand.',
     '<a href="https://mcmw.abilitynet.org.uk" class="govuk-link" target="_blank">AbilityNet</a> has advice on making your device easier to use if you have a disability.',
+  ],
   howAccessible: 'How accessible this website is',
   somePartsNot: 'We know some parts of this website are not fully accessible, for example:',
-  somePartsNotDescriptiveness: 'some heading labels are non-descriptive',
-  somePartsNotText: 'some content becomes obscured when resizing text',
-  somePartsNotPDF: "some PDF documents aren't fully accessible to screen reader software",
-  somePartsNotWelsh:
+  notAccessibleParts: [
+    'some heading labels are non-descriptive',
+    'some content becomes obscured when resizing text',
+    "some PDF documents aren't fully accessible to screen reader software",
     'a language attribute for some Welsh text is missing which will impact some text to speech software pronunciation',
-  somePartsNotError: 'error messages require improvement to convey suggestions on how to correct the error',
+    'error messages require improvement to convey suggestions on how to correct the error',
+  ],
   feedbackAndContactInformation: 'Feedback and contact information',
-  textRelayServiceInformation:
+  contactInfo: [
     'We provide a text relay service for people who are d/Deaf, hearing impaired or have a speech impediment.',
-  audioInductionLoopsInformation:
     'Our offices have audio induction loops, or if you contact us before your visit we can arrange a British Sign Language (BSL) interpreter.',
-  formatInformation:
     'If you require support when visiting or need information on this website in a different format like accessible PDF, large print, easy read, audio recording or braille contact us at:',
-  emailInfo: 'Email: hmctsforms@justice.gov.uk',
-  phone: 'Telephone:  0300 323 0185',
+  ],
+  phoneAndEmail: ['Email: hmctsforms@justice.gov.uk', 'Telephone: 0300 323 0185'],
   considerYourRequest: 'We’ll consider your request and get back to you in 10 working days.',
   reportingAccessibility: 'Reporting accessibility problems with this website',
-  lookingToImprove: 'We’re always looking to improve the accessibility of this website.',
-  improveAccessibility:
+  improvements: [
+    'We’re always looking to improve the accessibility of this website.',
     'If you find any problems not listed on this page or think we’re not meeting accessibility requirements:',
-  accessibilityPhoneNumber: 'Telephone: 0300 323 0185',
+  ],
   enforcementProcedure: 'Enforcement procedure',
-  humanRightsCommission:
+  humanRightsInfo: [
     'The Equality and Human Rights Commission (EHRC) is responsible for enforcing the <a href="https://www.legislation.gov.uk/uksi/2018/852/contents/made" class="govuk-link" target="_blank" aria-label="This link will open a new tab for legislation.gov.uk">Public Sector Bodies (Websites and Mobile Applications) (No. 2) Accessibility Regulations 2018 </a> (the ‘accessibility regulations’).',
-  notHappy:
     'If you’re not happy with how we respond to your complaint, contact the <a href="https://www.equalityadvisoryservice.com/" class="govuk-link" target="_blank" aria-label="This link will open in a new tab for Equality Advisory and Support Service">contact the Equality Advisory and Support Service (EASS)</a>.',
+  ],
+
   technicalInfo: 'Technical information about this website’s accessibility',
   hmctsIsCommitted:
     'HMCTS is committed to making its website accessible, in accordance with the Public Sector Bodies (Websites and Mobile Applications) (No. 2) Accessibility Regulations 2018.',
@@ -53,54 +55,41 @@ const en = {
   nonAccessibleContent: 'Non-accessible content',
   nonAccessibleDescription: 'The content listed below is non-accessible for the following reasons.',
   issuesWithDocuments: 'Non-compliance with the accessibility regulations',
-  issuesWithDocumentDescription1:
+  issuesWithDocumentDescriptions: [
     'Heading labels are provided however they are non-descriptive.  This does not meet WCAG 2.2 success criterion 2.4.6 Headings and Labels (Level AA).',
-  issuesWithDocumentDescription2:
     'An autocomplete attribute has been supplied an invalid value for ‘property name or number’ which may cause issues for screen reader users.  This does not meet WCAG 2.2 success criterion 1.3.5 Identify Input Purpose (Level AA).',
-  issuesWithDocumentDescription3:
     'Text was found to fail the minimum text contrast requirements within ‘draft’ marked PDF documents.  This does not meet WCAG 2.2 success criterion 1.4.3 Contrast (Minimum) (Level AA)',
-  issuesWithDocumentDescription4:
     'Content becomes obscured when resizing text on the page. This does not meet WCAG 2.2 success criterion 1.4.4 Resize text (Level AA).',
-  issuesWithDocumentDescription5:
     'Some error messages do not convey suggestions on how to correct the errors.  This does not meet WCAG 2.2 success criterion 3.3.3 Error Suggestion (Level AA).',
-  issuesWithDocumentDescription6:
     'A timeout feature is present however there is no way to adjust or cancel when the timeout occurs throughout the service. The lack of a prior warning may cause frustration and make it much more difficult for users who need additional time. This fails WCAG 2.2 success criterion 2.2.1 Timing Adjustable (Level A).',
-  issuesWithDocumentDescription7:
     'A fieldset is used however no legend was included and in some cases is unnecessary. This can be confusing for screen reader users when navigating out of context. This fails WCAG 2.2 success criterion 1.3.1 Info and Relationships (Level A).',
-  issuesWithDocumentDescription8:
     'Optional pages are not relayed as such for users navigating the service which means that some users may become stuck at this stage as they may feel forced to upload a document as nothing on the page informs users of the step being optional. This fails WCAG 2.2 success criterion 3.3.2 Labels or Instructions (Level A).',
-  issuesWithDocumentDescription9:
     'Some labels may be present but not programmatically associated to the form element which means that assistive technology users will be presented with an unlabelled input. This fails WCAG 2.2 success criterion 1.3.1 Info and Relationships (Level A).',
-  issuesWithDocumentDescription10:
     'Some form fields do not have a visible label present which means that assistive technology users do not get the necessary information conveyed to them. This fails WCAG 2.2 success criterion 3.3.2 Labels or Instructions (Level A).',
-  issuesWithDocumentDescription11:
     'At least on page title was found to be non-descriptive which means that the page does not present its purpose which would normally be presented in heading level one. This fails WCAG 2.2 success criterion 2.4.2 Page Titled (Level A).',
-  issuesWithDocumentDescription12:
     'At least one PDF document was found to be missing a title. This property is used by screen readers to informs these users of the document’s details. This fails WCAG 2.2 success criterion 2.4.2 Page Titled (Level A).',
-  issuesWithDocumentDescription13:
     'At least on PDF document does not contain tags used to define elements on the page which can prevent assistive technology users to access the content within the PDF. This fails WCAG 2.2 success criterion 1.3.1 Info and Relationships (Level A).',
-  issuesWithDocumentDescription14:
     'A non-interactive element receives focus when navigating via keyboard/screen reader software. This affects the navigation for keyboard and screen reader users where the section will receive the focus order when it is unneeded. This fails WCAG 2.2 success criterion 2.4.3 Focus Order (Level A).',
-  issuesWithDocumentDescription15:
     'An input has a label that is not programmatically associated. This means that hint text that contains important information when inputting data has not been associated to the relevant inputs on the page. This fails WCAG 2.2 success criterion 1.3.1 Info and Relationships (Level A).',
-  issuesWithDocumentDescription16:
     'Empty links may be encountered when navigating the service. This means that assistive technology users may experience unexpected functionality which may disorient these users.  This fails WCAG 2.2 success criterion 2.4.3 Focus Order (Level A).',
+  ],
   thirdPartyContent1: 'Content that’s not within the scope of the accessibility regulations',
   thirdPartyContent2: 'Third party content that’s under someone else’s control',
-  thirdPartyContentDescription1:
+  thirdPartyContent3:
     'Pages and websites that are linked to and from the Family Private Law service may not be fully accessible. These include: ',
-  thirdPartyContentDescription2: 'External pages hosting PDF documents',
-  thirdPartyContentDescription3: 'The payment pages which have a separate accessibility statement',
+  thirdPartyContentDescriptions: [
+    'External pages hosting PDF documents',
+    'The payment pages which have a separate accessibility statement',
+  ],
   toImproveAccessibility: 'What we’re doing to improve accessibility',
   improveAccessibilityDescription:
     'We are working to improve accessibility on this website, and plan to fix the identified issues by 30 November 2025.',
   preparationAccessibilityStatement: 'Preparation of this accessibility statement',
-  statementPreparationDate:
+  statementPreparationInfo: [
     'This statement was prepared on Wednesday 4 January 2023. It was last reviewed on 24 January 2025.',
-  statementPreparationWebsite:
     'This website was last tested by the Digital Accessibility Centre (DAC) on 31st July 2024 across a range of assistive technologies and with disabled users.',
-  statementReviewedFixes:
     'Fixes were reviewed by the HMCTS accessibility testing team in the Testing Centre of Excellence.',
+  ],
 };
 
 const cy = {
