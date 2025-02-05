5af239ea7d673fe611b921daf0ed59d85bebb71e
Caroline-Amanquah-HMCTS <Caroline.Amanquah@HMCTS.NET>
2025-02-05 11:09:19 +0000

    updated dashboard notifications

--- src/main/steps/common/task-list/components/notification-banner/content/da_applicant.ts
+++ src/main/steps/common/task-list/components/notification-banner/content/da_applicant.ts
@@ -127,10 +127,10 @@ const en: NotificationBannerContentConfig = {
       {
         contents: [
           {
-            text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
+            text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building. ',
           },
           {
-            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them and may not be able to print papers on the day.</strong>',
           },
         ],
       },
@@ -212,7 +212,7 @@ const en: NotificationBannerContentConfig = {
             text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
           },
           {
-            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them and may not be able to print papers on the day.</strong>',
           },
         ],
       },
@@ -249,7 +249,7 @@ const en: NotificationBannerContentConfig = {
             text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
           },
           {
-            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them and may not be able to print papers on the day.</strong>',
           },
         ],
       },
@@ -299,7 +299,7 @@ const en: NotificationBannerContentConfig = {
             text: 'If you’re coming to a court or tribunal for a hearing, bring your hearing letter with your case number – the case number helps you find where you need to go in the building.',
           },
           {
-            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them or be able to print papers on the day.</strong>',
+            text: '<strong>You must also bring any papers that you need for your hearing as the court will not provide you with electronic devices to view them and may not be able to print papers on the day.</strong>',
           },
         ],
       },
@@ -347,10 +347,10 @@ const cy: typeof en = {
       {
         contents: [
           {
-            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+            text: "Os ydych chi'n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â'ch llythyr gwrandawiad gyda'ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.",
           },
           {
-            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau gyda chi i’ch gwrandawiad oherwydd ni fydd y llys yn darparu dyfeisiau electronig ar eich cyfer ac efallai na fydd modd argraffu copïau papur ar y diwrnod.</strong>',
           },
         ],
       },
@@ -499,10 +499,10 @@ const cy: typeof en = {
       {
         contents: [
           {
-            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+            text: "Os ydych chi'n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â'ch llythyr gwrandawiad gyda'ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.",
           },
           {
-            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau gyda chi i’ch gwrandawiad oherwydd ni fydd y llys yn darparu dyfeisiau electronig ar eich cyfer ac efallai na fydd modd argraffu copïau papur ar y diwrnod.</strong>',
           },
         ],
       },
@@ -536,10 +536,10 @@ const cy: typeof en = {
       {
         contents: [
           {
-            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+            text: "Os ydych chi'n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â'ch llythyr gwrandawiad gyda'ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.",
           },
           {
-            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau gyda chi i’ch gwrandawiad oherwydd ni fydd y llys yn darparu dyfeisiau electronig ar eich cyfer ac efallai na fydd modd argraffu copïau papur ar y diwrnod.</strong>',
           },
         ],
       },
@@ -585,10 +585,10 @@ const cy: typeof en = {
       {
         contents: [
           {
-            text: 'Os ydych chi’n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â’ch llythyr gwrandawiad gyda’ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.',
+            text: "Os ydych chi'n dod i lys neu dribiwnlys ar gyfer gwrandawiad, dewch â'ch llythyr gwrandawiad gyda'ch rhif achos arno efo chi - mae rhif yr achos yn eich helpu i ddod o hyd i ble mae angen i chi fynd yn yr adeilad.",
           },
           {
-            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau sydd eu hangen arnoch ar gyfer eich gwrandawiad gan na fydd y llys yn darparu dyfeisiau electronig i chi eu gweld nac yn gallu argraffu’r papurau ar y diwrnod.</strong>',
+            text: '<strong>Rhaid i chi hefyd ddod ag unrhyw bapurau gyda chi i’ch gwrandawiad oherwydd ni fydd y llys yn darparu dyfeisiau electronig ar eich cyfer ac efallai na fydd modd argraffu copïau papur ar y diwrnod.</strong>',
           },
         ],
       },
