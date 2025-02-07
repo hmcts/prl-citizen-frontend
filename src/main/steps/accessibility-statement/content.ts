92c908ace11999a2c23123a6fb8aa3797ee490e4
Caroline-Amanquah-HMCTS <Caroline.Amanquah@HMCTS.NET>
2025-02-07 10:11:54 +0000

    updated welsh translation section

--- src/main/steps/accessibility-statement/content.ts
+++ src/main/steps/accessibility-statement/content.ts
@@ -94,90 +94,73 @@ export const en = {
 
 const cy = {
   title: 'Datganiad hygyrchedd ar gyfer y gwasanaeth mabwysiadu',
-  websiteRanBy: 'Mae’r gwasanaeth hwn yn caniatáu i ddarpar rieni wneud cais i fabwysiadu plentyn ar-lein.',
-  asManyAsPossible:
+  paragraphs: [
+    'Mae’r gwasanaeth hwn yn caniatáu i ddarpar rieni wneud cais i fabwysiadu plentyn ar-lein.',
     'Gwasanaeth Llysoedd a Thribiwnlysoedd EM sy’n gyfrifol am y gwasanaeth hwn.  Rydym eisiau i gymaint o bobl â phosibl allu ei ddefnyddio, felly rydym wedi ceisio ei wneud mor hygyrch â phosibl.  Er enghraifft, dylech allu:',
-  asManyAsPossibleColours: 'newid y lliwiau, y lefelau cyferbyniad a’r ffontiau',
-  asManyAsPossibleZoom: 'gwneud y testun hyd at 400% yn fwy heb iddo ddiflannu oddi ar y sgrin',
-  asManyAsPossibleKeyboard: "llywio'r rhan fwyaf o'r wefan gan ddefnyddio bysellfwrdd yn unig",
-  asManyAsPossibleSpeech: "llywio'r rhan fwyaf o'r wefan gan ddefnyddio meddalwedd adnabod llais",
-  asManyAsPossibleListen:
+  ],
+  accessibilityOptions: [
+    'newid y lliwiau, y lefelau cyferbyniad a’r ffontiau',
+    'gwneud y testun hyd at 400% yn fwy heb iddo ddiflannu oddi ar y sgrin',
+    "llywio'r rhan fwyaf o'r wefan gan ddefnyddio bysellfwrdd yn unig",
+    "llywio'r rhan fwyaf o'r wefan gan ddefnyddio meddalwedd adnabod llais",
     'gwrando ar y rhan fwyaf o’r wefan gan ddefnyddio darllenydd sgrin (gan gynnwys fersiynau diweddaraf NVDA, CCA (Dadansoddwr cyferbyniad lliwiau) a VoiceOver)',
-  simpleAsPossible: "Rydym hefyd wedi gwneud y testun mor syml â phosibl i'w ddeall.",
-  abilityNet:
+  ],
+  additionalParagraphs: [
+    "Rydym hefyd wedi gwneud y testun mor syml â phosibl i'w ddeall.",
     '<a href="https://mcmw.abilitynet.org.uk" class="govuk-link" target="_blank">Mae AbilityNet</a> yn rhoi cyngor ar sut i wneud eich dyfais yn haws i’w defnyddio os oes gennych anabledd.',
+  ],
   howAccessible: "Pa mor hygyrch yw'r wefan hon",
   somePartsNot: "Rydym yn gwybod nad yw rhai rhannau o'r wefan hon yn gwbl hygyrch:",
-  somePartsNotReflow: 'nid yw’r testun yn newid i fod mewn un golofn pan fyddwch yn newid maint ffenestr y porwr',
-  somePartsNotSpacing: 'ni allwch newid lled y llinellau neu fylchiad y testun',
-  somePartsNotFooter: 'Nid yw’r holl ddolenni yn y troedynnau yn weithredol ac felly nid ydynt wedi’u profi.',
+  notAccessibleParts: [
+    'nid yw’r testun yn newid i fod mewn un golofn pan fyddwch yn newid maint ffenestr y porwr',
+    'ni allwch newid lled y llinellau neu fylchiad y testun',
+    'Nid yw’r holl ddolenni yn y troedynnau yn weithredol ac felly nid ydynt wedi’u profi.',
+  ],
   feedbackAndContactInformation: 'Adborth a gwybodaeth gyswllt',
-  needMoreInformation:
+  contactInfo: [
     'Os ydych angen gwybodaeth sydd ar y wefan hon mewn fformat arall megis ar ffurf PDF hygyrch, print bras, fformat hawdd ei ddarllen, recordiad sain neu braille:',
-  email: 'E-bost: <a href=”privatelawproject@justice.gov.uk” class=”govuk-link”>privatelawproject@justice.gov.uk</a>',
-  phone: 'Ffoniwch: 01634 887900',
-  considerYourRequest: 'Byddwn yn ystyried eich cais ac yn ymateb o fewn 10 diwrnod gwaith.',
+  ],
+  phoneAndEmail: [
+    'E-bost: <a href=”privatelawproject@justice.gov.uk” class=”govuk-link”>privatelawproject@justice.gov.uk</a>',
+    'Ffoniwch: 01634 887900',
+  ],
+  considerYourRequest: 'Riportio',
   reportingAccessibility: "Riportio problemau hygyrchedd gyda'r wefan hon",
-  accessibilityPhoneNumber: 'Ffôn: 01634 887900',
-  improveAccessibility:
+  improvements: [
     'Rydym wastad yn ceisio gwella hygyrchedd y wefan hon. Os byddwch yn cael unrhyw broblemau nad ydynt yn cael eu crybwyll ar y dudalen hon, neu os ydych yn credu nad ydym yn bodloni gofynion y rheoliadau hygyrchedd, cysylltwch â: <a href=”mailto:privatelawproject@justice.gov.uk” class=”govuk-link”>privatelawproject@justice.gov.uk</a>.',
+  ],
   enforcementProcedure: 'Y Weithdrefn Orfodi',
-  humanRightsCommission:
+  humanRightsInfo: [
     'Y Comisiwn Cydraddoldeb a Hawliau Dynol (EHRC) sy’n gyfrifol am orfodi Rheoliadau Hygyrchedd Cyrff y Sector Cyhoeddus (Gwefannau a Rhaglenni Symudol) (Rhif 2) 2018 (y ‘rheoliadau hygyrchedd’).',
-  notHappy:
     'Os ydych wedi cysylltu â ni ynghylch mater hygyrchedd ac nad ydych yn fodlon â’n hymateb, gallwch gysylltu â’r <a href="https://www.equalityadvisoryservice.com/" class="govuk-link" target="_blank">Gwasanaeth Chynghori a Chymorth Cydraddoldeb (EASS)</a>.',
-  contactingUs: 'Cysylltu â ni dros y ffôn neu ymweld â ni’n bersonol',
-  contactCourtDirectly:
-    'Os oes gennych gwestiwn am hygyrchedd yn ein llysoedd teulu, gallwch <a href="https://www.find-court-tribunal.service.gov.uk/services/childcare-and-parenting/adoption/search-by-postcode?lng=cy" class="govuk-link" target="blank">cysylltu â’r llys yn uniongyrchol</a>.',
-  contactTextRelay:
-    'Rydym yn darparu gwasanaeth cyfnewid negeseuon testun ar gyfer pobl fyddar, pobl sydd â nam ar eu clyw a phobl sydd â nam ar eu lleferydd.',
-  contactInductionLoops:
-    'Mae gan y llysoedd teulu ddolenni sain a gallwch hefyd ofyn am gael mynediad heb risiau, cyfieithydd iaith arwyddion neu gyfieithydd iaith dramor.',
+  ],
   technicalInfo: 'Gwybodaeth dechnegol am hygyrchedd y wefan hon',
   hmctsIsCommitted:
     'Mae GLlTEM wedi ymrwymo i sicrhau bod ei wefannau yn hygyrch, a hynny yn unol â Rheoliadau Hygyrchedd Cyrff y Sector Gyhoeddus (Gwefannau a Rhaglenni Symudol) (Rhif 2) 2018.',
   complianceStatus: 'Statws cydymffurfiaeth',
   partiallyCompliant:
     'Mae’r wefan hon yn cydymffurfio’n rhannol â safon <a href="https://www.w3.org/TR/WCAG21/" class="govuk-link" target="blank">AA Canllawiau Hygyrchedd Cynnwys Gwe fersiwn 2.1,</a>a hynny oherwydd y materion o beidio â chydymffurfio a’r esemptiadau a restrir isod.',
-  partiallyCompliantMobile:
-    'Cynnwys ddim i’w weld heb golli gwybodaeth ac angen sgrolio mewn dwy ffordd wahanol wrth ddefnyddio’r gwasanaeth Mabwysiadu ar ddyfais symudol. ',
   nonAccessibleContent: 'Cynnwys sydd ddim o fewn cwmpas y rheoliadau hygyrchedd',
+  nonAccessibleDescription: 'Dogfennau',
   issuesWithDocuments: 'Dogfennau',
-  issuesWithDocumentDescription1:
+  issuesWithDocumentDescriptions: [
     'Nid yw nifer o’n dogfennau PDF a Word hŷn yn bodloni’r safonau hygyrchedd - er enghraifft, efallai nad ydynt wedi’u strwythuro fel eu bod yn hygyrch i ddarllenydd sgrin. Nid yw hyn yn bodloni WCAG 2.1 maen prawf llwyddiant 4.1.2 (Enw, Rôl, Gwerth).',
-  issuesWithDocumentDescription2:
     'Mae rhai o’n dogfennau PDF a Word yn hanfodol i’r gwasanaethau a ddarparwn. Erbyn Rhagfyr 2021, rydym yn cynllunio i naill ai ddatrys y materion neu eu cyfnewid gyda thudalennau HTML hygyrch. Bydd unrhyw ddogfennau PDF neu Word newydd y byddwn yn eu cyhoeddi yn bodloni’r safonau hygyrchedd.',
-  issuesSurveys: 'Arolygon',
-  issuesSurveysDescription:
     'Mae ein ffurflen adborth a’r arolwg adborth yn cael eu cynnal gan ddarparwr trydydd parti. Mi wnaethom adnabod nifer o faterion hygyrchedd gyda’r tudalennau hyn ond mae eu datrys y tu hwnt i’n rheolaeth. Rydym yn trafod gyda thîm Trawsnewid Perfformiad a Chanfyddiad y Weinyddiaeth Gyfiawnder i ganfod pa welliannau y gellir gwneud.',
-  issuesWithLinks: 'Problemau gyda dolenni',
-  issuesWithLinksDescription:
-    'Ar rai tudalennau, nid yw’r testun a ddefnyddir ar gyfer dolenni yn nodi’n glir i ble y bydd y ddolen yn mynd â chi neu beth yw ei phwrpas. Nid yw hyn yn bodloni WCAG 2.1 maen prawf llwyddiant 2.4.4 (Pwrpas y Ddolen, Ystyried y Cyd-destun).',
-  issuesWithHeadings: 'Problemau gyda phenawdau',
-  issuesWithHeadingsDescription: 'Amherthnasol',
-  issuesWithColour: 'Cyferbyniad lliwiau',
-  issuesWithColourDescription: 'Amherthnasol',
-  issuesWithLanguage: 'Problemau ieithyddol',
-  issuesWithLanguageDescription:
+  ],
+  thirdPartyContent1: 'Problemau gyda dolenni',
+  thirdPartyContent2: 'Problemau gyda dolenni',
+  thirdPartyContent3: 'Problemau gyda dolenni',
+  thirdPartyContentDescriptions: ['Problemau gyda dolenni', 'Problemau gyda dolenni'],
+  toImproveAccessibility: 'Problemau gyda phenawdau',
+  improveAccessibilityDescription: [
     'Ar rai tudalennau, nid yw’r iaith wedi’i rhoi mewn cod. Nid yw hyn yn bodloni WCAG 2.1 maen prawf llwyddiant 3.1.2 (Iaith tudalen) gan fod y cyfieithiad “CYMRAEG” yn cael ei baratoi ar hyn o bryd.',
-  issuesWithOther: 'Problemau hysbys eraill',
-  issuesWithOtherDescription1: 'Amherthnasol',
-  howWeTested: 'Sut rydym wedi profi’r wefan hon',
-  howWeTestedUI:
-    'Mae’r rhyngwyneb a ddefnyddir gan y defnyddiwr wedi’i brofi gan ein tîm gan ddefnyddio amrywiaeth o adnoddau.',
-  howWeTestedPatty: 'Pa11Y – Adnodd profi awtomatiaeth',
-  howWeTestedNVDA: 'Adnodd NVDA',
-  howWeTestedColourContrast: 'Adnodd dadansoddi cyferbyniad lliwiau i ddilysu cymarebau cyferbyniad',
-  howWeTestedVoiceOver: 'VoiceOver',
-  howWeTestedE2E: 'Profi holl borwyr GDS o’r dechrau i’r diwedd ar gyfer defnyddio bysellfwrdd.',
-  improvingAccessibility: 'Beth rydym yn ei wneud i wella hygyrchedd',
-  statementCommitted:
-    'Rydym wedi ymrwymo i sicrhau bod ein gwasanaethau’n hygyrch i’n holl gwsmeriaid a’u bod yn cydymffurfio â lefel AA Canllawiau Hygyrchedd Cynnwys Gwe - WCAG 2.1.',
-  statementDAC:
-    'I’n helpu i gyflawni hyn, byddwn yn comisiynu <a href="https://digitalaccessibilitycentre.org/" class="govuk-link" target="_blank">y Ganolfan Hygyrchedd Digidol (DAC)</a>. i gynnal archwiliad cydymffurfiad technegol WCAG 2.1 lefel AA lle bydd profion yn cael eu cynnal am dros 50 awr gan ddefnyddwyr sydd ag amrywiaeth eang o anableddau. Byddwn yn diweddaru’r datganiad hwn gyda dyddiad y profi ar ôl iddo gael ei gadarnhau. Bydd unrhyw faterion a adnabyddir yn ystod y profi yn cael eu hadolygu ac yn cael y sylw priodol',
-  preparationAccessibilityStatement: 'Paratoi’r datganiad hygyrchedd hwn',
-  statementPreparationDate:
+  ],
+  preparationAccessibilityStatement: 'Adnodd dadansoddi cyferbyniad lliwiau i ddilysu cymarebau cyferbyniad',
+  statementPreparationInfo: [
     'Cafodd y datganiad hwn ei baratoi ar 11 Mawrth 2022. Cafodd ei adolygu diwethaf ar 11 Mawrth 2022.',
+    'I’n helpu i gyflawni hyn, byddwn yn comisiynu <a href="https://digitalaccessibilitycentre.org/" class="govuk-link" target="_blank">y Ganolfan Hygyrchedd Digidol (DAC)</a>. i gynnal archwiliad cydymffurfiad technegol WCAG 2.1 lefel AA lle bydd profion yn cael eu cynnal am dros 50 awr gan ddefnyddwyr sydd ag amrywiaeth eang o anableddau. Byddwn yn diweddaru’r datganiad hwn gyda dyddiad y profi ar ôl iddo gael ei gadarnhau. Bydd unrhyw faterion a adnabyddir yn ystod y profi yn cael eu hadolygu ac yn cael y sylw priodol',
+  ],
 };
 
 const languages = {
