b0c1d5615b804e5dcd8c8ddc4e5790e997447a8e
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2025-03-28 11:30:23 +0000

    fixed lint issue

--- src/test/pact/publish/publish.ts
+++ src/test/pact/publish/publish.ts
@@ -8,26 +8,26 @@ const config = require('config');
 const git = require('git-rev-sync');
 
 if (process.env.PACT_TAG === 'master') {
-console.log('Publishing Pact contract');
-const opts = {
-  pactFilesOrDirs: [path.resolve(process.cwd(), config.services.pact.pactDirectory)],
-  pactBroker: config.services.pact.url,
-  consumerVersion: git.short(),
-  tags: config.services.pact.tag,
-};
+  console.log('Publishing Pact contract');
+  const opts = {
+    pactFilesOrDirs: [path.resolve(process.cwd(), config.services.pact.pactDirectory)],
+    pactBroker: config.services.pact.url,
+    consumerVersion: git.short(),
+    tags: config.services.pact.tag,
+  };
 
-const certPath = path.resolve(__dirname, './ca-bundle.crt');
-console.log('cert path = ' + certPath);
-process.env.SSL_CERT_FILE = certPath;
+  const certPath = path.resolve(__dirname, './ca-bundle.crt');
+  console.log('cert path = ' + certPath);
+  process.env.SSL_CERT_FILE = certPath;
 
-pact
-  .publishPacts(opts)
-  .then(() => {
-    console.log('Pact contract publishing complete!');
-  })
-  .catch(e => {
-    console.log('Pact contract publishing failed: ', e);
-  });
- } else {
+  pact
+    .publishPacts(opts)
+    .then(() => {
+      console.log('Pact contract publishing complete!');
+    })
+    .catch(e => {
+      console.log('Pact contract publishing failed: ', e);
+    });
+} else {
   console.log('Not publishing Pact contract on non-master branch');
-} 
+}
