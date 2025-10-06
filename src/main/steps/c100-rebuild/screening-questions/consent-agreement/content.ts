b401ff34d9b418eb4a3fc68ee4d2772b71b18d7f
SomeshRA <somesh.acharya1@hmcts.net>
2025-10-06 15:07:13 +0100

    FPVTL-SA-DAC-01-Fieldset-Legend: added missing comma

--- src/main/steps/c100-rebuild/screening-questions/consent-agreement/content.ts
+++ src/main/steps/c100-rebuild/screening-questions/consent-agreement/content.ts
@@ -53,7 +53,7 @@ export const form: FormContent = {
       classes: 'govuk-radios',
       fieldset: {
         legend: {
-          text: l => l.title
+          text: l => l.title,
         },
       },
       values: [
