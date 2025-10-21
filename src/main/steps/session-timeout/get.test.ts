b9d5a56d7831fef40ca3fc0cf6650d0bf3e3eb0d
Jefex7 <duartepireslopes@gmail.com>
2025-10-21 15:43:33 +0100

    FPVTL 1291 : re change template

--- src/main/steps/session-timeout/get.test.ts
+++ src/main/steps/session-timeout/get.test.ts
@@ -1,40 +1,17 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
-import { Language, generatePageContent } from '../common/common.content';
 
-import { generateContent } from './content';
 import { SessionTimeoutGetController } from './get';
 
-describe('SessionTimeoutGetController', () => {
-  let controller: SessionTimeoutGetController;
+describe('TimedOutGetController', () => {
+  const controller = new SessionTimeoutGetController();
 
-  beforeEach(() => {
-    controller = new SessionTimeoutGetController();
-  });
-
-  test('should destroy session and render timeout page', async () => {
-    const req = mockRequest({
-      session: {
-        lang: 'en' as Language,
-        user: { email: 'test@example.com' },
-        userCase: {},
-        destroy: jest.fn(cb => cb()), // immediately call the callback
-      },
-    });
-
+  test('Should destroy session and render timeout page', async () => {
+    const req = mockRequest();
     const res = mockResponse();
-
-    const content = generatePageContent({
-      language: req.session.lang,
-      pageContent: generateContent,
-      userCase: req.session.userCase,
-      userEmail: req.session.user.email,
-      additionalData: { req },
-    });
-
     await controller.get(req, res);
 
     expect(req.session.destroy).toHaveBeenCalled();
-    expect(res.render).toHaveBeenCalledWith('session-timeout/template', content);
+    expect(res.render).toHaveBeenCalledWith(expect.stringContaining('/session-timeout'), expect.anything());
   });
 });
--- src/main/steps/session-timeout/get.ts
+++ src/main/steps/session-timeout/get.ts
@@ -2,27 +2,19 @@ import autobind from 'autobind-decorator';
 import { Response } from 'express';
 
 import { AppRequest } from '../../app/controller/AppRequest';
-import { Language, generatePageContent } from '../common/common.content';
+import { GetController } from '../../app/controller/GetController';
 
 import { generateContent } from './content';
 
 @autobind
-export class SessionTimeoutGetController {
+export class SessionTimeoutGetController extends GetController {
+  constructor() {
+    super(__dirname + '/template', generateContent);
+  }
+
   public async get(req: AppRequest, res: Response): Promise<void> {
-    const language: Language = req.session?.lang === 'cy' ? 'cy' : 'en';
+    res.locals['lang'] = req.session.lang;
 
-    const content = generatePageContent({
-      language,
-      pageContent: generateContent,
-      userCase: req.session?.userCase,
-      userEmail: req.session?.user?.email,
-      additionalData: { req },
-    });
-
-    if (req.session) {
-      req.session.destroy(() => res.render('session-timeout/template', content));
-    } else {
-      res.render('session-timeout/template', content);
-    }
+    req.session.destroy(() => super.get(req, res));
   }
 }
