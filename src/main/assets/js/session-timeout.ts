5b093003c236633ee857ca9ce7c645b921de1fc2
Jefex7 <duartepireslopes@gmail.com>
2025-10-28 15:51:01 +0000

    FPVTL 1291 : maintain dialog box

--- src/main/assets/js/session-timeout.ts
+++ src/main/assets/js/session-timeout.ts
@@ -4,7 +4,7 @@ const KEEP_ALIVE_URL = '/keep-alive';
 const SESSION_TIMEOUT_URL = '/session-timeout';
 
 const ACTIVITY_PING_INTERVAL = 1 * 60 * 1000; // 1 minute
-const TOTAL_SESSION_TIMEOUT = 3 * 60 * 1000; // 3 minutes (time until redirect)
+const TOTAL_SESSION_TIMEOUT = 2 * 60 * 1000; // 3 minutes (time until redirect)
 const COUNTDOWN_DURATION = 50;
 
 const DIALOG_APPEARANCE_DELAY = TOTAL_SESSION_TIMEOUT - COUNTDOWN_DURATION * 1000;
@@ -55,10 +55,14 @@ const resetTimeout = () => {
 
 const pingUserActive = throttle(
   () => {
+    if (dialog && !dialog.classList.contains('govuk-!-display-none')) {
+      return;
+    }
     fetch(KEEP_ALIVE_URL, { cache: 'no-store' })
       .then(() => resetTimeout()) // Reset after successful ping
       .catch(() => console.warn('Keep-alive failed'));
   },
+
   ACTIVITY_PING_INTERVAL,
   { trailing: false }
 );
