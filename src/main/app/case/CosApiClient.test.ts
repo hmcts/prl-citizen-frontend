0f954e269963ee9c1b82bab2fcc45294ff9a6fab
sunilrochani <sunil.rochani@hotmail.com>
2025-12-17 16:06:49 +0000

    FPVTL-1510: Ensure Cases with legacy court values can still be progressed

--- src/main/app/case/CosApiClient.test.ts
+++ src/main/app/case/CosApiClient.test.ts
@@ -613,6 +613,15 @@ describe('CosApiClient', () => {
 
     expect(flag).toEqual(true);
   });
+
+  test('findCourtByPostCodeAndService', async () => {
+    const response = { data: 'Court Name' };
+    mockedAxios.get.mockReturnValueOnce(response as unknown as Promise<string>);
+    const req = mockRequest();
+    const client = new CosApiClient('abc', mockLogger);
+    const actual = await client.findCourtByPostCodeAndService('SA1 3TU', req.session.user);
+    expect(actual).toEqual(response.data);
+  });
 });
 
 describe('CosApiClientWithError', () => {
@@ -909,14 +918,5 @@ describe('createAWPApplication', () => {
       'Error occured, could not sumbit statement of service. - SubmitStatementOfService'
     );
     expect(mockLogger.error).toHaveBeenCalledWith('API Error POST undefined 500');
-  });
-
-  test('findCourtByPostCodeAndService', async () => {
-    const response = { data: 'Court Name' };
-    mockedAxios.get.mockReturnValueOnce(response as unknown as Promise<string>);
-    const req = mockRequest();
-    const client = new CosApiClient('abc', mockLogger);
-    const actual = await client.findCourtByPostCodeAndService('SA1 3TU', req.session.user);
-    expect(actual).toEqual(response.data);
   });
 });
