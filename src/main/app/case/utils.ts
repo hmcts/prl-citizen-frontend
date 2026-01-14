86fc77b2b26fed2e020cafa3b3e8b0cc6c8d927d
dharmendra kumar <dharmendra.kumar@hmcts.net>
2026-01-14 09:33:50 +0000

    Remove comments

--- src/main/app/case/utils.ts
+++ src/main/app/case/utils.ts
@@ -5,8 +5,6 @@ export const logError = (error: AxiosError, logger: LoggerInstance): void => {
   if (error.response) {
     logger.error(`API Error ${error.config?.method} ${error.config?.url} ${error.response.status}`);
     logger.info('Response: ', error.response.data);
-    logger.info('status: ', error.response.status);
-    logger.info('status text: ', error.response.statusText);
   } else if (error.request) {
     logger.error(`API Error ${error.config?.method} ${error.config?.url}`);
   } else {
