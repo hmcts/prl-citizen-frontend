e16b1d5d55623c6a8f363437e2fe63db94094419
DelythJustice <delyth.jones11@justice.gov.uk>
2026-07-30 16:27:32 +0100

    FPVTL-3195 clean up tests

--- src/main/steps/common/refuge/navigationController.test.ts
+++ src/main/steps/common/refuge/navigationController.test.ts
@@ -1,13 +1,5 @@
 import { mockRequest } from '../../../../test/unit/utils/mockRequest';
-import { CaseType } from '../../../app/case/definition';
-import {
-  C100_REFUGE_UPLOAD_DOC,
-  DASHBOARD_URL,
-  REFUGE_DOC_ALREADY_UPLOADED,
-  REFUGE_KEEPING_SAFE,
-  REFUGE_UPLOAD_DOC,
-  STAYING_IN_REFUGE,
-} from '../../urls';
+import { DASHBOARD_URL, REFUGE_KEEPING_SAFE, STAYING_IN_REFUGE } from '../../urls';
 
 import RefugeNavigationController from './navigationController';
 
@@ -163,47 +155,6 @@ describe('C8 refuge > navigationController', () => {
     );
   });
 
-  test('should redirect from upload doc', async () => {
-    req.originalUrl = '/applicant';
-    expect(RefugeNavigationController.getNextPageUrl(REFUGE_UPLOAD_DOC, req.session.userCase, req)).toBe(
-      '/applicant/confirm-contact-details/addressdetails'
-    );
-  });
-
-  test('should redirect from c100 upload doc', async () => {
-    req.originalUrl = '/c100-rebuild';
-    expect(RefugeNavigationController.getNextPageUrl(C100_REFUGE_UPLOAD_DOC, req.session.userCase, req)).toBe(
-      '/c100-rebuild/applicant/7483640e-0817-4ddc-b709-6723f7925474/personal-details'
-    );
-  });
-
-  test('should redirect from document already uploaded for C100 other person when yes selected', async () => {
-    req.originalUrl = '/c100-rebuild';
-    req.session.userCase = {
-      ...req.session.userCase,
-      reUploadRefugeDocument: 'Yes',
-      oprs_otherPersons: [
-        {
-          id: '6b792169-84df-4e9a-8299-c2c77c9b7e58',
-          applicantFirstName: 'Test',
-          applicantLastName: 'Test',
-          liveInRefuge: 'Yes',
-          refugeConfidentialityC8Form: {
-            document_url: 'MOCK_URL',
-            document_binary_url: 'MOCK_BINARY_URL',
-            document_filename: 'MOCK_FILENAME',
-          },
-        },
-      ],
-    };
-    req.params = {
-      id: '6b792169-84df-4e9a-8299-c2c77c9b7e58',
-    };
-    expect(RefugeNavigationController.getNextPageUrl(REFUGE_DOC_ALREADY_UPLOADED, req.session.userCase, req)).toBe(
-      '/c100-rebuild/refuge/upload-refuge-document/6b792169-84df-4e9a-8299-c2c77c9b7e58'
-    );
-  });
-
   test('should redirect from staying in refuge page to c8 upload for other person who is in refuge', async () => {
     const otherPersonId = '6b792169-84df-4e9a-8299-c2c77c9b7e58';
     req.originalUrl = '/c100-rebuild';
@@ -242,87 +193,6 @@ describe('C8 refuge > navigationController', () => {
 
     expect(RefugeNavigationController.getNextPageUrl(STAYING_IN_REFUGE, req.session.userCase, req)).toBe(
       `/c100-rebuild/other-person-details/${otherPersonId}/address/lookup`
-    );
-  });
-
-  test('should redirect from document already uploaded for C100 applicant when no selected', async () => {
-    req.originalUrl = '/c100-rebuild';
-    req.session.userCase = {
-      ...req.session.userCase,
-      reUploadRefugeDocument: 'No',
-      appl_allApplicants: [
-        {
-          id: '7483640e-0817-4ddc-b709-6723f7925474',
-          applicantFirstName: 'dummy',
-          applicantLastName: 'Test',
-          liveInRefuge: 'Yes',
-          refugeConfidentialityC8Form: {
-            document_url: 'MOCK_URL',
-            document_binary_url: 'MOCK_BINARY_URL',
-            document_filename: 'MOCK_FILENAME',
-          },
-        },
-      ],
-    };
-    req.params = {
-      id: '7483640e-0817-4ddc-b709-6723f7925474',
-    };
-    expect(RefugeNavigationController.getNextPageUrl(REFUGE_DOC_ALREADY_UPLOADED, req.session.userCase, req)).toBe(
-      '/c100-rebuild/applicant/7483640e-0817-4ddc-b709-6723f7925474/personal-details'
-    );
-  });
-
-  test('should redirect from document already uploaded for applicant/respondent when yes selected', async () => {
-    req.originalUrl = '/applicant';
-    req.session.userCase = {
-      ...req.session.userCase,
-      reUploadRefugeDocument: 'Yes',
-      isCitizenLivingInRefuge: 'Yes',
-      refugeDocument: {
-        document_url: 'MOCK_URL',
-        document_binary_url: 'MOCK_BINARY_URL',
-        document_filename: 'MOCK_FILENAME',
-      },
-    };
-    expect(RefugeNavigationController.getNextPageUrl(REFUGE_DOC_ALREADY_UPLOADED, req.session.userCase, req)).toBe(
-      '/applicant/refuge/upload-refuge-document'
-    );
-  });
-
-  test('should redirect from document already uploaded for applicant/respondent when no selected', async () => {
-    req.originalUrl = '/respondent';
-    req.session.userCase = {
-      ...req.session.userCase,
-      reUploadRefugeDocument: 'No',
-      isCitizenLivingInRefuge: 'Yes',
-      refugeDocument: {
-        document_url: 'MOCK_URL',
-        document_binary_url: 'MOCK_BINARY_URL',
-        document_filename: 'MOCK_FILENAME',
-      },
-      caseTypeOfApplication: CaseType.FL401,
-      caseInvites: [
-        {
-          id: '1234',
-          value: {
-            isApplicant: 'No',
-            invitedUserId: '1234',
-          },
-        },
-      ],
-      respondentsFL401: {
-        user: {
-          id: '1234',
-          idamId: '1234',
-        },
-      },
-    };
-    req.session.user = {
-      id: '1234',
-      idamId: '1234',
-    };
-    expect(RefugeNavigationController.getNextPageUrl(REFUGE_DOC_ALREADY_UPLOADED, req.session.userCase, req)).toBe(
-      '/respondent/confirm-contact-details/addressdetails'
     );
   });
 
--- src/main/steps/common/refuge/sequence.test.ts
+++ src/main/steps/common/refuge/sequence.test.ts
@@ -12,9 +12,9 @@ const refugeMockData = mockRequest({
 });
 
 describe('C8 refuge > sequence', () => {
-  test('should contain 1 entries in respondent 1 screen sequence', () => {
+  test('should contain 2 entries in refuge sequence', () => {
     const sequence = C8RefugeSequence.getSequence();
-    expect(sequence).toHaveLength(5);
+    expect(sequence).toHaveLength(2);
 
     expect(sequence[0].url).toBe('/:root/refuge/staying-in-refuge/:id?');
     expect(sequence[0].showInSection).toBe('c100');
@@ -25,24 +25,6 @@ describe('C8 refuge > sequence', () => {
     expect(sequence[1].url).toBe('/:root/refuge/keeping-details-safe/:id?');
     expect(sequence[1].showInSection).toBe('c100');
     expect(sequence[1].getNextStep(refugeMockData.session.userCase, refugeMockData)).toBe(
-      '/applicant/confirm-contact-details/addressdetails'
-    );
-
-    expect(sequence[2].url).toBe('/:root/refuge/upload-refuge-document/:removeFileId?');
-    expect(sequence[2].showInSection).toBe('c100');
-    expect(sequence[2].getNextStep(refugeMockData.session.userCase, refugeMockData)).toBe(
-      '/applicant/confirm-contact-details/addressdetails'
-    );
-
-    expect(sequence[3].url).toBe('/:root/refuge/upload-refuge-document/:id/:removeFileId?');
-    expect(sequence[3].showInSection).toBe('c100');
-    expect(sequence[3].getNextStep(refugeMockData.session.userCase, refugeMockData)).toBe(
-      '/applicant/confirm-contact-details/addressdetails'
-    );
-
-    expect(sequence[4].url).toBe('/:root/refuge/refuge-document-already-uploaded/:id?');
-    expect(sequence[4].showInSection).toBe('c100');
-    expect(sequence[4].getNextStep(refugeMockData.session.userCase, refugeMockData)).toBe(
       '/applicant/confirm-contact-details/addressdetails'
     );
   });
