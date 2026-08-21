c764901422d241a2d1fc52a398e457d152d5726d
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2026-08-21 17:31:59 +0100

    FPVTL-3124: Set filesize limit of 20MB before file is loaded into memory

--- src/main/modules/fileupload/index.ts
+++ src/main/modules/fileupload/index.ts
@@ -1,5 +1,6 @@
 import { Application } from 'express';
 import fileUpload from 'express-fileupload';
+import { MAX_UPLOAD_BYTES } from '../../app/case/definition';
 
 /**
  * @FileUpload
@@ -7,6 +8,10 @@ import fileUpload from 'express-fileupload';
  */
 export class FileUpload {
   public enableFor(app: Application): void {
-    app.use(fileUpload());
+    app.use(fileUpload({
+      limits: { fileSize: MAX_UPLOAD_BYTES }, // 20 MB file size limit
+      abortOnLimit: true,
+      responseOnLimit: `File size limit of ${MAX_UPLOAD_BYTES} bytes has been reached.`,
+    }));
   }
 }
