2cc50df97001b392f34d4d35a472967186613498
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2026-09-16 17:08:46 +0100

    Revert change

--- src/main/app/auth/user/oidc.ts
+++ src/main/app/auth/user/oidc.ts
@@ -10,9 +10,7 @@ import { AppRequest, UserDetails } from '../../controller/AppRequest';
 
 export const getRedirectUrl = (serviceUrl: string, callbackUrlPageLink: PageLink): string => {
   const clientId: string = config.get('services.idam.clientID');
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const loginUrl: string = config.get('services.idam.authorizationURL').replace('login', 'o/authorize');
+  const loginUrl: string = config.get('services.idam.authorizationURL');
   const callbackUrl = serviceUrl + callbackUrlPageLink;
   const scope: string = config.get('services.idam.authorizationScope');
 
